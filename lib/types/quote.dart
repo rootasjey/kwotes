454f627b27f3791621d089b25c62f03b38654218
rootasjey <jeremiecorpinot@outlook.com>
2019-11-24 15:42:23 +0100

    👆Update Quote type

--- lib/types/quote.dart
+++ lib/types/quote.dart
@@ -1,12 +1,16 @@
 
+import 'author.dart';
+
 class Quote {
+  final Author author;
   final String id;
   final String name;
 
-  Quote({this.id, this.name});
+  Quote({this.author, this.id, this.name});
 
   factory Quote.fromJSON(Map<String, dynamic> json) {
     return Quote(
+      author: json['author'] != null ? Author.fromJSON(json['author']) : null,
       id: json['id'],
       name: json['name'],
     );
