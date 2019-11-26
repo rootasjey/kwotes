b906ef9bb90514e702fcb80c96bc7b431aefee13
rootasjey <jeremiecorpinot@outlook.com>
2019-11-26 14:55:46 +0100

    👆Remove AppBar and replace w/ BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,32 @@ class App extends StatefulWidget {
   AppState createState() => AppState();
 }
 
-class AppState extends State<App> {
+class AppState extends State<App> with SingleTickerProviderStateMixin {
+  int _currentIndex = 0;
+
+  final List<Widget> _children = [
+    QuotidianWidget(),
+    RecentWidget(),
+    Icon(Icons.person),
+  ];
+
+  TabController _tabController;
+
   bool isAuth = false;
   ValueNotifier<GraphQLClient> client;
+
+  void _tabChanged () {
+    setState(() {
+      _currentIndex = _tabController.index;
+    });
+  }
 
   @override
   void initState() {
     super.initState();
+
+    _tabController = new TabController(vsync: this, length: _children.length);
+    _tabController.addListener(_tabChanged);
 
     createClient()
       .then((newClient) {
@@ -25,6 +44,13 @@ class AppState extends State<App> {
           client = newClient;
         });
       });
+  }
+
+  @override
+  void dispose() {
+    _tabController.removeListener(_tabChanged);
+    _tabController.dispose();
+    super.dispose();
   }
 
   Future<Map<String, dynamic>> getApiConfig() async {
@@ -68,23 +94,33 @@ class AppState extends State<App> {
         home: DefaultTabController(
           length: 3,
           child: Scaffold(
-            appBar: AppBar(
+            bottomNavigationBar: BottomNavigationBar(
               backgroundColor: Color(0xFF706FF0),
-              bottom: TabBar(
-                tabs: <Widget>[
-                  Tab(icon: Icon(Icons.wb_sunny),),
-                  Tab(icon: Icon(Icons.list),),
-                  Tab(icon: Icon(Icons.person_pin),),
-                ],
-              ),
-              title: Text('Memorare'),
+              selectedFontSize: 16.0,
+              selectedItemColor: Colors.white,
+              unselectedItemColor: Color(0xFFFFFFEE),
+              currentIndex: _currentIndex,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.wb_sunny),
+                  title: Text('Today'),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.list),
+                  title: Text('Recent'),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person_pin),
+                  title: Text('Account'),
+                ),
+              ],
+              onTap: (int index) {
+                _tabController.animateTo(index);
+              },
             ),
             body: TabBarView(
-              children: <Widget>[
-                QuotidianWidget(),
-                RecentWidget(),
-                Icon(Icons.person),
-              ],
+              children: _children,
+              controller: _tabController,
             ),
           ),
         )
