47faad24e4f9c1dc280869d9819b4064218e4fe3
rootasjey <jeremiecorpinot@outlook.com>
2019-11-26 01:06:07 +0100

    ➕Add scroll to Quotidian widget (w/ ListView)

--- lib/quotidian.dart
+++ lib/quotidian.dart
@@ -1,7 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:memorare/types/quotidian.dart';
+import 'types/quotidian.dart';
 
 enum QuoteAction { addList, like, share }
 
@@ -45,114 +44,110 @@ class QuotidianWidget extends StatelessWidget {
         var quotidian = Quotidian.fromJSON(result.data['quotidian']);
 
         return Scaffold(
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(color: Color(0xFF706FD3)),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
+          backgroundColor: Color(0xFF706FD3),
+          body: ListView(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(color: Color(0xFF706FD3)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      '${quotidian.quote.name}',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(right: 10.0),
+                            child: CircleAvatar(
+                              backgroundColor: Color(0xFFF56098),
+                              backgroundImage: quotidian.quote.author.imgUrl.length > 1 ?
+                                NetworkImage(quotidian.quote.author.imgUrl) :
+                                AssetImage('assets/images/monk.png'),
+                              child: Text('${quotidian.quote.author.name.substring(0,1)}'),
+                            ),
+                          ),
+                          Text(
+                            '${quotidian.quote.author.name}',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Text(
+                            '${quotidian.quote.references.length > 0 ? quotidian.quote.references[0].name : ""}',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
                       children: <Widget>[
-                        Text(
-                          '${quotidian.quote.name}',
-                          style: TextStyle(
+                        PopupMenuButton(
+                          icon: Icon(
+                            Icons.more_horiz,
                             color: Colors.white,
-                            fontSize: 35,
-                            fontWeight: FontWeight.bold
+                            size: 40,
+                            semanticLabel: 'Open quote actions',
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 20.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: <Widget>[
-                              Padding(
-                                padding: EdgeInsets.only(right: 10.0),
-                                child: CircleAvatar(
-                                  backgroundColor: Color(0xFFF56098),
-                                  backgroundImage: quotidian.quote.author.imgUrl.length > 1 ?
-                                    NetworkImage(quotidian.quote.author.imgUrl) :
-                                    AssetImage('assets/images/monk.png'),
-                                  child: Text('${quotidian.quote.author.name.substring(0,1)}'),
+                          itemBuilder: (BuildContext context) => <PopupMenuEntry<QuoteAction>>[
+                            const PopupMenuItem<QuoteAction>(
+                              value: QuoteAction.like,
+                              child: ListTile(
+                                leading: Icon(
+                                  Icons.favorite_border,
+                                  size: 25,
                                 ),
-                              ),
-                              Text(
-                                '${quotidian.quote.author.name}',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
+                                title: Text('Favorite'),
+                              )
+                            ),
+                            const PopupMenuItem<QuoteAction>(
+                              value: QuoteAction.addList,
+                              child: ListTile(
+                                  leading: Icon(
+                                  Icons.list,
+                                  size: 25,
                                 ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 10.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: <Widget>[
-                              Text(
-                                '${quotidian.quote.references.length > 0 ? quotidian.quote.references[0].name : ""}',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: <Widget>[
-                            PopupMenuButton(
-                              icon: Icon(
-                                Icons.more_horiz,
-                                color: Colors.white,
-                                size: 40,
-                                semanticLabel: 'Open quote actions',
-                              ),
-                              itemBuilder: (BuildContext context) => <PopupMenuEntry<QuoteAction>>[
-                                const PopupMenuItem<QuoteAction>(
-                                  value: QuoteAction.like,
-                                  child: ListTile(
-                                    leading: Icon(
-                                      Icons.favorite_border,
-                                      size: 25,
-                                    ),
-                                    title: Text('Favorite'),
-                                  )
+                                title: Text('Add to...'),
+                              )
+                            ),
+                            const PopupMenuItem<QuoteAction>(
+                              value: QuoteAction.share,
+                              child: ListTile(
+                                leading: Icon(
+                                  Icons.share,
+                                  size: 25,
                                 ),
-                                const PopupMenuItem<QuoteAction>(
-                                  value: QuoteAction.addList,
-                                  child: ListTile(
-                                      leading: Icon(
-                                      Icons.list,
-                                      size: 25,
-                                    ),
-                                    title: Text('Add to...'),
-                                  )
-                                ),
-                                const PopupMenuItem<QuoteAction>(
-                                  value: QuoteAction.share,
-                                  child: ListTile(
-                                    leading: Icon(
-                                      Icons.share,
-                                      size: 25,
-                                    ),
-                                    title: Text('Share'),
-                                  )
-                                ),
-                              ],
-                            )
+                                title: Text('Share'),
+                              )
+                            ),
                           ],
                         )
                       ],
-                    ),
-                    padding: EdgeInsets.only(top: 60.0, left: 20.0, right: 20.0),
-                  )
-                )
-              ],
-            ),
+                    )
+                  ],
+                ),
+                padding: EdgeInsets.only(top: 60.0, left: 20.0, right: 20.0),
+              )
+            ],
           ),
         );
       },
