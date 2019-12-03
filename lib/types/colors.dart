2aa2451ef4d9e119fd9703c810f6fa2913d4dda5
rootasjey <jeremiecorpinot@outlook.com>
2019-12-03 21:35:32 +0100

    ➕Add theme colors

--- lib/types/colors.dart
+++ lib/types/colors.dart
@@ -4,5 +4,7 @@ import 'package:flutter/material.dart';
 class ThemeColor {
   static Color primary = Color(0xFF706FD3);
   static Color secondary = Color(0xFFF56498);
-  static Color validation = Color(0xFFE74C3C);
+  static Color validation = Color(0xFF02ECC7);
+  static Color success = Color(0xFF02ECC7);
+  static Color error = Color(0xFF02ECC7);
 }
