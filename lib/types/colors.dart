0f83ea54401c80e77bfa252cee1adfbdcf398ae6
rootasjey <jeremiecorpinot@outlook.com>
2019-12-05 01:18:50 +0100

    👆Make the red color really red

--- lib/types/colors.dart
+++ lib/types/colors.dart
@@ -6,5 +6,5 @@ class ThemeColor {
   static Color secondary = Color(0xFFF56498);
   static Color validation = Color(0xFF02ECC7);
   static Color success = Color(0xFF02ECC7);
-  static Color error = Color(0xFF02ECC7);
+  static Color error = Color(0xFFE74C3C);
 }
