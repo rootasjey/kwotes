72d753d7c45d01487281929ae83eb24e4f319ef5
rootasjey <jeremiecorpinot@outlook.com>
2019-12-05 10:04:09 +0100

    ➕Add loading component to signup

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:memorare/components/email_field.dart';
+import 'package:memorare/components/loading.dart';
 import 'package:memorare/components/name_field.dart';
 import 'package:memorare/components/password_field.dart';
 import 'package:memorare/models/http_clients.dart';
@@ -22,6 +23,8 @@ class SignupScreenState extends State<SignupScreen> {
   final _emailFieldKey = GlobalKey<EmailFieldState>();
   final _passwordFieldKey = GlobalKey<PasswordFieldState>();
   final _confirmPasswordFieldKey = GlobalKey<FormFieldState>();
+
+  bool _isLoading = false;
 
   String confirmPassword = '';
   String email = '';
@@ -46,166 +49,185 @@ class SignupScreenState extends State<SignupScreen> {
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
-        Form(
-          key: _formKey,
-          child: Padding(
-            padding: EdgeInsets.all(40.0),
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(top: 10.0),
-                  child: Text(
-                    'Sign up to a new account.',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
+        Stack(
+          children: <Widget>[
+            Form(
+              key: _formKey,
+              child: Padding(
+                padding: EdgeInsets.all(40.0),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.0),
+                      child: Text(
+                        'Sign up to a new account.',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 40.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      NameField(key: _nameFieldKey),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 30.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      EmailField(key: _emailFieldKey),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 30.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      PasswordField(key: _passwordFieldKey),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 30.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      TextFormField(
-                        key: _confirmPasswordFieldKey,
-                        decoration: InputDecoration(
-                          icon: Icon(Icons.lock_outline),
-                          labelText: 'Confirm Password',
-                          errorMaxLines: 2
-                        ),
-                        obscureText: true,
-                        onChanged: (value) {
-                          confirmPassword = value;
-                          _confirmPasswordFieldKey.currentState.validate();
-                        },
-                        validator: (value) {
-                          if (confirmPassword.isEmpty) {
-                            return 'Password confirmation cannot be empty';
-                          }
-
-                          if (confirmPassword != _passwordFieldKey.currentState.fieldValue) {
-                            return 'Password and confirm password must match.';
-                          }
-
-                          return null;
-                        },
+                    Padding(
+                      padding: EdgeInsets.only(top: 40.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          NameField(key: _nameFieldKey),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-                Mutation(
-                  builder: (RunMutation runMutation, QueryResult result) {
-                    return Padding(
-                      padding: EdgeInsets.only(top: 60.0),
-                      child: RaisedButton(
-                        color: Color(0xFF2ECC71),
-                        onPressed: () {
-                          if (!_formKey.currentState.validate()) {
-                            return;
-                          }
-
-                          name = _nameFieldKey.currentState.fieldValue;
-                          email = _emailFieldKey.currentState.fieldValue;
-                          password = _passwordFieldKey.currentState.fieldValue;
-
-                          runMutation({
-                            'email': email,
-                            'name': name,
-                            'password': password,
-                          });
-                        },
-                        child: Padding(
-                          padding: EdgeInsets.all(15.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text(
-                                'Sign up',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20.0,
-                                ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(left: 10.0),
-                                child: Icon(Icons.arrow_forward, color: Colors.white,),
-                              )
-                            ],
-                          )
-                        ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 30.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          EmailField(key: _emailFieldKey),
+                        ],
                       ),
-                    );
-                  },
-                  options: MutationOptions(
-                    document: signupMutation,
-                  ),
-                  onCompleted: (dynamic resultData) {
-                    if (resultData == null) { return; }
-
-                    Map<String, dynamic> signinJson = resultData['signin'];
-
-                    var userData = UserData.fromJSON(signinJson);
-                    var userDataModel = Provider.of<UserDataModel>(context);
-
-                    userDataModel
-                      ..update(userData)
-                      ..setAuthenticated(true)
-                      ..saveToFile(signinJson);
-
-                    Credentials(email: email, password: password).saveToFile();
-
-                    Provider.of<HttpClientsModel>(context).setToken(userData.token);
-
-                    Navigator.of(context).pop();
-                  },
-                  update: (Cache cache, QueryResult result) {
-                    if (result.hasErrors) {
-                      for (var error in result.errors) {
-                        Scaffold.of(context)
-                          .showSnackBar(
-                            SnackBar(
-                              backgroundColor: ThemeColor.validation,
-                              content: Text(
-                                '${error.message}',
-                                style: TextStyle(color: Colors.white),
-                              ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 30.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          PasswordField(key: _passwordFieldKey),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 30.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          TextFormField(
+                            key: _confirmPasswordFieldKey,
+                            decoration: InputDecoration(
+                              icon: Icon(Icons.lock_outline),
+                              labelText: 'Confirm Password',
+                              errorMaxLines: 2
                             ),
-                          );
-                      }
-                    }
-                  },
+                            obscureText: true,
+                            onChanged: (value) {
+                              confirmPassword = value;
+                              _confirmPasswordFieldKey.currentState.validate();
+                            },
+                            validator: (value) {
+                              if (confirmPassword.isEmpty) {
+                                return 'Password confirmation cannot be empty';
+                              }
+
+                              if (confirmPassword != _passwordFieldKey.currentState.fieldValue) {
+                                return 'Password and confirm password must match.';
+                              }
+
+                              return null;
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    Mutation(
+                      builder: (RunMutation runMutation, QueryResult result) {
+                        return Padding(
+                          padding: EdgeInsets.only(top: 60.0),
+                          child: RaisedButton(
+                            color: Color(0xFF2ECC71),
+                            onPressed: () {
+                              if (!_formKey.currentState.validate()) {
+                                return;
+                              }
+
+                              setState(() {
+                                _isLoading = true;
+                              });
+
+                              name = _nameFieldKey.currentState.fieldValue;
+                              email = _emailFieldKey.currentState.fieldValue;
+                              password = _passwordFieldKey.currentState.fieldValue;
+
+                              runMutation({
+                                'email': email,
+                                'name': name,
+                                'password': password,
+                              });
+                            },
+                            child: Padding(
+                              padding: EdgeInsets.all(15.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text(
+                                    'Sign up',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20.0,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(left: 10.0),
+                                    child: Icon(Icons.arrow_forward, color: Colors.white,),
+                                  )
+                                ],
+                              )
+                            ),
+                          ),
+                        );
+                      },
+                      options: MutationOptions(
+                        document: signupMutation,
+                      ),
+                      onCompleted: (dynamic resultData) {
+                        setState(() {
+                          _isLoading = false;
+                        });
+
+                        if (resultData == null) { return; }
+
+                        Map<String, dynamic> signinJson = resultData['signin'];
+
+                        var userData = UserData.fromJSON(signinJson);
+                        var userDataModel = Provider.of<UserDataModel>(context);
+
+                        userDataModel
+                          ..update(userData)
+                          ..setAuthenticated(true)
+                          ..saveToFile(signinJson);
+
+                        Credentials(email: email, password: password).saveToFile();
+
+                        Provider.of<HttpClientsModel>(context).setToken(userData.token);
+
+                        Navigator.of(context).pop();
+                      },
+                      update: (Cache cache, QueryResult result) {
+                        setState(() {
+                          _isLoading = false;
+                        });
+
+                        if (result.hasErrors) {
+                          for (var error in result.errors) {
+                            Scaffold.of(context)
+                              .showSnackBar(
+                                SnackBar(
+                                  backgroundColor: ThemeColor.validation,
+                                  content: Text(
+                                    '${error.message}',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              );
+                          }
+                        }
+                      },
+                    ),
+                  ],
                 ),
-              ],
+              )
             ),
-          )
+
+            if (_isLoading)
+              LoadingComponent(title: 'Creating your account...'),
+          ],
         ),
       ],
     );
