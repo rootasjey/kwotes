7979cc1033fe8fe8f15aca7862776181426c6b95
rootasjey <jeremiecorpinot@outlook.com>
2019-12-07 19:18:47 +0100

    👆Update success color

--- lib/types/colors.dart
+++ lib/types/colors.dart
@@ -5,6 +5,6 @@ class ThemeColor {
   static Color primary = Color(0xFF706FD3);
   static Color secondary = Color(0xFFF56498);
   static Color validation = Color(0xFF02ECC7);
-  static Color success = Color(0xFF02ECC7);
+  static Color success = Color(0xFF2ECC71);
   static Color error = Color(0xFFE74C3C);
 }
