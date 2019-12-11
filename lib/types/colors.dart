417e63fa872ee534f5a7d1e6c4aa018a32ac9d2c
rootasjey <jeremiecorpinot@outlook.com>
2019-12-11 21:55:50 +0100

    ➕Add lighten & darken colors functions

--- lib/types/colors.dart
+++ lib/types/colors.dart
@@ -29,4 +29,22 @@ class ThemeColor {
         return primary;
     }
   }
+
+  static Color lighten(Color color, [double amount = .1]) {
+    assert(amount >= 0 && amount <= 1);
+
+    final hsl = HSLColor.fromColor(color);
+    final hslLight = hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0));
+
+    return hslLight.toColor();
+  }
+
+  static Color darken(Color color, [double amount = .1]) {
+    assert(amount >= 0 && amount <= 1);
+
+    final hsl = HSLColor.fromColor(color);
+    final hslDark = hsl.withLightness((hsl.lightness - amount).clamp(0.0, 1.0));
+
+    return hslDark.toColor();
+  }
 }
