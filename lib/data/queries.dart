fa1681b2bc3ec6c72eff1af6f50b65041729d3c4
rootasjey <jeremiecorpinot@outlook.com>
2019-12-19 15:33:41 +0100

    👆Avoid refreshing topic chips every page change

--- lib/data/queries.dart
+++ lib/data/queries.dart
@@ -2,7 +2,7 @@ import 'package:gql/language.dart';
 import 'package:gql/ast.dart';
 
 class QuoteQueries {
-    static DocumentNode queryTempQuotes = parseString("""
+  static DocumentNode queryTempQuotes = parseString("""
     query (\$lang: String, \$limit: Float, \$order: Float, \$skip: Float) {
       tempQuotes (lang: \$lang, limit: \$limit, order: \$order, skip: \$skip) {
         pagination {
@@ -19,4 +19,32 @@ class QuoteQueries {
     }
   """);
 
+  static DocumentNode topics = parseString("""
+    query {
+      randomTopics
+    }
+  """);
+}
+
+class QuotidianQueries {
+  static DocumentNode quotidians = parseString("""
+    query Quotidian {
+      quotidian {
+        id
+        quote {
+          author {
+            id
+            name
+          }
+          id
+          name
+          references {
+            id
+            name
+          }
+          topics
+        }
+      }
+    }
+  """);
 }
--- lib/screens/topics.dart
+++ lib/screens/topics.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:gql/language.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:memorare/components/error.dart';
 import 'package:memorare/components/loading.dart';
+import 'package:memorare/data/queries.dart';
+import 'package:memorare/models/http_clients.dart';
 import 'package:memorare/screens/quotes_by_topics.dart';
 import 'package:memorare/types/colors.dart';
+import 'package:provider/provider.dart';
+
+List<String> _topicsList = [];
 
 class Topics extends StatefulWidget {
   @override
@@ -12,89 +16,178 @@ class Topics extends StatefulWidget {
 }
 
 class _TopicsState extends State<Topics> {
+  List<String> topicsList = [];
+  bool isLoading = false;
+  bool hasExceptions = false;
+  String exceptionMessage =  '';
+
+  @override
+  didChangeDependencies () {
+    super.didChangeDependencies();
+
+    setState(() {
+      isLoading = true;
+    });
+
+    if (_topicsList.length > 0) {
+      setState(() {
+        topicsList = _topicsList;
+        isLoading = false;
+      });
+
+    } else {
+      fetchTopics()
+        .then((topics) {
+          setState(() {
+            topicsList = topics;
+            isLoading = false;
+          });
+        });
+    }
+  }
+
+  @override
+  dispose() {
+    _topicsList = topicsList;
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Query(
-      options: QueryOptions(
-        documentNode: parseString(queryTopics()),
-      ),
-      builder: (QueryResult result, { VoidCallback refetch, FetchMore fetchMore }) {
-        if (result.hasException) {
-          return ErrorComponent(
-            description: result.exception.graphqlErrors.first.message,
-            title: 'topics',
-          );
-        }
+    List<Widget> topicChips = [];
 
-        if (result.loading) {
-          return LoadingComponent(
-            title: 'Loading topics...',
-            padding: EdgeInsets.all(30.0),
-          );
-        }
+    for (var topic in topicsList) {
+      final chipColor = ThemeColor.topicColor(topic);
 
-        List<Widget> topicChips = [];
-
-        Map<String, dynamic> json = result.data;
-        for (var topic in json['randomTopics']) {
-          final chipColor = ThemeColor.topicColor(topic);
-
-          topicChips.add(
-            Padding(
-              padding: EdgeInsets.all(5.0),
-              child: ActionChip(
-                backgroundColor: chipColor,
-                labelPadding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return QuotesByTopics(topic: topic,);
-                      }
-                    )
-                  );
-                },
-                label: Text(
-                  topic,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                  ),
-                ),
+      topicChips.add(
+        Padding(
+          padding: EdgeInsets.all(5.0),
+          child: ActionChip(
+            backgroundColor: chipColor,
+            labelPadding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) {
+                    return QuotesByTopics(topic: topic,);
+                  }
+                )
+              );
+            },
+            label: Text(
+              topic,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
               ),
-            )
-          );
-        }
-
-        return Scaffold(
-          body: ListView(
-            padding: EdgeInsets.all(20.0),
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 40.0),
-                child: Text(
-                  'Topics',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 40.0),
-                ),
-              ),
-
-              Wrap(
-                alignment: WrapAlignment.center,
-                children: topicChips,
-              ),
-            ],
+            ),
           ),
-        );
-      },
+        )
+      );
+    }
+
+    return Scaffold(
+      body: ListView(
+        padding: EdgeInsets.all(20.0),
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 40.0),
+            child: Text(
+              'Topics',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 40.0),
+            ),
+          ),
+
+          if (isLoading)
+            LoadingComponent(),
+
+          if (!isLoading && hasExceptions)
+            ErrorComponent(description: exceptionMessage, title: 'Topics',),
+
+          if (topicChips.length == 0)
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 60.0),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    'Sorry, no topic could be retrieved for now.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.only(top: 40.0),
+                    child: RaisedButton(
+                      onPressed: () {
+                        setState(() {
+                          isLoading = true;
+                        });
+
+                        fetchTopics()
+                          .then((topics) {
+                            setState(() {
+                              topicsList = topics;
+                              isLoading = false;
+                            });
+                          });
+                      },
+                      child: Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text(
+                          'Try again',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+          if (isLoading == false && hasExceptions == false && topicChips.length > 0)
+            Wrap(
+              alignment: WrapAlignment.center,
+              children: topicChips,
+            ),
+        ],
+      ),
     );
   }
 
-  String queryTopics() {
-    return """
-      query {
-        randomTopics
+  Future<List<String>> fetchTopics() {
+    final httpClientModel = Provider.of<HttpClientsModel>(context);
+
+    return httpClientModel.defaultClient.value.mutate(MutationOptions(
+      documentNode: QuoteQueries.topics,
+    ))
+    .then((queryResult) {
+      List<String> topics = [];
+
+      if (queryResult.hasException) {
+        hasExceptions = true;
+
+        exceptionMessage = queryResult.exception.graphqlErrors.length > 0 ?
+          queryResult.exception.graphqlErrors.first.message :
+          queryResult.exception.clientException.message;
+
+        return topics;
       }
-    """;
+
+      final json = queryResult.data;
+
+      for (var str in json['randomTopics']) {
+        topics.add(str);
+      }
+
+      return topics;
+    })
+    .catchError((error) {
+      hasExceptions = true;
+      exceptionMessage = error.toString();
+      return List<String>();
+    });
   }
-}
+ }
