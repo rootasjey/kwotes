fa7b283750095b2ae92706cbda1d0c7a748096d8
rootasjey <jeremiecorpinot@outlook.com>
2019-12-22 00:24:37 +0100

    👆Update FilterFab style (w/ chips)

--- lib/components/filter_fab.dart
+++ lib/components/filter_fab.dart
@@ -15,67 +15,85 @@ class FilterFab extends StatefulWidget {
 class _FilterFabState extends State<FilterFab> {
   @override
   Widget build(BuildContext context) {
+    final accent = Provider.of<ThemeColor>(context).accent;
+
     return FloatingActionButton(
       onPressed: () {
-        showModalBottomSheet(
-          context: context,
-          builder: (BuildContext context) {
-            return Container(
-              padding: EdgeInsets.all(20.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Wrap(
-                    alignment: WrapAlignment.center,
-                    children: <Widget>[
-                      SizedBox(
-                        width: 100.0,
-                        height: 70.0,
-                        child: InkWell(
-                          onTap: () {
-                            Navigator.pop(context);
-
-                            if (widget.onOrderChanged != null) {
-                              final newOrder = widget.order > 0 ? -1 : 1;
-                              widget.onOrderChanged(newOrder);
-                            }
-                           },
-                          child: Column(
-                            children: <Widget>[
-                              Image(
-                                width: 40.0,
-                                height: 40.0,
-                                image: widget.order > 0 ?
-                                AssetImage('assets/images/sort_up.png') :
-                                AssetImage('assets/images/sort_down.png'),
-                                color: Colors.black54,
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(top: 10.0),
-                                child: Text(
-                                  widget.order > 0 ? 'Last to First' : 'First to Last',
-                                  style: TextStyle(
-                                    color: Colors.black54,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        )
-                      ),
-                    ],
-                  ),
-                ],
-              )
-            );
-          }
-        );
+        showFilterFabModal();
       },
       child: Icon(Icons.filter_list),
-      backgroundColor: Provider.of<ThemeColor>(context).accent,
+      backgroundColor: accent,
       foregroundColor: Colors.white,
     );
+  }
+
+void showFilterFabModal() {
+  final themeColor = Provider.of<ThemeColor>(context);
+  final accent = themeColor.accent;
+
+  showModalBottomSheet(
+    context: context,
+    builder: (BuildContext context) {
+      return Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Wrap(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ChoiceChip(
+                    label: Text(
+                      'First to last',
+                      style: TextStyle(
+                        color: themeColor.blackOrWhite,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(5.0),
+                    selected: widget.order == 1,
+                    selectedColor: accent,
+                    onSelected: (selected) {
+                      if (widget.onOrderChanged != null) {
+                        final newOrder = widget.order > 0 ? -1 : 1;
+                        widget.onOrderChanged(newOrder);
+                      }
+
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ChoiceChip(
+                    label: Text(
+                      'Last to first',
+                      style: TextStyle(
+                        color: themeColor.blackOrWhite,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(5.0),
+                    selected: widget.order == -1,
+                    selectedColor: accent,
+                    onSelected: (selected) {
+                      if (widget.onOrderChanged != null) {
+                        final newOrder = widget.order > 0 ? -1 : 1;
+                        widget.onOrderChanged(newOrder);
+                      }
+
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+
+              ],
+            ),
+          ],
+        )
+      );
+    }
+  );
   }
 }
