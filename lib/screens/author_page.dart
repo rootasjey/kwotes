6e16ab205e5293c4d20f69827220b5d9d4e609f2
rootasjey <jeremiecorpinot@outlook.com>
2019-12-23 11:41:15 +0100

    📦Refactor author page

--- lib/screens/author_page.dart
+++ lib/screens/author_page.dart
@@ -25,7 +25,6 @@ class _AuthorPageState extends State<AuthorPage> {
   List<Quote> quotes = [];
   bool areQuotesLoading = false;
   bool hasQuotesErrors = false;
-  bool areQuotesLoaded = false;
 
   bool isLoading = false;
   bool hasErrors = false;
@@ -42,19 +41,15 @@ class _AuthorPageState extends State<AuthorPage> {
     return Scaffold(
       body: Builder(builder: (BuildContext context) {
         if (!isLoading && hasErrors) {
-          return Scaffold(
-            body: ErrorComponent(
-              description: error != null ? error.toString() : '',
-            ),
+          return ErrorComponent(
+            description: error != null ? error.toString() : '',
           );
         }
 
         if (isLoading) {
-          return Scaffold(
-            body: LoadingComponent(
-              title: 'Loading ${widget.authorName}...',
-              padding: EdgeInsets.all(30.0),
-            ),
+          return LoadingComponent(
+            title: 'Loading ${widget.authorName}...',
+            padding: EdgeInsets.all(30.0),
           );
         }
 
@@ -100,87 +95,15 @@ class _AuthorPageState extends State<AuthorPage> {
                     alignment: AlignmentDirectional.center,
                     child: Column(
                       children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.only(top: 20.0),
-                          child: InkWell(
-                            onTap: () {
-                              if (author.imgUrl == null) { return; }
+                        avatar(),
 
-                              showDialog(
-                                context: context,
-                                barrierDismissible: true,
-                                builder: (BuildContext context) {
-                                  return AlertDialog(
-                                    content: Container(
-                                      child: Image(image: NetworkImage(author.imgUrl),),
-                                    ),
-                                  );
-                                }
-                              );
-                            },
-                            child: CircleAvatar(
-                              radius: 90.0,
-                              backgroundColor: ThemeColor.primary,
-                              backgroundImage: author.imgUrl != null ?
-                                NetworkImage(author.imgUrl) :
-                                AssetImage('assets/images/monk.png'),
-                            ),
-                          )
-                        ),
+                        name(),
 
-                        Padding(
-                          padding: EdgeInsets.only(top: 30.0),
-                          child: Text(
-                            author.name,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              fontSize: 25.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
+                        job(themeColor.background),
 
-                        Padding(
-                          padding: EdgeInsets.only(top: 10.0),
-                          child: Text(
-                            author.job,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              color: themeColor.background,
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
+                        summary(),
 
-                        Padding(
-                          padding: EdgeInsets.only(top: 60.0),
-                          child: Text(
-                            author.summary,
-                            textAlign: TextAlign.justify,
-                            style: TextStyle(
-                              fontSize: 22.0,
-                              fontWeight: FontWeight.w100,
-                              height: 1.5,
-                            ),
-                          ),
-                        ),
-
-                        if (author.wikiUrl != null && author.wikiUrl.length > 0)
-                          ButtonLink(
-                            icon: Icon(IconsMore.wikipedia_w, color: Colors.white,),
-                            padding: EdgeInsets.only(top: 60.0),
-                            text: 'Open Wikipedia',
-                            url: author.wikiUrl,
-                          ),
-
-                        if (author.url != null && author.url.length > 0)
-                          ButtonLink(
-                            icon: Icon(IconsMore.earth, color: Colors.white),
-                            padding: EdgeInsets.only(top: 10.0),
-                            text: 'Open website',
-                            url: author.url,
-                          ),
+                        buttonsLinks(),
 
                         Padding(padding: EdgeInsets.only(top: 40.0),),
 
@@ -189,46 +112,157 @@ class _AuthorPageState extends State<AuthorPage> {
 
                         Divider(),
 
-                        if (quotes.length > 0)
-                          Padding(
-                            padding: EdgeInsets.only(top: 40.0),
-                            child: SizedBox(
-                              height: 330.0,
-                              child: Swiper(
-                                itemCount: quotes.length,
-                                itemBuilder: (BuildContext context, int index) {
-                                  return Center(
-                                    child: MediumQuoteCard(quote: quotes.elementAt(index),),
-                                  );
-                                },
-                              ),
-                            ),
-                          ),
+                        authorQuotes(),
                       ],
                     ),
                   ),
 
-                  Positioned(
-                    left: 0.0,
-                    top: 0.0,
-                    child: Material(
-                      color: Colors.transparent,
-                      child: IconButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        icon: Icon(
-                          Icons.arrow_back,
-                        ),
-                      ),
-                    )
-                  ),
+                  backButton(),
                 ],
               ),
             ],
           ),
         );
       }),
+    );
+  }
+
+  Widget avatar() {
+    return Padding(
+      padding: EdgeInsets.only(top: 20.0),
+      child: InkWell(
+        onTap: () {
+          if (author.imgUrl == null) { return; }
+
+          showDialog(
+            context: context,
+            barrierDismissible: true,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                content: Container(
+                  child: Image(image: NetworkImage(author.imgUrl),),
+                ),
+              );
+            }
+          );
+        },
+        child: CircleAvatar(
+          radius: 90.0,
+          backgroundColor: ThemeColor.primary,
+          backgroundImage: author.imgUrl != null ?
+            NetworkImage(author.imgUrl) :
+            AssetImage('assets/images/monk.png'),
+        ),
+      )
+    );
+  }
+
+  Widget name() {
+    return Padding(
+      padding: EdgeInsets.only(top: 30.0),
+      child: Text(
+        author.name,
+        overflow: TextOverflow.ellipsis,
+        style: TextStyle(
+          fontSize: 25.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget job(Color color) {
+    return Padding(
+      padding: EdgeInsets.only(top: 10.0),
+      child: Text(
+        author.job,
+        overflow: TextOverflow.ellipsis,
+        style: TextStyle(
+          color: color,
+          fontSize: 18.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget summary() {
+    return Padding(
+      padding: EdgeInsets.only(top: 60.0),
+      child: Text(
+        author.summary,
+        textAlign: TextAlign.justify,
+        style: TextStyle(
+          fontSize: 22.0,
+          fontWeight: FontWeight.w100,
+          height: 1.5,
+        ),
+      ),
+    );
+  }
+
+  Widget buttonsLinks() {
+    final wikiUrlDefined = author.wikiUrl != null && author.wikiUrl.length > 0;
+    final urlDefined = author.url != null && author.url.length > 0;
+
+    return Column(
+      children: <Widget>[
+        if (wikiUrlDefined || urlDefined)
+          Padding(padding: EdgeInsets.only(top: 50),),
+
+        if (wikiUrlDefined)
+          ButtonLink(
+            icon: Icon(IconsMore.wikipedia_w, color: Colors.white,),
+            padding: EdgeInsets.only(top: 10.0),
+            text: 'Open Wikipedia',
+            url: author.wikiUrl,
+          ),
+
+        if (urlDefined)
+          ButtonLink(
+            icon: Icon(IconsMore.earth, color: Colors.white),
+            padding: EdgeInsets.only(top: 10.0),
+            text: 'Open website',
+            url: author.url,
+          ),
+      ],
+    );
+  }
+
+  Widget authorQuotes() {
+    return quotes.length > 0 ?
+      Padding(
+        padding: EdgeInsets.only(top: 40.0),
+        child: SizedBox(
+          height: 330.0,
+          child: Swiper(
+            itemCount: quotes.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Center(
+                child: MediumQuoteCard(quote: quotes.elementAt(index),),
+              );
+            },
+          ),
+        ),
+      ) :
+      Padding(padding: EdgeInsets.zero);
+  }
+
+  Widget backButton() {
+    return Positioned(
+      left: 0.0,
+      top: 0.0,
+      child: Material(
+        color: Colors.transparent,
+        child: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: Icon(
+            Icons.arrow_back,
+          ),
+        ),
+      )
     );
   }
 
