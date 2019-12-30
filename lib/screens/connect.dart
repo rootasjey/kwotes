95340f6889acbce3a7d88f65fc867b8daa7f41c0
rootasjey <jeremiecorpinot@outlook.com>
2019-12-30 09:51:33 +0100

    👆Update `connect` page layout

--- lib/screens/connect.dart
+++ lib/screens/connect.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:memorare/screens/signin.dart';
 import 'package:memorare/screens/signup.dart';
+import 'package:memorare/types/colors.dart';
+import 'package:provider/provider.dart';
 
 class Connect extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final accent = Provider.of<ThemeColor>(context).accent;
+
     return DefaultTabController(
       length: 2,
       child: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.transparent,
           bottom: TabBar(
             indicatorColor: Colors.white,
             labelStyle: TextStyle(fontWeight: FontWeight.bold),
@@ -17,7 +22,19 @@ class Connect extends StatelessWidget {
               Tab(text: 'SIGN UP',)
             ],
           ),
-          title: Text('Connect'),
+          title: Text(
+            'Connect',
+            style: TextStyle(
+              color: accent,
+              fontSize: 30.0,
+            ),
+          ),
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: Icon(Icons.arrow_back, color: accent,),
+          ),
         ),
         body: TabBarView(
           children: <Widget>[
