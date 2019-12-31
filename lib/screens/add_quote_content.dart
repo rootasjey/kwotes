d1dea7d5707097b04281e708f6092ff24353eb26
rootasjey <jeremiecorpinot@outlook.com>
2019-12-31 13:23:57 +0100

    ➕Add back button on add quote

--- lib/screens/add_quote_content.dart
+++ lib/screens/add_quote_content.dart
@@ -27,103 +27,119 @@ class _AddQuoteContentState extends State<AddQuoteContent> {
 
   @override
   Widget build(BuildContext context) {
-    final color = Provider.of<ThemeColor>(context).accent;
+    final accent = Provider.of<ThemeColor>(context).accent;
 
     return ListView(
       children: <Widget>[
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+        Stack(
           children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 60.0),
-              child: Text(
-                'Add content',
-                style: TextStyle(
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Text(
-              '${widget.step}/${widget.maxSteps}',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            Padding(
-              padding: EdgeInsets.only(left: 40.0, right: 40.0, top: 40.0),
-              child: TextField(
-                maxLines: null,
-                autofocus: true,
-                controller: _nameController,
-                keyboardType: TextInputType.multiline,
-                onChanged: (newValue) {
-                  name = newValue;
-                  AddQuoteInputs.name = newValue;
-                },
-                decoration: InputDecoration(
-                  hintText: 'Type your quote there',
-                  border: OutlineInputBorder(),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: color,
-                      width: 2.0,
-                    )
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 60.0),
+                  child: Text(
+                    'Add content',
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-            ),
 
-            Padding(
-              padding: EdgeInsets.only(right: 25.0, bottom: 60.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  FlatButton(
-                    onPressed: () {
-                      AddQuoteInputs.clearQuoteName();
-                      _nameController.clear();
+                Text(
+                  '${widget.step}/${widget.maxSteps}',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.only(left: 40.0, right: 40.0, top: 40.0),
+                  child: TextField(
+                    maxLines: null,
+                    autofocus: true,
+                    controller: _nameController,
+                    keyboardType: TextInputType.multiline,
+                    onChanged: (newValue) {
+                      name = newValue;
+                      AddQuoteInputs.name = newValue;
                     },
-                    child: Opacity(
-                      opacity: 0.6,
-                      child: Text(
-                        'Clear quote content',
+                    decoration: InputDecoration(
+                      hintText: 'Type your quote there',
+                      border: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: accent,
+                          width: 2.0,
+                        )
                       ),
-                    )
+                    ),
                   ),
-                ],
-              ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.only(right: 25.0, bottom: 60.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      FlatButton(
+                        onPressed: () {
+                          AddQuoteInputs.clearQuoteName();
+                          _nameController.clear();
+                        },
+                        child: Opacity(
+                          opacity: 0.6,
+                          child: Text(
+                            'Clear quote content',
+                          ),
+                        )
+                      ),
+                    ],
+                  ),
+                ),
+
+                langSelect(accent),
+              ],
             ),
 
-            DropdownButton<String>(
-              value: lang,
-              style: TextStyle(
-                color: color,
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-              ),
-              underline: Container(
-                height: 2.0,
-                color: color,
-              ),
-              onChanged: (newValue) {
-                setState(() {
-                  lang = newValue;
-                  AddQuoteInputs.lang = newValue;
-                });
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
               },
-              items: langs.map<DropdownMenuItem<String>>((value) {
-                return DropdownMenuItem(
-                  value: value,
-                  child: Text(value.toUpperCase()),
-                );
-              }).toList(),
+              icon: Icon(Icons.arrow_back),
             ),
           ],
         ),
       ],
+    );
+  }
+
+  Widget langSelect(Color color) {
+    return DropdownButton<String>(
+      value: lang,
+      style: TextStyle(
+        color: color,
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+      ),
+      underline: Container(
+        height: 2.0,
+        color: color,
+      ),
+      onChanged: (newValue) {
+        setState(() {
+          lang = newValue;
+          AddQuoteInputs.lang = newValue;
+        });
+      },
+      items: langs.map<DropdownMenuItem<String>>((value) {
+        return DropdownMenuItem(
+          value: value,
+          child: Text(value.toUpperCase()),
+        );
+      }).toList(),
     );
   }
 }
