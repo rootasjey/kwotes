385e77dcf7f2605fea9c993498f5c860344af1f0
rootasjey <jeremiecorpinot@outlook.com>
2020-01-07 13:01:32 +0100

    ➕Add `DraftsResponse` class

---
+++ lib/types/drafts_response.dart
@@ -0,0 +1,24 @@
+import 'package:memorare/types/pagination.dart';
+import 'package:memorare/types/temp_quote.dart';
+
+class DraftsResponse {
+  Pagination pagination;
+  List<TempQuote> entries;
+
+  DraftsResponse({this.entries, this.pagination});
+
+  factory DraftsResponse.fromJSON(Map<String, dynamic> json) {
+    List<TempQuote> drafts = [];
+
+    if (json['entries'] != null) {
+      for (var quote in json['entries']) {
+        drafts.add(TempQuote.fromJSON(quote));
+      }
+    }
+
+    return DraftsResponse(
+      pagination: json['pagination'] != null ? Pagination.fromJSON(json['pagination']) : null,
+      entries: drafts,
+    );
+  }
+}
