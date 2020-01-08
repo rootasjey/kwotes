fc793d8ede45206e8f3bee1e98fb558c93a68eba
rootasjey <jeremiecorpinot@outlook.com>
2020-01-08 09:41:28 +0100

    ➕Add pagination on quotes_list

--- lib/types/quotes_list.dart
+++ lib/types/quotes_list.dart
@@ -1,31 +1,40 @@
+import 'package:memorare/types/pagination.dart';
 import 'package:memorare/types/quote.dart';
 
 class QuotesList {
   final String id;
   String name;
   String description;
+  Pagination pagination;
   final List<Quote> quotes;
 
   QuotesList({
     this.description,
     this.id,
     this.name,
+    this.pagination,
     this.quotes
   });
 
   factory QuotesList.fromJSON(Map<String, dynamic> json) {
     List<Quote> _quotes = [];
+    Pagination _pagination;
 
     if (json['quotes'] != null) {
       for (var jsonQuote in json['quotes']['entries']) {
         _quotes.add(Quote.fromJSON(jsonQuote));
       }
+
+      _pagination = json['quotes']['pagination'] != null ?
+        Pagination.fromJSON(json['quotes']['pagination']) :
+        null;
     }
 
     return QuotesList(
       id: json['id'],
       description: json['description'],
       name: json['name'],
+      pagination: _pagination,
       quotes: _quotes,
     );
   }
