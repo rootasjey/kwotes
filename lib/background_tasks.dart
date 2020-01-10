9e229bc417741c622a0066d08a85a799e30033a2
rootasjey <jeremiecorpinot@outlook.com>
2020-01-10 20:16:53 +0100

    ➕Add background task (debug)

---
+++ lib/background_tasks.dart
@@ -0,0 +1,70 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:gql/language.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:memorare/types/quotidian.dart';
+import 'package:path_provider/path_provider.dart';
+
+class BackgroundTasks {
+  static Future<Quotidian> fetchQuotidian({String url, String apiKey}) async {
+    final HttpLink httpLink = HttpLink(
+        uri: url,
+        headers: {
+          'apikey': apiKey,
+        },
+      );
+
+    final client = ValueNotifier(
+      GraphQLClient(
+        cache: InMemoryCache(),
+        link: httpLink,
+        ),
+    );
+
+    try {
+      final response = await client.value.query(
+        QueryOptions(
+          documentNode: parseString("""
+            query {
+              quotidians (limit: 1) {
+                entries {
+                  id
+                  quote {
+                    author {
+                      name
+                    }
+                    id
+                    name
+                    references {
+                      id
+                      name
+                    }
+                    topics
+                  }
+                }
+              }
+            }
+          """),
+          fetchPolicy: FetchPolicy.networkOnly,
+        )
+      );
+
+      return Quotidian.fromJSON(response.data['quotidians']['entries'][0]);
+
+    } catch (e) {
+      return null;
+    }
+  }
+
+  static Future saveQuotidian({Quotidian quotidian}) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = '${directory.path}/quotidian';
+    final file = File(path);
+
+    final json = quotidian.toJSON();
+    final str = jsonEncode(json);
+    file.writeAsStringSync(str);
+  }
+}
