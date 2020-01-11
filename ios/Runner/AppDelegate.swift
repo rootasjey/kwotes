70b5ca568cbad8155e92783900d694823ba05197
rootasjey <jeremiecorpinot@outlook.com>
2020-01-11 20:39:40 +0100

    ➕Add iOS background tasks setup (👆swift 4.2)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,18 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+
+    if(!UserDefaults.standard.bool(forKey: "Notification")) {
+      UIApplication.shared.cancelAllLocalNotifications()
+      UserDefaults.standard.set(true, forKey: "Notification")
+    }
+
+    UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60*15))
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
