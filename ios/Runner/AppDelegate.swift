5470214f1a455d912c75c3979c4984da4b2170e0
rootasjey <jeremiecorpinot@outlook.com>
2020-01-16 04:07:42 +0100

    ➖Delete background fetch capability (+unused code)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import workmanager
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,20 +7,12 @@ import workmanager
     static func registerPlugins(with registry: FlutterPluginRegistry) {
         GeneratedPluginRegistrant.register(with: registry)
     }
-    
+
     override func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         GeneratedPluginRegistrant.register(with: self)
-        
-        WorkmanagerPlugin.setPluginRegistrantCallback { registry in
-            // The following code will be called upon WorkmanagerPlugin's registration.
-            // Note : all of the app's plugins may not be required in this context ;
-            // instead of using GeneratedPluginRegistrant.register(with: registry),
-            // you may want to register only specific plugins.
-            GeneratedPluginRegistrant.register(with: registry)
-        }
 
         if #available(iOS 10.0, *) {
             UNUserNotificationCenter.current().delegate = self
