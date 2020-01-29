20e91872fd7e67e8a6b6aaef009f84e40e700184
rootasjey <jeremiecorpinot@outlook.com>
2020-01-29 23:47:51 +0100

    👆Update flutter config.

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/usr/local/Caskroom/flutter/latest/flutter"
+export "FLUTTER_ROOT=/Users/jeremiecorpinot/dev/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/jeremiecorpinot/GitHub/memorare/mobile"
-export "FLUTTER_TARGET=/Users/jeremiecorpinot/GitHub/memorare/mobile/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/usr/local/Caskroom/flutter/latest/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/jeremiecorpinot/dev/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "TRACK_WIDGET_CREATION=true"
