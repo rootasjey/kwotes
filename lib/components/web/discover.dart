29bf2f8cdc98e7c60cd5fbe0c230a1575537b5ba
rootasjey <jeremiecorpinot@outlook.com>
2020-02-02 01:46:04 +0100

    ➕(web) Add dynamic data to Discover

--- lib/components/web/discover.dart
+++ lib/components/web/discover.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:memorare/components/web/discover_card.dart';
+import 'package:memorare/components/web/firestore_app.dart';
+import 'package:memorare/types/author.dart';
+import 'package:memorare/types/reference.dart';
 
 class Discover extends StatefulWidget {
   @override
@@ -7,8 +10,23 @@ class Discover extends StatefulWidget {
 }
 
 class _DiscoverState extends State<Discover> {
+  List<Reference> references = [];
+  List<Author> authors = [];
+
+  bool isLoading = false;
+
+  @override
+  initState() {
+    super.initState();
+
+    if (references.length > 0) { return; }
+    fetchAuthorsAndReferences();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final cards = createCards();
+
     return SizedBox(
       height: 600.0,
       child: Column(
@@ -47,26 +65,91 @@ class _DiscoverState extends State<Discover> {
               ),
               scrollDirection: Axis.horizontal,
               shrinkWrap: true,
-              children: <Widget>[
-                DiscoverCard(
-                  name: 'The 4-Hour Work Week',
-                  summary: 'A book about taking control of your own life.',
-                ),
-
-                DiscoverCard(
-                  name: 'Nowtech',
-                  summary: 'Nowtech is a french content production about technology and applications. They produce a daily live on YouTube.',
-                ),
-
-                DiscoverCard(
-                  name: 'Le Bourgeois gentilhomme',
-                  summary: "Le Bourgeois gentilhomme is a five-act comédie-ballet — a play intermingled with music, dance and singing — written by Molière, first presented on 14 October 1670 before the court of Louis XIV at the Château of Chambord by Molière's troupe of actors.",
-                ),
-              ],
+              children: cards,
             ),
           )
         ],
       ),
     );
+  }
+
+  List<Widget> createCards() {
+    List<Widget> cards = [];
+
+    for (var reference in references) {
+      cards.add(
+        DiscoverCard(
+          name: reference.name,
+          summary: reference.summary,
+        ),
+      );
+    }
+
+    for (var author in authors) {
+      cards.add(
+        DiscoverCard(
+          name: author.name,
+          summary: author.summary,
+        ),
+      );
+    }
+
+    return cards;
+  }
+
+  void fetchAuthorsAndReferences() async {
+    if (!this.mounted) {
+      return;
+    }
+
+    setState(() {
+      isLoading = true;
+    });
+
+    List<Reference> referencesList = [];
+    List<Author> authorsList = [];
+
+    try {
+      final refsSnapshot = await FirestoreApp.instance
+        .collection("references")
+        .where('lang', '==', 'en')
+        .orderBy('updatedAt', 'desc')
+        .limit(2)
+        .get();
+
+      if (!refsSnapshot.empty) {
+        refsSnapshot.forEach((docSnapshot) {
+          final ref = Reference.fromJSON(docSnapshot.data());
+          referencesList.add(ref);
+        });
+      }
+
+      final authorsSnapshot = await FirestoreApp.instance
+        .collection('authors')
+        .limit(1)
+        .get();
+
+      if (!authorsSnapshot.empty) {
+        authorsSnapshot.forEach((doc) {
+          final author = Author.fromJSON(doc.data());
+          authorsList.add(author);
+        });
+      }
+
+      setState(() {
+        authors = authorsList;
+        references = referencesList;
+
+        isLoading = false;
+      });
+
+    } catch (error, stackTrace) {
+      debugPrint('error => $error');
+      debugPrint(stackTrace.toString());
+
+      setState(() {
+        isLoading = false;
+      });
+    }
   }
 }
