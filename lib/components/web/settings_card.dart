5a25bb85faf8d9f6fe308d013ff0d2014c20361f
rootasjey <jeremiecorpinot@outlook.com>
2020-02-19 09:41:43 +0100

    ➕Add SettingsCard component

---
+++ lib/components/web/settings_card.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class SettingsCard extends StatelessWidget {
+  final String name;
+  final Icon icon;
+  final Function onTap;
+
+  SettingsCard({
+    this.name,
+    this.icon,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: SizedBox(
+        width: 200,
+        height: 250.0,
+        child: Card(
+          child: InkWell(
+            onTap: onTap,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: Opacity(
+                    opacity: .6,
+                    child: icon,
+                  ),
+                ),
+
+                Text(
+                  name,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ),
+      ),
+    );
+  }
+}
