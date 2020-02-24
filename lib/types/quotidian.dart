007418fa26bf8d125a7db4c2fa522ff2cda6a104
rootasjey <jeremiecorpinot@outlook.com>
2020-02-24 11:25:22 +0100

    👆Add new props. to Quotidian class

--- lib/types/quotidian.dart
+++ lib/types/quotidian.dart
@@ -1,23 +1,42 @@
 import 'package:memorare/types/quote.dart';
 
 class Quotidian {
+  final DateTime createdAt;
+  final DateTime date;
   final String id;
+  final String lang;
   final Quote quote;
+  final DateTime updatedAt;
 
-  Quotidian({this.id, this.quote});
+  Quotidian({
+    this.createdAt,
+    this.date,
+    this.id,
+    this.lang,
+    this.quote,
+    this.updatedAt,
+  });
 
   factory Quotidian.fromJSON(Map<String, dynamic> json) {
     return Quotidian(
-      id: json['id'],
-      quote: Quote.fromJSON(json['quote']),
+      createdAt : json['createdAt'],
+      date      : json['date'],
+      id        : json['id'],
+      lang      : json['lang'],
+      quote     : Quote.fromJSON(json['quote']),
+      updatedAt : json['updatedAt'],
     );
   }
 
   Map<String, dynamic> toJSON() {
     Map<String, dynamic> json = Map();
 
-    json['id']    = id;
-    json['quote'] = quote.toJSON();
+    json['createdAt'] = createdAt;
+    json['date']      = date;
+    json['id']        = id;
+    json['lang']      = lang;
+    json['quote']     = quote.toJSON();
+    json['updatedAt'] = updatedAt;
 
     return json;
   }
