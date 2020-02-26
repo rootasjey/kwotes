e8b37e15d489597d1ad12b296f9e2eb4d6d5411f
rootasjey <jeremiecorpinot@outlook.com>
2020-02-26 11:10:46 +0100

    ➕Add nav buttons for `add quote` page

---
+++ lib/screens/web/add_quote_nav_buttons.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class AddQuoteNavButtons extends StatelessWidget {
+  final String prevMessage;
+  final String nextMessage;
+
+  final Function onPrevPressed;
+  final Function onNextPressed;
+
+  AddQuoteNavButtons({
+    this.nextMessage = 'Next',
+    this.prevMessage = 'Previous',
+    this.onNextPressed,
+    this.onPrevPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 100.0, bottom: 300.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: FlatButton(
+              onPressed: onPrevPressed,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(
+                  width: 2.0,
+                ),
+                borderRadius: BorderRadius.circular(2.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Text(
+                  prevMessage,
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: FlatButton(
+              onPressed: onNextPressed,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(
+                  color: Colors.blue,
+                  width: 2.0,
+                ),
+                borderRadius: BorderRadius.circular(2.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Text(
+                  nextMessage,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
