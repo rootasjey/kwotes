9948336d48b64571439c5be6b47e5a7b4ee35f34
rootasjey <jeremiecorpinot@outlook.com>
2020-02-26 15:04:40 +0100

    👆(web) Update help layout

--- lib/screens/web/add_quote_author.dart
+++ lib/screens/web/add_quote_author.dart
@@ -174,38 +174,47 @@ class _AddQuoteAuthorState extends State<AddQuoteAuthor> {
             return Padding(
               padding: const EdgeInsets.all(40.0),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 40.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          'Help',
-                          style: TextStyle(
-                            fontSize: 25.0,
-                          ),
+                  SizedBox(
+                    width: 500.0,
+                    child: Padding(
+                      padding: EdgeInsets.only(bottom: 40.0),
+                      child: Text(
+                        'Help',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 25.0,
                         ),
-                      ],
-                    ),
-                  ),
-
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 20.0),
-                    child: Text(
-                      '- Author information are optional',
-                      style: TextStyle(
-                        fontSize: 17.0,
                       ),
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 20.0),
-                    child: Text(
-                      '- If you select the author\'s name in the dropdown list, other fields can stay empty',
-                      style: TextStyle(
-                        fontSize: 17.0,
+
+                  SizedBox(
+                    width: 500.0,
+                    child: Opacity(
+                      opacity: .6,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 20.0),
+                            child: Text(
+                              '- Author information are optional',
+                              style: TextStyle(
+                                fontSize: 17.0,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 20.0),
+                            child: Text(
+                              '- If you select the author\'s name in the dropdown list, other fields can stay empty',
+                              style: TextStyle(
+                                fontSize: 17.0,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
--- lib/screens/web/add_quote_content.dart
+++ lib/screens/web/add_quote_content.dart
@@ -134,70 +134,100 @@ class _AddQuoteContentState extends State<AddQuoteContent> {
             return Padding(
               padding: EdgeInsets.all(40.0),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 40.0),
-                    child: Text(
-                      'Help',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 25.0,
+                  SizedBox(
+                    width: 500.0,
+                    child: Padding(
+                      padding: EdgeInsets.only(bottom: 40.0),
+                      child: Text(
+                        'Help',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 25.0,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(
+                    width: 500.0,
+                    child: Opacity(
+                      opacity: .6,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 20.0),
+                            child: Text(
+                              '• Only the quote\'s content is required for submission',
+                              style: TextStyle(
+                                fontSize: 17.0,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 20.0),
+                            child: Text(
+                              '• Your quote should be short (<200 characters), catchy and memorable',
+                              style: TextStyle(
+                                fontSize: 17.0,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 20.0),
+                            child: Text(
+                              '• Quotes with an author or a reference are preferred. A reference can be a movie, a book, a song, a game or from any cultural material',
+                              style: TextStyle(
+                                fontSize: 17.0,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 20.0),
+                            child: Text(
+                              '• The moderators can reject, remove or modify your quotes without notice, before or after validation',
+                              style: TextStyle(
+                                fontSize: 17.0,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 20.0),
+                            child: Text(
+                              '• Explicit, offensive and disrespectful words and ideas can be rejected',
+                              style: TextStyle(
+                                fontSize: 17.0,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 20.0),
+                            child: Text(
+                              "• Long press the green 'propose' button at any step to save your quote in drafts",
+                              style: TextStyle(
+                                fontSize: 17.0,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
 
                   Padding(
-                    padding: EdgeInsets.only(bottom: 20.0),
-                    child: Text(
-                      '- Only the quote\'s content is required for submission',
-                      style: TextStyle(
-                        fontSize: 17.0,
+                    padding: const EdgeInsets.only(top: 100.0),
+                    child: FlatButton(
+                      onPressed: () {
+                        FluroRouter.router.pop(context);
+                      },
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(width: 2.0),
+                        borderRadius: BorderRadius.circular(2.0),
                       ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 20.0),
-                    child: Text(
-                      '- Your quote should be short (<200 characters), catchy and memorable',
-                      style: TextStyle(
-                        fontSize: 17.0,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 20.0),
-                    child: Text(
-                      '- Quotes with a reference are preferred. A reference can be a movie, a book, a song, a game or from any cultural material',
-                      style: TextStyle(
-                        fontSize: 17.0,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 20.0),
-                    child: Text(
-                      '- The moderators can reject, remove or modify your quotes without notice, before or after validation',
-                      style: TextStyle(
-                        fontSize: 17.0,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 20.0),
-                    child: Text(
-                      '- Explicit, offensive and disrespectful words and ideas can be rejected',
-                      style: TextStyle(
-                        fontSize: 17.0,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 20.0),
-                    child: Text(
-                      '- Long press the green validation button (with a check mark) at any step to save your quote in drafts',
-                      style: TextStyle(
-                        fontSize: 17.0,
+                      child: Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text('Close'),
                       ),
                     ),
                   ),
