34a565fc929b49062a40b86ab8796b3a8bb81767
rootasjey <jeremiecorpinot@outlook.com>
2020-03-02 11:57:28 +0100

    👆(web) Update `AppIconHeader`

--- lib/components/web/app_icon_header.dart
+++ lib/components/web/app_icon_header.dart
@@ -3,19 +3,29 @@ import 'package:memorare/utils/route_names.dart';
 import 'package:memorare/utils/router.dart';
 
 class AppIconHeader extends StatelessWidget {
+  final EdgeInsetsGeometry padding;
+
+  AppIconHeader({
+    this.padding = const EdgeInsets.symmetric(vertical: 80.0),
+  });
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 80.0),
-      child: InkWell(
-        onTap: () {
-          FluroRouter.router.navigateTo(context, HomeRoute);
-        },
-        borderRadius: BorderRadius.circular(50.0),
-        child: Image(
+      padding: padding,
+      child: Material(
+        elevation: 1.0,
+        shape: CircleBorder(),
+        clipBehavior: Clip.hardEdge,
+        color: Colors.transparent,
+        child: Ink.image(
           image: AssetImage('assets/images/icon-small.png'),
-          width: 90.0,
-          height: 90.0,
+          fit: BoxFit.cover,
+          width: 60.0,
+          height: 60.0,
+          child: InkWell(
+            onTap: () => FluroRouter.router.navigateTo(context, HomeRoute),
+          ),
         ),
       ),
     );
