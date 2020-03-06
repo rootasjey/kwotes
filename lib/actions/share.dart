90596bc96fb6470657803be27130c349ffea3c9f
rootasjey <jeremiecorpinot@outlook.com>
2020-03-06 11:43:33 +0100

    ➕(web) Add share twitter action

---
+++ lib/actions/share.dart
@@ -0,0 +1,17 @@
+import 'package:memorare/types/quote.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+/// Sahre the target quote to twitter.
+Future shareTwitter({Quote quote}) async {
+  final quoteName = quote.name;
+  final authorName = quote.author.name;
+
+  String quoteAndAuthor = '"$quoteName"';
+
+  if (authorName.isNotEmpty) {
+    quoteAndAuthor += ' — $authorName';
+  }
+
+  final url = 'https://twitter.com/intent/tweet?via=memorareapp&text=$quoteAndAuthor';
+  await launch(url);
+}
