c4cbe92f375eb7ff75fbb76cc31b78e984e059d5
rootasjey <jeremiecorpinot@outlook.com>
2020-03-11 00:49:20 +0100

    👌(web) Code format

--- lib/components/web/footer.dart
+++ lib/components/web/footer.dart
@@ -29,201 +29,213 @@ class _FooterState extends State<Footer> {
       child: Wrap(
         alignment: WrapAlignment.spaceAround,
         children: <Widget>[
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(
-                  bottom: 30.0,
-                  left: 15.0
-                ),
-                child: Opacity(
-                  opacity: .5,
-                  child: Text(
-                    'LANGUAGE',
-                    style: TextStyle(
-                      fontSize: 18.0,
-                    ),
-                  ),
-                ),
-              ),
+          languages(),
 
-              FlatButton(
-                onPressed: () async {
-                  Language.setLang(Language.en);
-                  updateUserAccountLang();
-                },
-                child: Opacity(
-                  opacity: .5,
-                  child: Text(
-                    'English',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
-                  ),
-                )
-              ),
+          developers(),
 
-              FlatButton(
-                onPressed: () {
-                  Language.setLang(Language.fr);
-                  updateUserAccountLang();
-                },
-                child: Opacity(
-                  opacity: .5,
-                  child: Text(
-                    'Français',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
-                  ),
-                )
-              ),
-            ],
-          ),
-
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(
-                  bottom: 30.0,
-                  left: 15.0
-                ),
-                child: Opacity(
-                  opacity: .5,
-                  child: Text(
-                    'DEVELOPERS',
-                    style: TextStyle(
-                      fontSize: 18.0,
-                    ),
-                  ),
-                ),
-              ),
-
-              FlatButton(
-                onPressed: null,
-                child: Opacity(
-                  opacity: .5,
-                  child: Text(
-                    'Documentation',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
-                  ),
-                )
-              ),
-
-              FlatButton(
-                onPressed: null,
-                child: Opacity(
-                  opacity: .5,
-                  child: Text(
-                    'API References',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
-                  ),
-                )
-              ),
-
-              FlatButton(
-                onPressed: null,
-                child: Opacity(
-                  opacity: .5,
-                  child: Text(
-                    'API Status',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
-                  ),
-                )
-              ),
-
-              FlatButton(
-                onPressed: () async {
-                  await launch('https://github.com/memorare/app');
-                },
-                child: Opacity(
-                  opacity: .5,
-                  child: Text(
-                    'GitHub',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
-                  ),
-                )
-              ),
-            ],
-          ),
-
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(
-                  bottom: 30.0,
-                  left: 15.0
-                ),
-                child: Opacity(
-                  opacity: .5,
-                  child: Text(
-                    'RESOURCES',
-                    style: TextStyle(
-                      fontSize: 18.0,
-                    ),
-                  ),
-                ),
-              ),
-
-              FlatButton(
-                onPressed: () {
-                  FluroRouter.router.navigateTo(context, AboutRoute);
-                },
-                child: Opacity(
-                  opacity: .5,
-                  child: Text(
-                    'About',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
-                  ),
-                )
-              ),
-
-              FlatButton(
-                onPressed: () {
-                  FluroRouter.router.navigateTo(context, ContactRoute);
-                },
-                child: Opacity(
-                  opacity: .5,
-                  child: Text(
-                    'Contact',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
-                  ),
-                )
-              ),
-
-              FlatButton(
-                onPressed: () {
-                  FluroRouter.router.navigateTo(context, PrivacyRoute);
-                },
-                child: Opacity(
-                  opacity: .5,
-                  child: Text(
-                    'Privacy & Terms',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
-                  ),
-                )
-              ),
-            ],
-          ),
+          resourcesLinks(),
         ],
       ),
+    );
+  }
+
+  Widget developers() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(
+            bottom: 30.0,
+            left: 15.0
+          ),
+          child: Opacity(
+            opacity: .5,
+            child: Text(
+              'DEVELOPERS',
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
+            ),
+          ),
+        ),
+
+        FlatButton(
+          onPressed: null,
+          child: Opacity(
+            opacity: .5,
+            child: Text(
+              'Documentation',
+              style: TextStyle(
+                fontSize: 15.0,
+              ),
+            ),
+          )
+        ),
+
+        FlatButton(
+          onPressed: null,
+          child: Opacity(
+            opacity: .5,
+            child: Text(
+              'API References',
+              style: TextStyle(
+                fontSize: 15.0,
+              ),
+            ),
+          )
+        ),
+
+        FlatButton(
+          onPressed: null,
+          child: Opacity(
+            opacity: .5,
+            child: Text(
+              'API Status',
+              style: TextStyle(
+                fontSize: 15.0,
+              ),
+            ),
+          )
+        ),
+
+        FlatButton(
+          onPressed: () async {
+            await launch('https://github.com/memorare/app');
+          },
+          child: Opacity(
+            opacity: .5,
+            child: Text(
+              'GitHub',
+              style: TextStyle(
+                fontSize: 15.0,
+              ),
+            ),
+          )
+        ),
+      ],
+    );
+  }
+
+  Widget languages() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(
+            bottom: 30.0,
+            left: 15.0
+          ),
+          child: Opacity(
+            opacity: .5,
+            child: Text(
+              'LANGUAGE',
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
+            ),
+          ),
+        ),
+
+        FlatButton(
+          onPressed: () async {
+            Language.setLang(Language.en);
+            updateUserAccountLang();
+          },
+          child: Opacity(
+            opacity: .5,
+            child: Text(
+              'English',
+              style: TextStyle(
+                fontSize: 15.0,
+              ),
+            ),
+          )
+        ),
+
+        FlatButton(
+          onPressed: () {
+            Language.setLang(Language.fr);
+            updateUserAccountLang();
+          },
+          child: Opacity(
+            opacity: .5,
+            child: Text(
+              'Français',
+              style: TextStyle(
+                fontSize: 15.0,
+              ),
+            ),
+          )
+        ),
+      ],
+    );
+  }
+
+  Widget resourcesLinks() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(
+            bottom: 30.0,
+            left: 15.0
+          ),
+          child: Opacity(
+            opacity: .5,
+            child: Text(
+              'RESOURCES',
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
+            ),
+          ),
+        ),
+
+        FlatButton(
+          onPressed: () {
+            FluroRouter.router.navigateTo(context, AboutRoute);
+          },
+          child: Opacity(
+            opacity: .5,
+            child: Text(
+              'About',
+              style: TextStyle(
+                fontSize: 15.0,
+              ),
+            ),
+          )
+        ),
+
+        FlatButton(
+          onPressed: () {
+            FluroRouter.router.navigateTo(context, ContactRoute);
+          },
+          child: Opacity(
+            opacity: .5,
+            child: Text(
+              'Contact',
+              style: TextStyle(
+                fontSize: 15.0,
+              ),
+            ),
+          )
+        ),
+
+        FlatButton(
+          onPressed: () {
+            FluroRouter.router.navigateTo(context, PrivacyRoute);
+          },
+          child: Opacity(
+            opacity: .5,
+            child: Text(
+              'Privacy & Terms',
+              style: TextStyle(
+                fontSize: 15.0,
+              ),
+            ),
+          )
+        ),
+      ],
     );
   }
 
