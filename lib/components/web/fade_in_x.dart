0e425ca1e6f342a387a7a7ca1c12c54e99ad37a2
rootasjey <jeremiecorpinot@outlook.com>
2020-03-12 00:53:17 +0100

    ➕(web) Add fade in animations widgets

---
+++ lib/components/web/fade_in_x.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
+
+/// Animate translateX and opacity of a child widget.
+class FadeInX extends StatelessWidget {
+  final double delay;
+  final Widget child;
+
+  final double beginX;
+  final double endX;
+
+  FadeInX({
+    this.beginX = 0.0,
+    this.child,
+    this.delay = 0.0,
+    this.endX = 0.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final tween = MultiTrackTween([
+      Track('opacity')
+          .add(Duration(milliseconds: 500), Tween(begin: 0.0, end: 1.0)),
+      Track('translateX').add(
+          Duration(milliseconds: 500), Tween(begin: beginX, end: endX),
+          curve: Curves.easeOut)
+    ]);
+
+    return ControlledAnimation(
+      delay: Duration(milliseconds: (300 * delay).round()),
+      duration: tween.duration,
+      tween: tween,
+      child: child,
+      builderWithChild: (context, child, animation) => Opacity(
+        opacity: animation['opacity'],
+        child: Transform.translate(
+          offset: Offset(animation['translateX'], 0),
+          child: child),
+      ),
+    );
+  }
+}
---
+++ lib/components/web/fade_in_y.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
+
+/// Animate translateY and opacity of a child widget.
+class FadeInY extends StatelessWidget {
+  final double delay;
+  final Widget child;
+
+  final double beginY;
+  final double endY;
+
+  FadeInY({
+    this.beginY = 0.0,
+    this.child,
+    this.delay = 0.0,
+    this.endY = 0.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final tween = MultiTrackTween([
+      Track('opacity')
+          .add(Duration(milliseconds: 500), Tween(begin: 0.0, end: 1.0)),
+      Track('translateY').add(
+          Duration(milliseconds: 500), Tween(begin: beginY, end: endY),
+          curve: Curves.easeOut)
+    ]);
+
+    return ControlledAnimation(
+      delay: Duration(milliseconds: (300 * delay).round()),
+      duration: tween.duration,
+      tween: tween,
+      child: child,
+      builderWithChild: (context, child, animation) => Opacity(
+        opacity: animation['opacity'],
+        child: Transform.translate(
+          offset: Offset(animation['translateY'], 0),
+          child: child),
+      ),
+    );
+  }
+}
