565252760d04a98dc0f7452bfd62967c02c8984c
rootasjey <jeremiecorpinot@outlook.com>
2020-03-12 21:41:50 +0100

    ➕(web) Add QuoteCardGridItem widget

---
+++ lib/components/web/quote_card_grid_item.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:memorare/types/font_size.dart';
+import 'package:memorare/types/quote.dart';
+import 'package:memorare/utils/route_names.dart';
+import 'package:memorare/utils/router.dart';
+
+class QuoteCardGridItem extends StatelessWidget {
+  final Quote quote;
+  final Function onLongPress;
+  final PopupMenuButton<String> popupMenuButton;
+
+  QuoteCardGridItem({
+    this.onLongPress,
+    this.popupMenuButton,
+    this.quote,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      margin: EdgeInsets.zero,
+      child: InkWell(
+        onTap: () {
+          FluroRouter.router.navigateTo(
+            context,
+            QuotePageRoute.replaceFirst(':id', quote.id)
+          );
+        },
+        onLongPress: onLongPress,
+        child: Stack(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    quote.name.length > 115 ?
+                      '${quote.name.substring(0, 115)}...' : quote.name,
+                    style: TextStyle(
+                      fontSize: FontSize.gridItem(quote.name),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            if (popupMenuButton != null)
+              Positioned(
+                right: 0,
+                bottom: 0,
+                child: popupMenuButton,
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
