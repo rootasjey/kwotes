a2fe3d72854a72b4caeeb8becdb65bdf5a9369d7
rootasjey <jeremiecorpinot@outlook.com>
2020-03-12 10:56:13 +0100

    👆(web) Update signin w/ animations

--- lib/screens/web/signin.dart
+++ lib/screens/web/signin.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:memorare/components/web/fade_in_y.dart';
 import 'package:memorare/components/web/loading_animation.dart';
 import 'package:memorare/components/web/nav_back_header.dart';
 import 'package:memorare/state/user_connection.dart';
@@ -114,115 +115,134 @@ class _SigninState extends State<Signin> {
   Widget idleContainer() {
     return Column(
       children: <Widget>[
-        Padding(
-          padding: EdgeInsets.only(top: 10.0),
-          child: Text(
-            'Sign in into your existing account.',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-
-        FlatButton(
-          onPressed: () {
-            FluroRouter.router.navigateTo(
-              context,
-              SignupRoute,
-            );
-          },
-          child: Opacity(
-            opacity: .6,
+        FadeInY(
+          beginY: 50.0,
+          child: Padding(
+            padding: EdgeInsets.only(top: 10.0),
             child: Text(
-              "I don't have an account"
+              'Sign in into your existing account.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          )
-        ),
-
-        Padding(
-          padding: EdgeInsets.only(top: 40.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                autofocus: true,
-                decoration: InputDecoration(
-                  icon: Icon(Icons.email),
-                  labelText: 'Email',
-                ),
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value) {
-                  email = value;
-                },
-                onFieldSubmitted: (value) {
-                  FocusScope.of(context).requestFocus(_passwordNode);
-                },
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Email login cannot be empty';
-                  }
-
-                  return null;
-                },
-              ),
-            ],
           ),
         ),
 
-        Padding(
-          padding: EdgeInsets.only(top: 30.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                focusNode: _passwordNode,
-                decoration: InputDecoration(
-                  icon: Icon(Icons.lock_outline),
-                  labelText: 'Password',
-                ),
-                obscureText: true,
-                onChanged: (value) {
-                  password = value;
-                },
-                onFieldSubmitted: (value) {
-                  if (value.length == 0) { return; }
-                  signin();
-                },
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Password login cannot be empty';
-                  }
-
-                  return null;
-                },
-              ),
-            ],
-          ),
-        ),
-
-        Padding(
-          padding: const EdgeInsets.only(top: 60.0),
+        FadeInY(
+          delay: 1,
+          beginY: 50.0,
           child: FlatButton(
             onPressed: () {
-              signin();
+              FluroRouter.router.navigateTo(
+                context,
+                SignupRoute,
+              );
             },
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Text('Sign me in'),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
-                    child: Icon(Icons.arrow_forward),
-                  )
-                ],
+            child: Opacity(
+              opacity: .6,
+              child: Text(
+                "I don't have an account"
+              ),
+            )
+          ),
+        ),
+
+        FadeInY(
+          delay: 1.5,
+          beginY: 50.0,
+          child: Padding(
+            padding: EdgeInsets.only(top: 40.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TextFormField(
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.email),
+                    labelText: 'Email',
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  onFieldSubmitted: (value) {
+                    FocusScope.of(context).requestFocus(_passwordNode);
+                  },
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Email login cannot be empty';
+                    }
+
+                    return null;
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+
+        FadeInY(
+          delay: 2,
+          beginY: 50.0,
+          child: Padding(
+            padding: EdgeInsets.only(top: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TextFormField(
+                  focusNode: _passwordNode,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.lock_outline),
+                    labelText: 'Password',
+                  ),
+                  obscureText: true,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  onFieldSubmitted: (value) {
+                    if (value.length == 0) { return; }
+                    signin();
+                  },
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Password login cannot be empty';
+                    }
+
+                    return null;
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+
+        FadeInY(
+          delay: 2,
+          beginY: 50.0,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 60.0),
+            child: FlatButton(
+              onPressed: () {
+                signin();
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text('Sign me in'),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: Icon(Icons.arrow_forward),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
-        )
+        ),
       ],
     );
   }
