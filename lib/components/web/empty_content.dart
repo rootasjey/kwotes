fb0ccea0accf0c3576b7a27f585817ef044767ee
rootasjey <jeremiecorpinot@outlook.com>
2020-03-16 15:10:49 +0100

    ➕(web) Add EmptyContent component

---
+++ lib/components/web/empty_content.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class EmptyContent extends StatelessWidget {
+  final Widget icon;
+  final String subtitle;
+  final String title;
+  final Function onRefresh;
+
+  EmptyContent({
+    this.icon,
+    this.onRefresh,
+    this.subtitle,
+    this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(40.0),
+      child: Column(
+        children: <Widget>[
+          if (icon != null)
+            icon,
+
+          Opacity(
+            opacity: .8,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Text(
+                title,
+                style: TextStyle(
+                  fontSize: 25.0,
+                ),
+              ),
+            ),
+          ),
+
+          if (subtitle != null)
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: Opacity(
+                opacity: .6,
+                child: Text(
+                  subtitle,
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  ),
+                ),
+              ),
+            ),
+
+          if (onRefresh != null)
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Opacity(
+                opacity: .8,
+                child: FlatButton(
+                  onPressed: onRefresh,
+                  child: Text(
+                    'Refresh',
+                  ),
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
