9984616cc43c550669bdf37beb82508c89149828
rootasjey <jeremiecorpinot@outlook.com>
2020-03-17 14:09:10 +0100

    ➕(web) Add `UserQuotesList` class

---
+++ lib/types/user_quotes_list.dart
@@ -0,0 +1,31 @@
+class UserQuotesList {
+  final DateTime createdAt;
+  String description;
+  final String iconUrl;
+  final String id;
+  String name;
+  bool isPublic;
+  final DateTime updatedAt;
+
+  UserQuotesList({
+    this.createdAt,
+    this.description,
+    this.iconUrl,
+    this.id,
+    this.isPublic,
+    this.name,
+    this.updatedAt,
+  });
+
+  factory UserQuotesList.fromJSON(Map<String, dynamic> json) {
+    return UserQuotesList(
+      createdAt   : json['createdAt'],
+      iconUrl     : json['iconUrl'] ?? '',
+      id          : json['id'],
+      isPublic    : json['isPublic'],
+      description : json['description'] ?? '',
+      name        : json['name'] ?? '',
+      updatedAt   : json['updatedAt'],
+    );
+  }
+}
