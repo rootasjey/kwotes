2ad9ff026ae2c66b0ab0d47bb27d37200ca1eaef
rootasjey <jeremiecorpinot@outlook.com>
2020-03-22 13:52:51 +0100

    💄(web) Update styles of favourites

--- lib/screens/web/favourites.dart
+++ lib/screens/web/favourites.dart
@@ -4,13 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:memorare/actions/favourites.dart';
 import 'package:memorare/actions/share.dart';
 import 'package:memorare/components/web/app_icon_header.dart';
-import 'package:memorare/components/web/empty_flat_card.dart';
+import 'package:memorare/components/web/empty_content.dart';
 import 'package:memorare/components/web/fade_in_y.dart';
 import 'package:memorare/components/web/firestore_app.dart';
 import 'package:memorare/components/web/footer.dart';
-import 'package:memorare/components/web/full_page_loading.dart';
+import 'package:memorare/components/web/loading_animation.dart';
 import 'package:memorare/components/web/nav_back_footer.dart';
 import 'package:memorare/state/topics_colors.dart';
+import 'package:memorare/types/font_size.dart';
 import 'package:memorare/types/quote.dart';
 import 'package:memorare/utils/route_names.dart';
 import 'package:memorare/utils/router.dart';
@@ -74,157 +75,212 @@ class _FavouritesState extends State<Favourites> {
   }
 
   Widget body() {
-    if (isLoading) {
-      return FullPageLoading(
-        title: 'Loading your favourites...',
-      );
-    }
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        final screenWidth = MediaQuery.of(context).size.width;
 
-    if (!isLoading && quotes.length == 0) {
-      return Container(
-        height: MediaQuery.of(context).size.height - 300.0,
-        child:  EmptyFlatCard(
-          onPressed: () => fetchFavQuotes(),
-        ),
-      );
-    }
+        return NotificationListener(
+          onNotification: (ScrollNotification scrollNotif) {
+            // FAB visibility
+            if (scrollNotif.metrics.pixels < 50 && isFabVisible) {
+              setState(() {
+                isFabVisible = false;
+              });
+            } else if (scrollNotif.metrics.pixels > 50 && !isFabVisible) {
+              setState(() {
+                isFabVisible = true;
+              });
+            }
 
-    return listQuotes();
-  }
+            // Load more scenario
+            if (scrollNotif.metrics.pixels < scrollNotif.metrics.maxScrollExtent - 100.0) {
+              return false;
+            }
 
-  Widget listQuotes() {
-    return NotificationListener(
-      onNotification: (ScrollNotification scrollNotif) {
-        // FAB visibility
-        if (scrollNotif.metrics.pixels < 50 && isFabVisible) {
-          setState(() {
-            isFabVisible = false;
-          });
-        } else if (scrollNotif.metrics.pixels > 50 && !isFabVisible) {
-          setState(() {
-            isFabVisible = true;
-          });
-        }
+            if (hasNext && !isLoadingMore) {
+              fetchMoreFavQuotes();
+            }
 
-        // Load more scenario
-        if (scrollNotif.metrics.pixels < scrollNotif.metrics.maxScrollExtent - 100.0) {
-          return false;
-        }
-
-        if (hasNext && !isLoadingMore) {
-          fetchMoreFavQuotes();
-        }
-
-        return false;
-      },
-      child: CustomScrollView(
-        controller: _scrollController,
-        slivers: <Widget>[
-          SliverAppBar(
-            floating: true,
-            snap: true,
-            expandedHeight: 320.0,
-            backgroundColor: Colors.transparent,
-            automaticallyImplyLeading: false,
-            flexibleSpace: Stack(
-              children: <Widget>[
-                Column(
+            return false;
+          },
+          child: CustomScrollView(
+            controller: _scrollController,
+            slivers: <Widget>[
+              SliverAppBar(
+                floating: true,
+                snap: true,
+                expandedHeight: 320.0,
+                backgroundColor: Colors.transparent,
+                automaticallyImplyLeading: false,
+                flexibleSpace: Stack(
                   children: <Widget>[
-                    FadeInY(
-                      beginY: 50.0,
-                      child: AppIconHeader(),
+                    Column(
+                      children: <Widget>[
+                        FadeInY(
+                          beginY: 50.0,
+                          child: AppIconHeader(),
+                        ),
+
+                        FadeInY(
+                          delay: .5,
+                          beginY: 50.0,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                'Favourites',
+                                style: TextStyle(
+                                  fontSize: 30.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
 
-                    FadeInY(
-                      delay: .5,
-                      beginY: 50.0,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            'Favourites',
-                            style: TextStyle(
-                              fontSize: 30.0,
-                            ),
-                          ),
-                        ],
+                    Positioned(
+                      left: 80.0,
+                      top: 50.0,
+                      child: IconButton(
+                        onPressed: () {
+                          FluroRouter.router.pop(context);
+                        },
+                        tooltip: 'Back',
+                        icon: Icon(Icons.arrow_back),
                       ),
                     ),
                   ],
                 ),
+              ),
 
-                Positioned(
-                  left: 80.0,
-                  top: 50.0,
-                  child: IconButton(
-                    onPressed: () {
-                      FluroRouter.router.pop(context);
-                    },
-                    tooltip: 'Back',
-                    icon: Icon(Icons.arrow_back),
+              listContent(screenWidth: screenWidth),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget listContent({double screenWidth}) {
+    if (isLoading) {
+      return SliverList(
+        delegate: SliverChildListDelegate([
+            LoadingAnimation(
+              title: 'Loading your favourites...',
+            ),
+          ]
+        ),
+      );
+    }
+
+    if (quotes.length == 0) {
+      return SliverList(
+        delegate: SliverChildListDelegate([
+            FadeInY(
+              delay: 2.0,
+              beginY: 50.0,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 40.0),
+                child: EmptyContent(
+                  icon: Opacity(
+                    opacity: .8,
+                    child: Icon(
+                      Icons.favorite_border,
+                      size: 60.0,
+                      color: Color(0xFFFF005C),
+                    ),
                   ),
+                  title: "You've no favourites quotes at this moment",
+                  subtitle: 'You can add them with the ❤️ button',
                 ),
-              ],
+              ),
+            ),
+          ]
+        ),
+      );
+    }
+
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          final quote = quotes.elementAt(index);
+
+          return FadeInY(
+            delay: 2.0 + index.toDouble() * 0.1,
+            beginY: 50.0,
+            child: quoteContainer(
+              quote: quote,
+              screenWidth: screenWidth,
+            )
+          );
+        },
+        childCount: quotes.length,
+      ),
+    );
+  }
+
+  Widget quoteContainer({Quote quote, double screenWidth}) {
+    final topicColor = appTopicsColors.find(quote.topics.first);
+
+    return Container(
+      padding: const EdgeInsets.all(80.0),
+      height: MediaQuery.of(context).size.height,
+      child: Column(
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              FluroRouter.router.navigateTo(
+                context,
+                QuotePageRoute.replaceFirst(':id', quote.quoteId),
+              );
+            },
+            child: Text(
+              quote.name,
+              style: TextStyle(
+                fontSize: FontSize.hero(quote.name) / (2000 / screenWidth),
+              ),
             ),
           ),
 
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                final quote = quotes.elementAt(index);
-                final topicColor = appTopicsColors.find(quote.topics.first);
+          topicColor != null ?
+            SizedBox(
+              width: 100.0,
+              child: Divider(
+                color: Color(topicColor.decimal),
+                thickness: 2.0,
+                height: 40.0,
+              )
+            ) :
+            SizedBox(
+              width: 100.0,
+              child: Divider(
+                thickness: 2.0,
+                height: 40.0,
+              ),
+            ),
 
-                return FadeInY(
-                  delay: 2.0 + index.toDouble() * 0.1,
-                  beginY: 50.0,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20.0,
-                      vertical: 40.0,
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        InkWell(
-                          onTap: () {
-                            FluroRouter.router.navigateTo(
-                              context,
-                              QuotePageRoute.replaceFirst(':id', quote.id),
-                            );
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: SizedBox(
-                              width: 400.0,
-                              child: Text(
-                                quote.name,
-                                style: TextStyle(
-                                  fontSize: 25.0,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-
-                        SizedBox(
-                          width: 100.0,
-                          child: Divider(
-                            color: topicColor != null ?
-                              Color(topicColor.decimal) :
-                              Colors.white,
-                            thickness: 2.0,
-                            height: 40.0,
-                          )
-                        ),
-
-                        userActions(quote),
-                      ],
-                    ),
-                  ),
-                );
-              },
-              childCount: quotes.length,
+          GestureDetector(
+            onTap: () {
+              FluroRouter.router.navigateTo(
+                context,
+                AuthorRoute.replaceFirst(':id', quote.author.id),
+              );
+            },
+            child: Opacity(
+              opacity: .6,
+              child: Text(
+                quote.author.name,
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
             ),
           ),
+
+          Padding(padding: const EdgeInsets.only(top: 15.0),),
+
+          userActions(quote),
         ],
       ),
     );
