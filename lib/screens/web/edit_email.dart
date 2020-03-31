acfd1e5c3baa553b7bbb3a30eca4253c6519076f
rootasjey <jeremiecorpinot@outlook.com>
2020-03-31 18:08:07 +0200

    👌(web) Code format

--- lib/screens/web/edit_email.dart
+++ lib/screens/web/edit_email.dart
@@ -43,145 +43,170 @@ class _EditEmailState extends State<EditEmail> {
 
   Widget body() {
     if (isCompleted) {
-      return SizedBox(
-        width: 600.0,
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 30.0),
-              child: Icon(
-                Icons.check_circle,
-                size: 80.0,
-                color: Colors.green,
-              ),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.only(top: 30.0, bottom: 0.0),
-              child: Text(
-                'Your email has been successfuly updated.',
-                style: TextStyle(
-                  fontSize: 20.0,
-                ),
-              ),
-            ),
-
-            NavBackFooter(),
-          ],
-        ),
-      );
+      return completedScreen();
     }
-    if (isUpdating) {
-      return SizedBox(
-        width: 600.0,
-        child: Column(
-          children: <Widget>[
-            CircularProgressIndicator(),
 
-            Padding(
-              padding: const EdgeInsets.only(top: 40.0),
-              child: Text(
-                'Updating your email...',
-                style: TextStyle(
-                  fontSize: 20.0,
-                ),
-              ),
-            )
-          ],
-        ),
-      );
+    if (isUpdating) {
+      return updatingScreen();
     }
 
     return SizedBox(
       width: 400.0,
       child: Column(
         children: <Widget>[
-          Text(
-            'Update email',
-            style: TextStyle(
-              fontSize: 35.0,
-            ),
-          ),
+          titleText(),
 
-          Padding(
-            padding: const EdgeInsets.only(top: 70.0, bottom: 50.0),
-            child: Image.asset(
-              'assets/images/write-email-${stateColors.iconExt}.png',
-              width: 100.0,
-            ),
-          ),
+          titleImg(),
 
-          Padding(
-            padding: const EdgeInsets.only(top: 80.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(
-                    icon: Icon(Icons.email),
-                    labelText: 'Enter your new email',
-                  ),
-                  onChanged: (value) {
-                    email = value;
-                  },
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'New email cannot be empty';
-                    }
-
-                    return null;
-                  },
-                ),
-              ],
-            ),
-          ),
-
-          Padding(
-            padding: EdgeInsets.only(top: 50.0, bottom: 80.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(
-                    icon: Icon(Icons.lock_outline),
-                    labelText: 'Entering your password',
-                  ),
-                  obscureText: true,
-                  onChanged: (value) {
-                    password = value;
-                  },
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Password login cannot be empty';
-                    }
-
-                    return null;
-                  },
-                ),
-              ],
-            ),
-          ),
-
-          RaisedButton(
-            onPressed: () {
-              updateEmail();
-            },
-            shape: RoundedRectangleBorder(
-              side: BorderSide(
-                color: stateColors.primary,
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Text(
-                'Update',
-              ),
-            )
-          ),
+          validationButton(),
 
           Padding(padding: const EdgeInsets.only(bottom: 200.0),),
         ],
       ),
+    );
+  }
+
+  Widget completedScreen() {
+    return SizedBox(
+      width: 400.0,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 30.0),
+            child: Icon(
+              Icons.check_circle,
+              size: 80.0,
+              color: Colors.green,
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(top: 30.0, bottom: 0.0),
+            child: Text(
+              'Your email has been successfuly updated.',
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+          ),
+
+          NavBackFooter(),
+        ],
+      ),
+    );
+  }
+
+  Widget titleText() {
+    return Text(
+      'Update email',
+      style: TextStyle(
+        fontSize: 35.0,
+      ),
+    );
+  }
+
+  Widget titleImg() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 70.0, bottom: 50.0),
+      child: Image.asset(
+        'assets/images/write-email-${stateColors.iconExt}.png',
+        width: 100.0,
+      ),
+    );
+  }
+
+  Widget emailInput() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 80.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+              icon: Icon(Icons.email),
+              labelText: 'Enter your new email',
+            ),
+            onChanged: (value) {
+              email = value;
+            },
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'New email cannot be empty';
+              }
+
+              return null;
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget passwordInput() {
+    return Padding(
+      padding: EdgeInsets.only(top: 50.0, bottom: 80.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+              icon: Icon(Icons.lock_outline),
+              labelText: 'Entering your password',
+            ),
+            obscureText: true,
+            onChanged: (value) {
+              password = value;
+            },
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Password login cannot be empty';
+              }
+
+              return null;
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget updatingScreen() {
+    return SizedBox(
+      width: 400.0,
+      child: Column(
+        children: <Widget>[
+          CircularProgressIndicator(),
+
+          Padding(
+            padding: const EdgeInsets.only(top: 40.0),
+            child: Text(
+              'Updating your email...',
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget validationButton() {
+    return RaisedButton(
+      onPressed: () {
+        updateEmail();
+      },
+      shape: RoundedRectangleBorder(
+        side: BorderSide(
+          color: stateColors.primary,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Text(
+          'Update',
+        ),
+      )
     );
   }
 
