ca464ccab7ee4997421977ccfc6e050e7755bd56
rootasjey <jeremiecorpinot@outlook.com>
2020-04-08 00:14:47 +0200

    👆(mobile) Update Discover screen

--- lib/screens/discover.dart
+++ lib/screens/discover.dart
@@ -1,15 +1,11 @@
-import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:memorare/components/empty_view.dart';
 import 'package:memorare/components/loading.dart';
-import 'package:memorare/data/queries.dart';
-import 'package:memorare/models/user_data.dart';
+import 'package:memorare/components/web/fade_in_y.dart';
 import 'package:memorare/screens/author_page.dart';
 import 'package:memorare/screens/reference_page.dart';
 import 'package:memorare/types/author.dart';
-import 'package:memorare/types/colors.dart';
 import 'package:memorare/types/reference.dart';
-import 'package:provider/provider.dart';
 
 List<Reference> _references = [];
 List<Author> _authors = [];
@@ -20,59 +16,18 @@ class Discover extends StatefulWidget {
 }
 
 class _DiscoverState extends State<Discover> {
-  List<Reference> references = [];
-  List<Author> authors = [];
-
   String lang = 'en';
-
   bool isLoading = false;
-  bool hasConnection = true;
-  bool hasErrorsAuthors = false;
-  bool hasErrorsReferences = false;
-  Error error;
 
   @override
   void initState() {
     super.initState();
 
-    setState(() {
-      references = _references;
-      authors = _authors;
-    });
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-
-    if (authors.length > 0 || references.length > 0) {
+    if (_authors.length > 0 || _references.length > 0) {
       return;
     }
 
-    DataConnectionChecker().hasConnection
-    .then((_hasConnection) {
-      if (!hasConnection) {
-        setState(() {
-          hasConnection = _hasConnection;
-        });
-
-        return;
-      }
-
-      final userData = Provider.of<UserDataModel>(context, listen: false);
-      lang = (userData.data.lang != null && userData.data.lang.isNotEmpty) ?
-        userData.data.lang : 'en';
-
-      fetchRandomAuthors(lang);
-      fetchRandomReferences(lang);
-    });
-  }
-
-  @override
-  void dispose() {
-    _references = references;
-    _authors = authors;
-    super.dispose();
+    fetchAuthorsAndReferences();
   }
 
   @override
@@ -80,114 +35,16 @@ class _DiscoverState extends State<Discover> {
     return Scaffold(
       body: Builder(
         builder: (BuildContext context) {
-          final themeColor = Provider.of<ThemeColor>(context);
-          final backgroundColor = themeColor.background;
-
-          if (!isLoading && !hasConnection) {
-            return EmptyView(
-              title: 'No connection',
-              description: 'The app cannot reach Internet right now.',
-              onRefresh: () {
-                fetchRandomAuthors(lang);
-                fetchRandomReferences(lang);
-              },
-            );
-          }
-
           if (isLoading) {
             return LoadingComponent(
               backgroundColor: Colors.transparent,
-              color: backgroundColor,
               title: 'Loading Discover section...',
-            );
-          }
-
-          if (hasErrorsAuthors && hasErrorsReferences) {
-            return EmptyView(
-              title: 'Discover',
-              description: error != null ?
-                error.toString()
-                : 'An unexpected error ocurred. Please try again.',
-              onRefresh: () {
-                fetchRandomAuthors(lang);
-                fetchRandomReferences(lang);
-              },
-            );
-          }
-
-          if (authors.length == 0 && references.length == 0) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                EmptyView(
-                  title: 'Discover',
-                  description: 'It is odd. There is no new data to discover at the moment 🤔. ',
-                ),
-                Padding(
-                  padding: EdgeInsets.all(5.0),
-                  child: Padding(
-                    padding: EdgeInsets.all(5.0),
-                    child: Text(
-                      'Try again'
-                    ),
-                  ),
-                ),
-              ],
-            );
-          }
-
-          List<Widget> cards = [];
-
-          for (var reference in references) {
-            cards.add(
-              discoverCard(
-                title: reference.name,
-                imgUrl: reference.imgUrl,
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (BuildContext context) {
-                        return ReferencePage(
-                          id: reference.id,
-                          referenceName: reference.name,
-                        );
-                      }
-                    )
-                  );
-                }
-              )
-            );
-          }
-
-          for (var author in authors) {
-            cards.add(
-              discoverCard(
-                title: author.name,
-                imgUrl: author.imgUrl,
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (BuildContext context) {
-                        return AuthorPage(
-                          id: author.id,
-                          authorName: author.name,
-                        );
-                      }
-                    )
-                  );
-                }
-              )
             );
           }
 
           return RefreshIndicator(
             onRefresh: () async {
-              final userData = Provider.of<UserDataModel>(context, listen: false);
-              final lang = userData.data.lang;
-
-              await fetchRandomReferences(lang);
-              await fetchRandomAuthors(lang);
+              await fetchAuthorsAndReferences();
               return null;
             },
             child: ListView(
@@ -220,7 +77,7 @@ class _DiscoverState extends State<Discover> {
                 Divider(height: 60.0,),
                 Wrap(
                   alignment: WrapAlignment.center,
-                  children: cards,
+                  children: cardsList(),
                 ),
               ],
             ),
@@ -228,6 +85,67 @@ class _DiscoverState extends State<Discover> {
         },
       ),
     );
+  }
+
+  List<Widget> cardsList() {
+    List<Widget> cards = [];
+    double index = 0;
+
+    for (var reference in _references) {
+      cards.add(
+        FadeInY(
+          delay: index,
+          beginY: 100.0,
+          child: discoverCard(
+            title: reference.name,
+            imgUrl: reference.imgUrl,
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) {
+                    return ReferencePage(
+                      id: reference.id,
+                      referenceName: reference.name,
+                    );
+                  }
+                )
+              );
+            }
+          ),
+        )
+      );
+
+      index += 1.0;
+    }
+
+    for (var author in _authors) {
+      cards.add(
+        FadeInY(
+          delay: index,
+          beginY: 100.0,
+          child: discoverCard(
+            title: author.name,
+            imgUrl: author.imgUrl,
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) {
+                    return AuthorPage(
+                      id: author.id,
+                      authorName: author.name,
+                    );
+                  }
+                )
+              );
+            }
+          ),
+        )
+      );
+
+      index += 1.0;
+    }
+
+    return cards;
   }
 
   Widget discoverCard({String title, String imgUrl, Function onTap}) {
@@ -282,64 +200,62 @@ class _DiscoverState extends State<Discover> {
     );
   }
 
-  Future fetchRandomAuthors(String lang) async {
+  Future fetchAuthorsAndReferences() async {
+    _authors.clear();
+    _references.clear();
+
     setState(() {
       isLoading = true;
-      hasErrorsAuthors = false;
     });
 
-    hasConnection = await DataConnectionChecker().hasConnection;
+    try {
+      final refsSnapshot = await Firestore.instance
+        .collection('references')
+        .orderBy('updatedAt', descending: true)
+        .limit(2)
+        .getDocuments();
 
-    if (!hasConnection) {
+      if (refsSnapshot.documents.isNotEmpty) {
+        refsSnapshot.documents.forEach((doc) {
+          final data = doc.data;
+          data['id'] = doc.documentID;
+
+          final ref = Reference.fromJSON(data);
+          _references.add(ref);
+        });
+      }
+
+      final authorsSnapshot = await Firestore.instance
+        .collection('authors')
+        .orderBy('updatedAt', descending: true)
+        .limit(2)
+        .getDocuments();
+
+      if (authorsSnapshot.documents.isNotEmpty) {
+        authorsSnapshot.documents.forEach((doc) {
+          final data = doc.data;
+          data['id'] = doc.documentID;
+
+          final author = Author.fromJSON(data);
+          _authors.add(author);
+        });
+      }
+
+      if (!this.mounted) {
+        return;
+      }
+
       setState(() {
         isLoading = false;
-        hasErrorsAuthors = true;
       });
 
-      return;
-    }
+    } catch (error, stackTrace) {
+      debugPrint('error => $error');
+      debugPrint(stackTrace.toString());
 
-    return Queries.randomAuthors(context, lang)
-      .then((authorsResp) {
-        setState(() {
-          authors = authorsResp.toSet().toList();
-          isLoading = false;
-        });
-      })
-      .catchError((err) {
-        setState(() {
-          error = err;
-          hasErrorsAuthors = true;
-          isLoading = false;
-        });
-      });
-  }
-
-  Future fetchRandomReferences(String lang) async {
-    hasErrorsReferences = false;
-
-    hasConnection = await DataConnectionChecker().hasConnection;
-
-    if (!hasConnection) {
       setState(() {
         isLoading = false;
-        hasErrorsAuthors = true;
       });
-
-      return;
     }
-
-    return Queries.randomReferences(context, lang)
-      .then((referencesResp) {
-        setState(() {
-          references = referencesResp.toSet().toList();
-          isLoading = false;
-        });
-      })
-      .catchError((err) {
-        error = err;
-        hasErrorsReferences = true;
-        isLoading = false;
-      });
   }
 }
