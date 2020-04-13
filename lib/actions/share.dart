a8c0de70cd802cce529bf35af935002ae6bf24bd
rootasjey <jeremiecorpinot@outlook.com>
2020-04-13 15:48:13 +0200

    ➕(mobile) Add `shareFromMobile` function

--- lib/actions/share.dart
+++ lib/actions/share.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:memorare/types/quote.dart';
+import 'package:share/share.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 /// Sahre the target quote to twitter.
@@ -16,4 +18,27 @@ Future shareTwitter({Quote quote}) async {
 
   final url = 'https://twitter.com/intent/tweet?via=outofcontextapp&text=$quoteAndAuthor$hashtags';
   await launch(url);
+}
+
+void shareFromMobile({Quote quote, BuildContext context}) {
+  final RenderBox box = context.findRenderObject();
+  final quoteName = quote.name;
+  final authorName = quote.author.name;
+  final referenceName = quote.mainReference.name;
+
+  String sharingText = quoteName;
+
+  if (authorName != null && authorName.length > 0) {
+    sharingText += ' — $authorName';
+  }
+
+  if (referenceName != null && referenceName.length > 0) {
+    sharingText += ' — $referenceName';
+  }
+
+  Share.share(
+    sharingText,
+    subject: 'Out Of Context',
+    sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size,
+  );
 }
