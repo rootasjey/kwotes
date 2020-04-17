742ea36b62c6ec9ee85bda9c0a308d1dcaad63a5
rootasjey <jeremiecorpinot@outlook.com>
2020-04-17 20:52:09 +0200

    ➕(mobile) Add OrderLangButton

---
+++ lib/components/order_lang_button.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:memorare/state/colors.dart';
+
+class OrderLangButton extends StatefulWidget {
+  final bool descending;
+  final String lang;
+  final Function onOrderChanged;
+  final Function onLangChanged;
+
+  OrderLangButton({
+    this.descending,
+    this.lang,
+    this.onLangChanged,
+    this.onOrderChanged,
+  });
+
+  @override
+  _OrderLangButtonState createState() => _OrderLangButtonState();
+}
+
+class _OrderLangButtonState extends State<OrderLangButton> {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        showFilterFabModal();
+      },
+      icon: Icon(
+        Icons.filter_list,
+      ),
+    );
+  }
+
+  void showFilterFabModal() {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              orderSection(),
+
+              Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: langSection(),
+              ),
+            ],
+          )
+        );
+      }
+    );
+  }
+
+  Widget langSection() {
+    return Column(
+      children: <Widget>[
+        Text('Language'),
+
+        Wrap(
+          spacing: 20.0,
+          children: <Widget>[
+            ChoiceChip(
+                label: Text(
+                  'English',
+                  style: TextStyle(
+                    color: widget.lang == 'en' ?
+                      Colors.white :
+                      stateColors.foreground,
+                  ),
+                ),
+                padding: EdgeInsets.all(5.0),
+                selected: widget.lang == 'en',
+                selectedColor: stateColors.primary,
+                onSelected: (selected) {
+                  if (widget.onLangChanged != null) {
+                    widget.onLangChanged('en');
+                  }
+
+                  Navigator.pop(context);
+                },
+              ),
+
+            ChoiceChip(
+              label: Text(
+                'Français',
+                style: TextStyle(
+                  color: widget.lang == 'fr' ?
+                    Colors.white :
+                    stateColors.foreground,
+                ),
+              ),
+              padding: EdgeInsets.all(5.0),
+              selected: widget.lang == 'fr',
+              selectedColor: stateColors.primary,
+              onSelected: (selected) {
+                if (widget.onLangChanged != null) {
+                  widget.onLangChanged('fr');
+                }
+
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget orderSection() {
+    return Column(
+      children: <Widget>[
+        Text('Order'),
+
+        Wrap(
+          spacing: 20.0,
+          children: <Widget>[
+            ChoiceChip(
+              label: Text(
+                'First added',
+                style: TextStyle(
+                  color: !widget.descending ?
+                    Colors.white :
+                    stateColors.foreground,
+                ),
+              ),
+              padding: EdgeInsets.all(5.0),
+              selected: !widget.descending,
+              selectedColor: stateColors.primary,
+              onSelected: (selected) {
+                if (widget.onOrderChanged != null) {
+                  final newOrder = !widget.descending;
+                  widget.onOrderChanged(newOrder);
+                }
+
+                Navigator.pop(context);
+              },
+            ),
+
+            ChoiceChip(
+              label: Text(
+                'Last added',
+                style: TextStyle(
+                  color: widget.descending ?
+                    Colors.white :
+                    stateColors.foreground,
+                ),
+              ),
+              padding: EdgeInsets.all(5.0),
+              selected: widget.descending,
+              selectedColor: stateColors.primary,
+              onSelected: (selected) {
+                if (widget.onOrderChanged != null) {
+                  final newOrder = !widget.descending;
+                  widget.onOrderChanged(newOrder);
+                }
+
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
