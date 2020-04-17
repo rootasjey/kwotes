93f1a6b030aea04ad0b85398b54d040c226478ca
rootasjey <jeremiecorpinot@outlook.com>
2020-04-17 21:05:43 +0200

    👆(mobile) Update published quotes page

--- lib/screens/published_quotes.dart
+++ lib/screens/published_quotes.dart
@@ -1,15 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:memorare/components/empty_view.dart';
-import 'package:memorare/components/error.dart';
-import 'package:memorare/components/filter_fab.dart';
-import 'package:memorare/components/loading.dart';
-import 'package:memorare/components/small_quote_card.dart';
-import 'package:memorare/data/queries.dart';
-import 'package:memorare/screens/add_quote.dart';
-import 'package:memorare/types/colors.dart';
-import 'package:memorare/types/pagination.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:memorare/actions/favourites.dart';
+import 'package:memorare/components/error_container.dart';
+import 'package:memorare/components/order_lang_button.dart';
+import 'package:memorare/components/web/empty_content.dart';
+import 'package:memorare/components/web/fade_in_y.dart';
+import 'package:memorare/components/web/loading_animation.dart';
+import 'package:memorare/router/route_names.dart';
+import 'package:memorare/router/router.dart';
+import 'package:memorare/state/colors.dart';
+import 'package:memorare/state/topics_colors.dart';
+import 'package:memorare/state/user_state.dart';
 import 'package:memorare/types/quote.dart';
-import 'package:provider/provider.dart';
 
 class MyPublishedQuotes extends StatefulWidget {
   @override
@@ -17,197 +20,385 @@ class MyPublishedQuotes extends StatefulWidget {
 }
 
 class MyPublishedQuotesState extends State<MyPublishedQuotes> {
-  String lang = 'en';
-  int order = -1;
+  bool hasNext        = true;
+  bool hasErrors      = false;
+  bool isLoading      = false;
+  bool isLoadingMore  = false;
+  String lang         = 'en';
+  int limit           = 30;
+  bool descending     = true;
+
+  ScrollController _scrollController = ScrollController();
   List<Quote> quotes = [];
 
-  Pagination pagination = Pagination();
-  bool isLoadingMoreQuotes = false;
-
-  ScrollController gridViewScrollController = ScrollController();
-
-  int attempts = 1;
-  int maxAttempts = 2;
-
-  bool isLoading = false;
-  bool hasErrors = false;
-  Error error;
+  var lastDoc;
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-
-    if (quotes.length > 0) { return; }
-    fetchQuotes();
+  initState() {
+    super.initState();
+    fetch();
   }
-
   @override
   Widget build(BuildContext context) {
-    final accent = Provider.of<ThemeColor>(context).accent;
+    return Scaffold(
+      body: body(),
+    );
+  }
 
+  Widget body() {
+    return RefreshIndicator(
+      onRefresh: () async {
+        await fetch();
+        return null;
+      },
+      child: NotificationListener<ScrollNotification>(
+        onNotification: (ScrollNotification scrollNotif) {
+          if (scrollNotif.metrics.pixels < scrollNotif.metrics.maxScrollExtent) {
+            return false;
+          }
+
+          if (hasNext && !isLoadingMore) {
+            fetchMore();
+          }
+
+          return false;
+        },
+        child: CustomScrollView(
+          controller: _scrollController,
+          slivers: <Widget>[
+            appBar(),
+            bodyListContent(),
+          ],
+        ),
+      )
+    );
+  }
+
+  Widget appBar() {
+    return Observer(
+      builder: (_) {
+        return SliverAppBar(
+          floating: true,
+          snap: true,
+          expandedHeight: 120.0,
+          backgroundColor: stateColors.softBackground,
+          automaticallyImplyLeading: false,
+          flexibleSpace: Stack(
+            children: <Widget>[
+              FadeInY(
+                delay: 1.0,
+                beginY: 50.0,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 50.0),
+                  child: FlatButton(
+                    onPressed: () {
+                      if (quotes.length == 0) { return; }
+
+                      _scrollController.animateTo(
+                        0,
+                        duration: Duration(seconds: 2),
+                        curve: Curves.easeOutQuint
+                      );
+                    },
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width - 60.0,
+                      child: Text(
+                        'Published',
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 25.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              Positioned(
+                right: 20.0,
+                top: 50.0,
+                child: OrderLangButton(
+                  descending: descending,
+                  lang: lang,
+                  onLangChanged: (newLang) {
+                    lang = newLang;
+                    fetch();
+                  },
+                  onOrderChanged: (newDescending) {
+                    setState(() {
+                      descending = newDescending;
+                    });
+
+                    fetch();
+                  },
+                ),
+              ),
+
+              Positioned(
+                left: 20.0,
+                top: 50.0,
+                child: IconButton(
+                  onPressed: () {
+                    FluroRouter.router.pop(context);
+                  },
+                  tooltip: 'Back',
+                  icon: Icon(Icons.arrow_back),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget bodyListContent() {
     if (isLoading) {
-      return Scaffold(
-        body: LoadingComponent(
-          title: 'Loading my published quotes...',
+      return SliverList(
+        delegate: SliverChildListDelegate([
+            Padding(
+              padding: const EdgeInsets.only(top: 200.0),
+              child: LoadingAnimation(),
+            ),
+          ]
         ),
       );
     }
 
     if (!isLoading && hasErrors) {
-      return ErrorComponent(
-        description: error != null ? error.toString() : '',
+      return SliverList(
+        delegate: SliverChildListDelegate([
+          Padding(
+            padding: const EdgeInsets.only(top: 150.0),
+            child: ErrorContainer(
+              onPressed: () {
+                fetch();
+              },
+            ),
+          ),
+        ]),
       );
     }
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        centerTitle: true,
-        elevation: 0,
-        title: InkWell(
-          onTap: () {
-            if (quotes.length == 0) { return; }
-
-            gridViewScrollController.animateTo(
-              0,
-              duration: Duration(seconds: 2),
-              curve: Curves.easeOutQuint
-            );
-          },
-          child: Text(
-            'Published quotes',
-            style: TextStyle(
-              color: accent,
-              fontSize: 25.0,
-            ),
-          ),
-        ),
-        leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          icon: Icon(Icons.arrow_back, color: accent,),
-        ),
-      ),
-      floatingActionButton: quotes.length > 0 ?
-        FilterFab(
-          onOrderChanged: (int newOrder) {
-            setState(() {
-              order = newOrder;
-            });
-
-            fetchQuotes();
-          },
-          order: order,
-        ):
-        Padding(padding: EdgeInsets.zero,),
-      body: Builder(
-        builder: (BuildContext context) {
-          if (quotes.length == 0) {
-            return EmptyView(
-              title: 'No quotes',
-              icon: Icon(Icons.speaker_notes_off, size: 60.0),
-              description: 'You have no quotes published yet. Go to the Add Quote page to start sharing your thoughts with others.',
-              onRefresh: () async {
-                await fetchQuotes();
-                return null;
-              },
-              onTapDescription: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) {
-                      return AddQuote();
-                    }
-                  )
-                );
-              },
-            );
-          }
-
-          return RefreshIndicator(
-            onRefresh: () async {
-              await fetchQuotes();
-              return null;
-            },
-            child: NotificationListener<ScrollNotification>(
-              onNotification: (ScrollNotification scrollNotif) {
-                if (scrollNotif.metrics.pixels < scrollNotif.metrics.maxScrollExtent) {
-                  return false;
-                }
-
-                if (pagination.hasNext && !isLoadingMoreQuotes) {
-                  fetchMoreQuotes();
-                }
-
-                return false;
-              },
-              child: GridView.builder(
-                itemCount: quotes.length,
-                padding: EdgeInsets.symmetric(vertical: 20.0),
-                controller: gridViewScrollController,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                itemBuilder: (BuildContext context, int index) {
-                  return SmallQuoteCard(quote: quotes.elementAt(index),);
-                },
+    if (quotes.length == 0) {
+      return SliverList(
+        delegate: SliverChildListDelegate([
+            FadeInY(
+              delay: 2.0,
+              beginY: 50.0,
+              child: EmptyContent(
+                icon: Opacity(
+                  opacity: .8,
+                  child: Icon(
+                    Icons.favorite_border,
+                    size: 120.0,
+                    color: Color(0xFFFF005C),
+                  ),
+                ),
+                title: "You've published no quote yet",
+                subtitle: 'They will appear when you add a new quote',
+                onRefresh: () => fetch(),
               ),
-            )
+            ),
+          ]
+        ),
+      );
+    }
+
+    return sliverQuotesList();
+  }
+
+  Widget sliverQuotesList() {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          final quote = quotes.elementAt(index);
+          final topicColor = appTopicsColors.find(quote.topics.first);
+
+          return InkWell(
+            onTap: () {
+              FluroRouter.router.navigateTo(
+                context,
+                QuotePageRoute.replaceFirst(':id', quote.id),
+              );
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(padding: const EdgeInsets.only(top: 20.0),),
+
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    quote.name,
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+
+                Center(
+                  child: IconButton(
+                    onPressed: () {
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 20.0,
+                              vertical: 60.0,
+                            ),
+                            child: Wrap(
+                              spacing: 30.0,
+                              alignment: WrapAlignment.center,
+                              children: <Widget>[
+                                IconButton(
+                                  iconSize: 40.0,
+                                  tooltip: 'Add to favourites',
+                                  onPressed: () {
+                                    addToFavourites(
+                                      context: context,
+                                      quote: quote,
+                                    );
+                                  },
+                                  icon: Opacity(
+                                    opacity: .6,
+                                    child: Icon(
+                                      Icons.favorite_border,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        }
+                      );
+                    },
+                    icon: Icon(
+                      Icons.more_horiz,
+                      color: topicColor != null ?
+                      Color(topicColor.decimal) : stateColors.primary,
+                    ),
+                  ),
+                ),
+
+                Padding(padding: const EdgeInsets.only(top: 10.0),),
+                Divider(),
+              ],
+            ),
           );
         },
-      )
+        childCount: quotes.length,
+      ),
     );
   }
 
-  Future fetchQuotes() {
+  Future fetch() async {
     setState(() {
       isLoading = true;
     });
 
-    pagination = Pagination();
+    lastDoc = null;
+    quotes.clear();
 
-    return Queries.myPublihshedQuotes(
-      context: context,
-      lang: lang,
-      limit: pagination.limit,
-      order: order,
-      skip: pagination.skip,
+    try {
+      final userAuth = await userState.userAuth;
 
-    ).then((quotesResp) {
+      if (userAuth == null) {
+        FluroRouter.router.navigateTo(context, SigninRoute);
+        return;
+      }
+
+      final snapshot = await Firestore.instance
+        .collection('quotes')
+        .where('user.id', isEqualTo: userAuth.uid)
+        .where('lang', isEqualTo: lang)
+        .orderBy('createdAt', descending: descending)
+        .limit(30)
+        .getDocuments();
+
+      if (snapshot.documents.isEmpty) {
+        setState(() {
+          isLoading = false;
+        });
+
+        return;
+      }
+
+      snapshot.documents.forEach((doc) {
+        final data = doc.data;
+        data['id'] = doc.documentID;
+
+        final quote = Quote.fromJSON(data);
+        quotes.add(quote);
+      });
+
+      lastDoc = snapshot.documents.last;
+
       setState(() {
         isLoading = false;
-        quotes = quotesResp.entries;
-        pagination = quotesResp.pagination;
       });
-    })
-    .catchError((err) {
+
+    } catch (error) {
+      debugPrint(error.toString());
+
       setState(() {
         isLoading = false;
-        hasErrors = true;
-        error = err;
       });
-    });
+    }
   }
 
-  Future fetchMoreQuotes() {
-    isLoadingMoreQuotes = true;
+  void fetchMore() async {
+    if (lastDoc == null) { return; }
 
-    return Queries.myPublihshedQuotes(
-      context: context,
-      lang: lang,
-      limit: pagination.limit,
-      order: order,
-      skip: pagination.nextSkip,
-
-    ).then((quotesResp) {
-      setState(() {
-        isLoadingMoreQuotes = false;
-        quotes.addAll(quotesResp.entries);
-        pagination = quotesResp.pagination;
-      });
-    })
-    .catchError((err) {
-      setState(() {
-        isLoadingMoreQuotes = false;
-      });
+    setState(() {
+      isLoadingMore = true;
     });
+
+    try {
+      final userAuth = await userState.userAuth;
+
+      if (userAuth == null) {
+        FluroRouter.router.navigateTo(context, SigninRoute);
+        return;
+      }
+
+      final snapshot = await Firestore.instance
+        .collection('quotes')
+        .where('user.id', isEqualTo: userAuth.uid)
+        .where('lang', isEqualTo: lang)
+        .orderBy('createdAt', descending: descending)
+        .startAfterDocument(lastDoc)
+        .limit(30)
+        .getDocuments();
+
+      if (snapshot.documents.isEmpty) {
+        setState(() {
+          isLoadingMore = false;
+        });
+
+        return;
+      }
+
+      snapshot.documents.forEach((doc) {
+        final data = doc.data;
+        data['id'] = doc.documentID;
+
+        final quote = Quote.fromJSON(data);
+        quotes.insert(quotes.length - 1, quote);
+      });
+
+      setState(() {
+        isLoadingMore = false;
+      });
+
+    } catch (error) {
+      debugPrint(error.toString());
+
+      setState(() {
+        isLoadingMore = false;
+      });
+    }
   }
 }
