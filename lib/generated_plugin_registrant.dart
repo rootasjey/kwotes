17f73ae3f73cb266f99e9dfe387a34c5c9856152
rootasjey <jeremiecorpinot@outlook.com>
2020-04-18 00:19:29 +0200

    💉 Update dependency flutter

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -13,6 +13,7 @@ import 'package:url_launcher_web/url_launcher_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
+// ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
   FirestoreWeb.registerWith(registry.registrarFor(FirestoreWeb));
   FirebaseAuthWeb.registerWith(registry.registrarFor(FirebaseAuthWeb));
