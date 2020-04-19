c94acecdcfa087245d82067af6c4c775995a2325
rootasjey <jeremiecorpinot@outlook.com>
2020-04-19 10:55:34 +0200

    👆(mobile) Update Urls w/ new props.

--- lib/types/urls.dart
+++ lib/types/urls.dart
@@ -1,22 +1,43 @@
 class Urls {
-  final String affiliate;
-  final String image;
-  final String website;
-  final String wikipedia;
+  String affiliate;
+  String amazon;
+  String facebook;
+  String image;
+  String netflix;
+  String primeVideo;
+  String twitch;
+  String twitter;
+  String website;
+  String wikipedia;
+  String youTube;
 
   Urls({
     this.affiliate  = '',
+    this.amazon     = '',
+    this.facebook   = '',
     this.image      = '',
+    this.netflix    = '',
+    this.primeVideo = '',
+    this.twitch     = '',
+    this.twitter    = '',
     this.website    = '',
     this.wikipedia  = '',
+    this.youTube    = '',
   });
 
   factory Urls.fromJSON(Map<String, dynamic> json) {
     return Urls(
-      affiliate : json['affiliate'] ?? '',
-      image     : json['image']     ?? '',
-      website   : json['website']   ?? '',
-      wikipedia : json['wikipedia'] ?? '',
+      affiliate   : json['affiliate']   ?? '',
+      amazon      : json['amazon']      ?? '',
+      facebook    : json['facebook']    ?? '',
+      image       : json['image']       ?? '',
+      netflix     : json['netflix']     ?? '',
+      primeVideo  : json['primeVideo']  ?? '',
+      twitch      : json['twitch']      ?? '',
+      twitter     : json['twitter']     ?? '',
+      website     : json['website']     ?? '',
+      wikipedia   : json['wikipedia']   ?? '',
+      youTube     : json['youTube']     ?? '',
     );
   }
 }
