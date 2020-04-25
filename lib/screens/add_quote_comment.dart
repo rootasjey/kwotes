eee37bbd112d9db0a62a9bd8b50f410d8651b8e7
rootasjey <jeremiecorpinot@outlook.com>
2020-04-25 12:05:14 +0200

    💄(mobile) Add animations on  `add quote comment`

--- lib/screens/add_quote_comment.dart
+++ lib/screens/add_quote_comment.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:memorare/components/web/fade_in_y.dart';
 import 'package:memorare/data/add_quote_inputs.dart';
 
 class AddQuoteComment extends StatefulWidget {
@@ -20,6 +21,10 @@ class AddQuoteComment extends StatefulWidget {
 }
 
 class _AddQuoteCommentState extends State<AddQuoteComment> {
+  final beginY    = 100.0;
+  final delay     = 1.0;
+  final delayStep = 1.2;
+
   final commentController = TextEditingController();
 
   @override
@@ -62,26 +67,34 @@ class _AddQuoteCommentState extends State<AddQuoteComment> {
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 45.0),
-              child: Text(
-                'Add comment',
-                style: TextStyle(
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.bold,
+            FadeInY(
+              delay: delay + (1 * delayStep),
+              beginY: beginY,
+              child: Padding(
+                padding: EdgeInsets.only(top: 45.0),
+                child: Text(
+                  'Add comment',
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
           ],
         ),
 
-        Opacity(
-          opacity: 0.6,
-          child: Text(
-            '${widget.step}/${widget.maxSteps}',
-            style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
+        FadeInY(
+          delay: delay + (2 * delayStep),
+          beginY: beginY,
+          child: Opacity(
+            opacity: 0.6,
+            child: Text(
+              '${widget.step}/${widget.maxSteps}',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ),
@@ -92,42 +105,50 @@ class _AddQuoteCommentState extends State<AddQuoteComment> {
   Widget input() {
     return Column(
       children: <Widget>[
-        Padding(
-          padding: EdgeInsets.only(top: 60.0),
-          child: SizedBox(
-            width: 300.0,
-            child: TextField(
-              controller: commentController,
-              textCapitalization: TextCapitalization.sentences,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Add a comment about the quote'
+        FadeInY(
+          delay: delay + (3 * delayStep),
+          beginY: beginY,
+          child: Padding(
+            padding: EdgeInsets.only(top: 100.0),
+            child: SizedBox(
+              width: 300.0,
+              child: TextField(
+                controller: commentController,
+                textCapitalization: TextCapitalization.sentences,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Add a comment about the quote'
+                ),
+                onChanged: (newValue) {
+                  AddQuoteInputs.comment = newValue;
+                },
               ),
-              onChanged: (newValue) {
-                AddQuoteInputs.comment = newValue;
-              },
             ),
           ),
         ),
 
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              FlatButton(
-                onPressed: () {
-                  AddQuoteInputs.comment = '';
-                  commentController.clear();
-                },
-                child: Opacity(
-                  opacity: 0.6,
-                  child: Text(
-                    'Clear comment',
-                  ),
-                )
-              ),
-            ],
+        FadeInY(
+          delay: delay + (4 * delayStep),
+          beginY: beginY,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                FlatButton(
+                  onPressed: () {
+                    AddQuoteInputs.comment = '';
+                    commentController.clear();
+                  },
+                  child: Opacity(
+                    opacity: 0.6,
+                    child: Text(
+                      'Clear comment',
+                    ),
+                  )
+                ),
+              ],
+            ),
           ),
         ),
       ],
@@ -171,50 +192,54 @@ class _AddQuoteCommentState extends State<AddQuoteComment> {
   }
 
   Widget helpButton() {
-    return IconButton(
-      padding: EdgeInsets.symmetric(vertical: 20.0),
-      onPressed: () {
-        showModalBottomSheet(
-          context: context,
-          builder: (BuildContext context) {
-            return ListView(
-              padding: EdgeInsets.all(40.0),
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(bottom: 40.0),
-                  child: Text(
-                    'Help',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 25.0,
+    return FadeInY(
+      delay: delay + (5 * delayStep),
+      beginY: beginY,
+      child: IconButton(
+        padding: EdgeInsets.symmetric(vertical: 20.0),
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return ListView(
+                padding: EdgeInsets.all(40.0),
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 40.0),
+                    child: Text(
+                      'Help',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 25.0,
+                      ),
                     ),
                   ),
-                ),
 
-                Padding(
-                  padding: EdgeInsets.only(bottom: 20.0),
-                  child: Text(
-                    '• Comment is optional',
-                    style: TextStyle(
-                      fontSize: 17.0,
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 20.0),
+                    child: Text(
+                      '• Comment is optional',
+                      style: TextStyle(
+                        fontSize: 17.0,
+                      ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 20.0),
-                  child: Text(
-                    '• Use it if you want to specify the context, the hidden meaning of the quote or something related',
-                    style: TextStyle(
-                      fontSize: 17.0,
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 20.0),
+                    child: Text(
+                      '• Use it if you want to specify the context, the hidden meaning of the quote or something related',
+                      style: TextStyle(
+                        fontSize: 17.0,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            );
-          }
-        );
-      },
-      icon: Icon(Icons.help),
+                ],
+              );
+            }
+          );
+        },
+        icon: Icon(Icons.help),
+      ),
     );
   }
 }
