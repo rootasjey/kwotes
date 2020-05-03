425b47b6c91ba95cc60142070bbbd7974aa66534
rootasjey <jeremiecorpinot@outlook.com>
2020-05-03 14:52:54 +0200

    ➖(mobile) Delete unecessary method

--- lib/screens/admin_quotes.dart
+++ lib/screens/admin_quotes.dart
@@ -8,13 +8,11 @@ import 'package:memorare/components/order_lang_button.dart';
 import 'package:memorare/components/web/empty_content.dart';
 import 'package:memorare/components/web/fade_in_y.dart';
 import 'package:memorare/components/web/loading_animation.dart';
-import 'package:memorare/data/add_quote_inputs.dart';
 import 'package:memorare/router/route_names.dart';
 import 'package:memorare/router/router.dart';
 import 'package:memorare/state/colors.dart';
 import 'package:memorare/state/topics_colors.dart';
 import 'package:memorare/types/quote.dart';
-import 'package:memorare/types/temp_quote.dart';
 import 'package:memorare/utils/app_localstorage.dart';
 import 'package:memorare/utils/snack.dart';
 
@@ -318,11 +316,6 @@ class AdminQuotesState extends State<AdminQuotes> {
         type: SnackType.error,
       );
     }
-  }
-
-  void editTempQuote(TempQuote tempQuote) async {
-    AddQuoteInputs.populateWithTempQuote(tempQuote);
-    FluroRouter.router.navigateTo(context, AddQuoteContentRoute);
   }
 
   Future fetch() async {
