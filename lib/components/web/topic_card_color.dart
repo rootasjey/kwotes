e588fd4b443625cedc2d25b14b161ed69f93279f
rootasjey <jeremiecorpinot@outlook.com>
2020-05-09 10:45:13 +0200

    👆Add new TopicCardColor prop. (outline, tooltip)

--- lib/components/web/topic_card_color.dart
+++ lib/components/web/topic_card_color.dart
@@ -7,10 +7,12 @@ class TopicCardColor extends StatelessWidget {
   final String displayName;
   final double elevation;
   final String name;
-  final double size;
-  final TextStyle style;
   final Function onColorTap;
   final Function onTextTap;
+  final bool outline;
+  final double size;
+  final TextStyle style;
+  final String tooltip;
 
   TopicCardColor({
     this.color,
@@ -19,8 +21,10 @@ class TopicCardColor extends StatelessWidget {
     this.name = '',
     this.onColorTap,
     this.onTextTap,
+    this.outline = false,
     this.size = 70.0,
     this.style,
+    this.tooltip,
   });
 
   @override
@@ -29,48 +33,77 @@ class TopicCardColor extends StatelessWidget {
       padding: const EdgeInsets.all(20.0),
       child: Column(
         children: <Widget>[
-          SizedBox(
-            height: size,
-            width: size,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              elevation: elevation,
-              color: color,
-              child: InkWell(
-                onTap: () {
-                  if (onColorTap != null) {
-                    onColorTap();
-                    return;
-                  }
-
-                  FluroRouter.router.navigateTo(
-                    context,
-                    TopicRoute.replaceFirst(':name', name)
-                  );
-                },
-              ),
-            ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.only(top: 5.0),
-            child: Opacity(
-              opacity: .5,
-              child: InkWell(
-                onTap: onTextTap,
-                child: Text(
-                  displayName != null && displayName.length > 0 ?
-                    displayName : name,
-                  overflow: TextOverflow.ellipsis,
-                  style: style,
-                ),
-              ),
-            ),
-          ),
+          cardColor(context: context),
+          cardName(),
         ],
       ),
+    );
+  }
+
+  Widget cardColor({BuildContext context}) {
+    final card = SizedBox(
+      height: size,
+      width: size,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: outline ? color : Colors.transparent,
+          ),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        elevation: elevation,
+        color: outline ? Colors.transparent : color,
+        child: InkWell(
+          onTap: () {
+            if (onColorTap != null) {
+              onColorTap();
+              return;
+            }
+
+            FluroRouter.router.navigateTo(
+              context,
+              TopicRoute.replaceFirst(':name', name)
+            );
+          },
+        ),
+      ),
+    );
+
+    if (tooltip != null && tooltip.length > 0) {
+      return withTooltip(child: card);
+    }
+
+    return card;
+  }
+
+  Widget cardName() {
+    final text = Padding(
+      padding: const EdgeInsets.only(top: 5.0),
+      child: Opacity(
+        opacity: .5,
+        child: InkWell(
+          onTap: onTextTap,
+          child: Text(
+            displayName != null && displayName.length > 0 ?
+              displayName : name,
+            overflow: TextOverflow.ellipsis,
+            style: style,
+          ),
+        ),
+      ),
+    );
+
+    if (tooltip != null && tooltip.length > 0) {
+      return withTooltip(child: text);
+    }
+
+    return text;
+  }
+
+  Widget withTooltip({Widget child}) {
+    return Tooltip(
+      message: name,
+      child: child,
     );
   }
 }
