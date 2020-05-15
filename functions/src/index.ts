910ae2d310373a677dce5ec06d629b744a743076
rootasjey <jeremiecorpinot@outlook.com>
2020-05-16 01:52:52 +0200

    ➕(cloud function) Add quota function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,52 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const firestore = admin.firestore();
+
+export const incrementQuoteQuota = functions
+  .region('europe-west3')
+  .firestore
+  .document('tempquotes/{tempquoteId}')
+  .onCreate(async (snapshot, context) => {
+    const snapshotData = snapshot.data();
+    if (!snapshotData) { return; }
+
+    const userUid = snapshotData.user.id;
+    if (!userUid) { return; }
+
+    const userDoc = await firestore
+      .collection('users')
+      .doc(userUid)
+      .get();
+
+    if (!userDoc.exists) { return; }
+
+    const data = userDoc.data();
+    if (!data) { return; }
+
+    const serverDate = new Date(context.timestamp);
+    const quota = data.quota;
+
+    let date = quota.date !== null && typeof quota.date !== 'undefined' ?
+      quota.date.toDate() :
+      new Date('January 1');
+
+    let current: number = quota['current'];
+
+    if (date.getDate() !== serverDate.getDate() ||
+      date.getMonth() !== serverDate.getMonth() ||
+      date.getFullYear() !== serverDate.getFullYear()) {
+
+      current = 0;
+      date = serverDate;
+    }
+
+    current += 1;
+
+    return await userDoc.ref
+      .update({
+        'quota.date': admin.firestore.Timestamp.fromDate(date),
+        'quota.current': current,
+      });
+  });
