2238cdfb66a75242eb3f5159429e6ad6ca776cd0
rootasjey <jeremiecorpinot@outlook.com>
2020-05-25 00:14:26 +0200

    💄Add new loading animation component

---
+++ lib/components/loading_animation.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:memorare/components/animated_app_icon.dart';
+
+class LoadingAnimation extends StatelessWidget {
+  final TextStyle style;
+  final String textTitle;
+  final Widget title;
+  final double size;
+
+  LoadingAnimation({
+    this.size = 100.0,
+    this.style = const TextStyle(fontSize: 30.0,),
+    this.textTitle = 'Loading...',
+    this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AnimatedAppIcon(size: size),
+
+        title != null ?
+          title :
+          Padding(
+            padding: const EdgeInsets.only(top: 40.0),
+            child: Text(
+              textTitle,
+              style: style
+            ),
+          ),
+      ],
+    );
+  }
+}
