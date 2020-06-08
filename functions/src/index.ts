91ee565093ffcb2556b816a41cefb7d6e2c2af8b
rootasjey <jeremiecorpinot@outlook.com>
2020-06-08 23:51:02 +0200

    ➕(functions) Add onFavAdded/onFavDeleted

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -61,6 +61,52 @@ async function isUserNameOk(snapshot: functions.firestore.DocumentSnapshot) {
   return usserNameSnap.empty;
 }
 
+export const onFavAdded = functions
+  .region('europe-west3')
+  .firestore
+  .document('users/{userId}/favourites/{quoteId}')
+  .onCreate(async (snapshot, context) => {
+    const quoteSnap = await firestore
+      .collection('quotes')
+      .doc(snapshot.id)
+      .get();
+
+    if (!quoteSnap.exists) { return; }
+
+    const data = quoteSnap.data();
+    if (!data) { return; }
+
+    const favCount: number = data.stats.likes;
+    return await quoteSnap.ref
+      .update({
+        'stats.likes': favCount + 1,
+      });
+  });
+
+export const onFavDeleted = functions
+  .region('europe-west3')
+  .firestore
+  .document('users/{userId}/favourites/{quoteId}')
+  .onDelete(async (snapshot, context) => {
+    const quoteSnap = await firestore
+      .collection('quotes')
+      .doc(snapshot.id)
+      .get();
+
+    if (!quoteSnap.exists) { return; }
+
+    const data = quoteSnap.data();
+    if (!data) { return; }
+
+    const favCount: number = data.stats.likes;
+    if (favCount === 0) { return; }
+
+    return await quoteSnap.ref
+      .update({
+        'stats.likes': favCount - 1,
+      });
+  });
+
 // Add all missing props.
 async function populateUserData(snapshot: functions.firestore.DocumentSnapshot) {
   const user = await admin.auth().getUser(snapshot.id);
