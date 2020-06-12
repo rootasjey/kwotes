9684486d27dd1fb9ce10579924f48ed04b0021fe
rootasjey <jeremiecorpinot@outlook.com>
2020-06-12 16:22:05 +0200

    👆Better adapt app bar to small screens

--- lib/components/web/sliver_app_header.dart
+++ lib/components/web/sliver_app_header.dart
@@ -6,20 +6,122 @@ import 'package:memorare/router/router.dart';
 import 'package:memorare/state/colors.dart';
 
 class SliverAppHeader extends StatelessWidget {
-  final String title;
   final Widget bottomButton;
+  final Function onScrollToTop;
   final Widget rightButton;
+  final String subTitle;
+  final String title;
 
   final double widthLimit = 500.0;
 
-  SliverAppHeader({this.title, this.bottomButton, this.rightButton});
+  SliverAppHeader({
+    this.title,
+    this.subTitle,
+    this.bottomButton,
+    this.rightButton,
+    this.onScrollToTop,
+  });
 
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
-    final top = width < widthLimit ? 55.0 : 85.0;
+    final top = width < widthLimit ? 50.0 : 85.0;
     final left = width < widthLimit ? 20.0 : 80.0;
 
+    // Small screens
+    if (width < widthLimit) {
+      return Observer(
+        builder: (context) {
+          return SliverAppBar(
+            floating: true,
+            snap: true,
+            backgroundColor: stateColors.softBackground,
+            expandedHeight: 130.0,
+            automaticallyImplyLeading: false,
+            flexibleSpace: Stack(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    FadeInY(
+                      delay: 0.0,
+                      beginY: 50.0,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 52.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            InkWell(
+                              onTap: this.onScrollToTop,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  title,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    fontSize: 25.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+
+                    if (this.subTitle != null)
+                      FadeInY(
+                        delay: .5,
+                        beginY: 50.0,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 17.0),
+                          child: Opacity(
+                            opacity: .6,
+                            child: Text(
+                              this.subTitle,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ),
+                      ),
+
+                    if (this.bottomButton != null)
+                      FadeInY(
+                        delay: .8,
+                        beginY: 50.0,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 10.0),
+                          child: this.bottomButton,
+                        ),
+                      ),
+                  ],
+                ),
+
+                if (this.rightButton != null)
+                  Positioned(
+                  right: 20.0,
+                  top: top,
+                  child: this.rightButton,
+                ),
+
+                Positioned(
+                  left: left,
+                  top: top,
+                  child: IconButton(
+                    onPressed: () {
+                      FluroRouter.router.pop(context);
+                    },
+                    tooltip: 'Back',
+                    icon: Icon(Icons.arrow_back),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    }
+
+    // Large screens
     return Observer(
       builder: (context) {
         return SliverAppBar(
