f61423725e48d80c507ba837ce2ca9ad7d22f3d4
rootasjey <jeremiecorpinot@outlook.com>
2020-06-14 02:34:58 +0200

    👆(mobile) Add values check + 💄

--- lib/screens/edit_password.dart
+++ lib/screens/edit_password.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:memorare/components/web/fade_in_y.dart';
+import 'package:memorare/components/web/sliver_app_header.dart';
 import 'package:memorare/router/route_names.dart';
 import 'package:memorare/router/router.dart';
 import 'package:memorare/state/colors.dart';
 import 'package:memorare/state/user_state.dart';
+import 'package:memorare/utils/snack.dart';
 
 class EditPassword extends StatefulWidget {
   @override
@@ -14,6 +16,8 @@ class EditPassword extends StatefulWidget {
 class _EditPasswordState extends State<EditPassword> {
   String password     = '';
   String newPassword  = '';
+
+  final newPasswordNode = FocusNode();
 
   bool isUpdating     = false;
   bool isCompleted    = false;
@@ -25,34 +29,15 @@ class _EditPasswordState extends State<EditPassword> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(100.0),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 20.0),
-          child: AppBar(
-            backgroundColor: stateColors.softBackground,
-            centerTitle: true,
-            elevation: 2,
-            title: Text(
-              'Update password',
-              style: TextStyle(
-                fontSize: 25.0,
-                color: stateColors.foreground,
-              ),
-            ),
-            leading: IconButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              icon: Icon(
-                Icons.arrow_back,
-                color: stateColors.foreground,
-              ),
-            ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppHeader(
+            title: 'Update password',
+            subTitle: 'If your password is compromised or old',
           ),
-        ),
+          body(),
+        ],
       ),
-      body: body(),
     );
   }
 
@@ -65,84 +50,58 @@ class _EditPasswordState extends State<EditPassword> {
       return updatingScreen();
     }
 
-    return ListView(
-      padding: const EdgeInsets.all(40.0),
-      children: <Widget>[
-        Column(
-          children: <Widget>[
-            FadeInY(
-              delay: delay + (1 * delayStep),
-              beginY: beginY,
-              child: imageTitle(),
-            ),
-
-            FadeInY(
-              delay: delay + (2 * delayStep),
-              beginY: beginY,
-              child: currentPasswordInput(),
-            ),
-
-            FadeInY(
-              delay: delay + (3 * delayStep),
-              beginY: beginY,
-              child: newPasswordInput(),
-            ),
-
-            FadeInY(
-              delay: delay + (4 * delayStep),
-              beginY: beginY,
-              child: validationButton(),
-            ),
-
-            Padding(padding: const EdgeInsets.only(bottom: 200.0),),
-          ],
-        ),
-      ],
-    );
+    return idleView();
   }
 
   Widget completedScreen() {
-    return Container(
-      padding: const EdgeInsets.all(40.0),
-      child: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(top: 80.0),
-            child: Icon(
-              Icons.check_circle_outline,
-              size: 80.0,
-            ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.only(top: 30.0, bottom: 0.0),
-            child: Text(
-              'Your password has been successfuly updated.',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20.0,
+    return SliverList(
+      delegate: SliverChildListDelegate([
+        Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 80.0),
+                child: Icon(
+                  Icons.check_circle_outline,
+                  size: 80.0,
+                ),
               ),
-            ),
+
+              Padding(
+                padding: const EdgeInsets.only(top: 30.0, bottom: 0.0),
+                child: Text(
+                  'Your password has been successfuly updated.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+      ]),
     );
   }
 
-  Widget currentPasswordInput() {
-    return Padding(
+  Widget currentPasswordInput({double width}) {
+    return Container(
+      width: width,
       padding: const EdgeInsets.only(top: 80.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           TextFormField(
+            autofocus: true,
             decoration: InputDecoration(
               icon: Icon(Icons.lock_open),
-              labelText: 'Enter your current password',
+              labelText: 'Current password',
             ),
             onChanged: (value) {
               password = value;
             },
+            onFieldSubmitted: (_) => newPasswordNode.requestFocus(),
             obscureText: true,
             validator: (value) {
               if (value.isEmpty) {
@@ -157,26 +116,125 @@ class _EditPasswordState extends State<EditPassword> {
     );
   }
 
-  Widget imageTitle() {
-    return Padding(
-      padding: const EdgeInsets.only(top: 70.0, bottom: 50.0),
-      child: Image.asset(
-        'assets/images/lock-${stateColors.iconExt}.png',
-        width: 100.0,
+  Widget helpCard() {
+    return Container(
+      padding: const EdgeInsets.only(
+        top: 60.0,
+        left: 25.0,
+        right: 25.0,
+      ),
+      child: Card(
+        child: ListTile(
+          contentPadding: const EdgeInsets.all(16.0),
+          leading: Icon(Icons.help_outline),
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return SimpleDialog(
+                  title: Text(
+                    'Recommandations',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  contentPadding: const EdgeInsets.all(25.0),
+                  children: <Widget>[
+                    Text(
+                      "Choose at least a 6-characters password length",
+                      style: TextStyle(
+                      ),
+                    ),
+                    Padding(padding: const EdgeInsets.only(top: 15.0)),
+                    Text(
+                      "Choose a pass-phrase",
+                      style: TextStyle(
+                      ),
+                    ),
+                    Padding(padding: const EdgeInsets.only(top: 15.0)),
+                    Text(
+                      "Use special characters (e.g. *!#?)",
+                      style: TextStyle(
+                      ),
+                    ),
+                  ],
+                );
+              }
+            );
+          },
+          title: Opacity(
+            opacity: .6,
+            child: Text(
+              'Choosing a good password',
+            ),
+          ),
+          subtitle: Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              "Resist against over brute-force attacks",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 14.0,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
 
-  Widget newPasswordInput() {
-    return Padding(
+  Widget idleView() {
+    final width = MediaQuery.of(context).size.width;
+    final inputWidth = width < 500.0 ? 260.0 : width;
+
+    return SliverList(
+      delegate: SliverChildListDelegate([
+        Column(
+          children: <Widget>[
+            FadeInY(
+              delay: delay + (0 * delayStep),
+              beginY: beginY,
+              child: helpCard(),
+            ),
+
+            FadeInY(
+              delay: delay + (1 * delayStep),
+              beginY: beginY,
+              child: currentPasswordInput(width: inputWidth),
+            ),
+
+            FadeInY(
+              delay: delay + (2 * delayStep),
+              beginY: beginY,
+              child: newPasswordInput(width: inputWidth),
+            ),
+
+            FadeInY(
+              delay: delay + (3 * delayStep),
+              beginY: beginY,
+              child: validationButton(),
+            ),
+
+            Padding(padding: const EdgeInsets.only(bottom: 200.0),),
+          ],
+        ),
+      ]),
+    );
+  }
+
+  Widget newPasswordInput({double width}) {
+    return Container(
+      width: width,
       padding: const EdgeInsets.only(top: 40.0, bottom: 80.0,),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           TextFormField(
+            focusNode: newPasswordNode,
             decoration: InputDecoration(
               icon: Icon(Icons.lock_outline),
-              labelText: 'Enter your new password',
+              labelText: 'New password',
             ),
             obscureText: true,
             onChanged: (value) {
@@ -205,23 +263,27 @@ class _EditPasswordState extends State<EditPassword> {
   }
 
   Widget updatingScreen() {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          CircularProgressIndicator(),
+    return SliverList(
+      delegate: SliverChildListDelegate([
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircularProgressIndicator(),
 
-          Padding(
-            padding: const EdgeInsets.only(top: 40.0),
-            child: Text(
-              'Updating your password...',
-              style: TextStyle(
-                fontSize: 20.0,
-              ),
-            ),
-          )
-        ],
-      ),
+              Padding(
+                padding: const EdgeInsets.only(top: 40.0),
+                child: Text(
+                  'Updating your password...',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ])
     );
   }
 
@@ -230,22 +292,33 @@ class _EditPasswordState extends State<EditPassword> {
       onPressed: () {
         updatePassword();
       },
-      color: stateColors.softBackground,
+      color: stateColors.primary,
       shape: RoundedRectangleBorder(
-        side: BorderSide(
-          color: stateColors.primary,
-        ),
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Text(
-          'Update',
+      child: SizedBox(
+        width: 240.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'UPDATE',
+                style: TextStyle(
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
         ),
       )
     );
   }
 
   void updatePassword() async {
+    if (!inputValuesOk()) { return; }
+
     setState(() {
       isUpdating = true;
     });
@@ -283,12 +356,35 @@ class _EditPasswordState extends State<EditPassword> {
         isUpdating = false;
       });
 
-      Scaffold.of(context).showSnackBar(
-        SnackBar(
-          backgroundColor: Colors.red,
-          content: Text('Error while updating your email. Please try again or contact us.'),
-        )
+      showSnack(
+        context: context,
+        message: 'Error while updating your password. Please try again or contact us.',
+        type: SnackType.error,
       );
     }
+  }
+
+  bool inputValuesOk() {
+    if (password.isEmpty) {
+      showSnack(
+        context: context,
+        message: "Password cannot be empty.",
+        type: SnackType.error,
+      );
+
+      return false;
+    }
+
+    if (newPassword.isEmpty) {
+      showSnack(
+        context: context,
+        message: "New password cannot be empty.",
+        type: SnackType.error,
+      );
+
+      return false;
+    }
+
+    return true;
   }
 }
