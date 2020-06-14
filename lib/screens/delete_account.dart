50a605403c9742293e87c86ba1d0dc859737b348
rootasjey <jeremiecorpinot@outlook.com>
2020-06-14 06:10:49 +0200

    👆(mobile) Add input check + 💄

--- lib/screens/delete_account.dart
+++ lib/screens/delete_account.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:memorare/components/web/fade_in_y.dart';
+import 'package:memorare/components/web/sliver_app_header.dart';
 import 'package:memorare/router/route_names.dart';
 import 'package:memorare/router/router.dart';
 import 'package:memorare/state/colors.dart';
@@ -28,180 +30,150 @@ class DeleteAccountState extends State<DeleteAccount> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(100.0),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 20.0),
-          child: AppBar(
-            backgroundColor: stateColors.softBackground,
-            centerTitle: true,
-            elevation: 2,
-            title: Text(
-              'Delete account',
-              style: TextStyle(
-                fontSize: 25.0,
-                color: stateColors.foreground,
-              ),
-            ),
-            leading: IconButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              icon: Icon(
-                Icons.arrow_back,
-                color: stateColors.foreground,
-              ),
-            ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppHeader(
+            title: 'Account deletion',
+            subTitle: 'Well, this marks the end of the adventure',
           ),
-        ),
+          body(),
+        ],
       ),
-      body: body(),
     );
   }
 
   Widget body() {
     if (isCompleted) {
-      return completedScreen();
+      return completedView();
     }
 
     if (isDeleting) {
-      return deletingSceen();
+      return deletingView();
     }
 
-    return ListView(
-      padding: const EdgeInsets.all(40.0),
-      children: [
-        Column(
-          children: <Widget>[
-            FadeInY(
-              delay: delay + (1 * delayStep),
-              beginY: beginY,
-              child: imageTitle(),
-            ),
-
-            FadeInY(
-              delay: delay + (2 * delayStep),
-              beginY: beginY,
-              child: descriptionText(),
-            ),
-
-            FadeInY(
-              delay: delay + (3 * delayStep),
-              beginY: beginY,
-              child: passwordInput(),
-            ),
-
-            FadeInY(
-              delay: delay + (4 * delayStep),
-              beginY: beginY,
-              child: validationButton(),
-            ),
-
-            Padding(padding: const EdgeInsets.only(bottom: 200.0),),
-          ],
-        ),
-      ],
-    );
+    return idleView();
   }
 
-  Widget completedScreen() {
-    return Container(
-      padding: const EdgeInsets.all(40.0),
-      child: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(top: 40.0),
-            child: Icon(
-              Icons.check_circle_outline,
-              size: 80.0,
-            ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.only(top: 30.0, bottom: 0.0),
-            child: Text(
-              'Your account has been successfuly deleted. \nWe hope to see you again.',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20.0,
+  Widget completedView() {
+    return SliverList(
+      delegate: SliverChildListDelegate([
+        Container(
+          padding: const EdgeInsets.all(40.0),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 40.0),
+                child: Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.green.shade300,
+                  size: 80.0,
+                ),
               ),
-            ),
-          ),
 
-          Padding(
-            padding: const EdgeInsets.only(top: 45.0,),
-            child: FlatButton(
-              onPressed: () {
-                FluroRouter.router.navigateTo(
-                  context,
-                  HomeRoute,
-                );
-              },
-              child: Opacity(
-                opacity: .6,
+              Padding(
+                padding: const EdgeInsets.only(top: 30.0,),
                 child: Text(
-                  'Back home',
+                  'Your account has been successfuly deleted',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
                 ),
               ),
-            ),
+
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0,),
+                child: Opacity(
+                  opacity: .6,
+                  child: Text(
+                    'We hope to see you again',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                    ),
+                  ),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(top: 45.0,),
+                child: FlatButton(
+                  onPressed: () {
+                    FluroRouter.router.navigateTo(
+                      context,
+                      HomeRoute,
+                    );
+                  },
+                  child: Opacity(
+                    opacity: .6,
+                    child: Text(
+                      'Back home',
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+      ])
     );
   }
 
-  Widget deletingSceen() {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          CircularProgressIndicator(),
+  Widget deletingView() {
+    return SliverList(
+      delegate: SliverChildListDelegate([
+        Container(
+          padding: const EdgeInsets.only(top: 100.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircularProgressIndicator(),
 
-          Padding(
-            padding: const EdgeInsets.only(top: 40.0),
-            child: Text(
-              'Deleting your data...',
-              style: TextStyle(
-                fontSize: 20.0,
-              ),
-            ),
-          )
-        ],
-      ),
+              Padding(
+                padding: const EdgeInsets.only(top: 40.0),
+                child: Text(
+                  'Deleting your data...',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ]),
     );
   }
 
-  Widget descriptionText() {
-    return Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: Column(
-        children: <Widget>[
-          Opacity(
-            opacity: .8,
-            child: Text(
-              'Are you sure you want to delete your account?',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 25.0,
+  Widget idleView() {
+    return SliverList(
+      delegate: SliverChildListDelegate([
+        Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Column(
+            children: <Widget>[
+              FadeInY(
+                delay: delay + (0 * delayStep),
+                beginY: beginY,
+                child: warningCard(),
               ),
-            ),
-          ),
 
-          Padding(
-            padding: const EdgeInsets.only(top: 30.0),
-            child: Opacity(
-              opacity: .6,
-              child: Text(
-                '• This action is irreversible\n• All your data will be whiped\n• Your published quotes will be kept',
-                style: TextStyle(
-                  fontSize: 18.0,
-                  height: 1.4,
-                ),
+              FadeInY(
+                delay: delay + (.5 * delayStep),
+                beginY: beginY,
+                child: passwordInput(),
               ),
-            ),
+
+              FadeInY(
+                delay: delay + (1 * delayStep),
+                beginY: beginY,
+                child: validationButton(),
+              ),
+
+              Padding(padding: const EdgeInsets.only(bottom: 200.0),),
+            ],
           ),
-        ],
-      ),
+        )
+      ]),
     );
   }
 
@@ -260,22 +232,106 @@ class DeleteAccountState extends State<DeleteAccount> {
       onPressed: () {
         deleteAccount();
       },
-      color: stateColors.softBackground,
+      color: stateColors.primary,
       shape: RoundedRectangleBorder(
-        side: BorderSide(
-          color: stateColors.primary,
+        borderRadius: BorderRadius.all(
+          Radius.circular(7.0),
         ),
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Text(
-          'Delete',
+      child: SizedBox(
+        width: 240.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Text(
+                'DELETE ACCOUNT',
+                style: TextStyle(
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
         ),
-      )
+      ),
+    );
+  }
+
+  Widget warningCard() {
+    return Card(
+      child: ListTile(
+        contentPadding: const EdgeInsets.all(16.0),
+        leading: Icon(Icons.warning),
+        title: Opacity(
+          opacity: .6,
+          child: Text(
+            'Are you sure?',
+          ),
+        ),
+        subtitle: Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: Text(
+            "This action is irreversible",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 14.0,
+            ),
+          ),
+        ),
+        onTap: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return SimpleDialog(
+                title: Text(
+                  'What happens after?',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                // contentPadding: const EdgeInsets.all(25.0),
+                children: <Widget>[
+                  Divider(),
+                  Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Your personal data will be deleted",
+                          style: TextStyle(
+                          ),
+                        ),
+                        Padding(padding: const EdgeInsets.only(top: 15.0)),
+                        Text(
+                          "Your published quotes will stay",
+                          style: TextStyle(
+                          ),
+                        ),
+                        Padding(padding: const EdgeInsets.only(top: 15.0)),
+                        Text(
+                          "Your username will (slowly) be dissaciated with the published quotes",
+                          style: TextStyle(
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            }
+          );
+        },
+      ),
     );
   }
 
   void deleteAccount() async {
+    if (!inputValuesOk()) { return; }
+
     setState(() {
       isDeleting = true;
     });
@@ -324,9 +380,23 @@ class DeleteAccountState extends State<DeleteAccount> {
 
       showSnack(
         context: context,
-        message: 'Error while deleting your account. Please try again or contact us.',
+        message: (error as PlatformException).message,
         type: SnackType.error,
       );
     }
+  }
+
+  bool inputValuesOk() {
+    if (password.isEmpty) {
+      showSnack(
+        context: context,
+        message: "Password cannot be empty.",
+        type: SnackType.error,
+      );
+
+      return false;
+    }
+
+    return true;
   }
 }
