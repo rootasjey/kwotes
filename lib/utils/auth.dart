afbd33a68cf80c3384194788dfabe84f45239448
rootasjey <jeremiecorpinot@outlook.com>
2020-07-03 14:35:40 +0200

    ✂️ Factorize checkAuth()

--- lib/utils/auth.dart
+++ lib/utils/auth.dart
@@ -1,6 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
+import 'package:memorare/router/route_names.dart';
+import 'package:memorare/router/router.dart';
+import 'package:memorare/state/user_state.dart';
 import 'package:memorare/utils/app_localstorage.dart';
 import 'package:supercharged/supercharged.dart';
+
+void checkAuth({BuildContext context}) async {
+  try {
+    final userAuth = await userState.userAuth;
+
+    if (userAuth == null) {
+      FluroRouter.router.navigateTo(context, SigninRoute, replace: true,);
+      return;
+    }
+
+  } catch (error) {
+    FluroRouter.router.navigateTo(context, SigninRoute, replace: true,);
+  }
+}
 
 Future<FirebaseUser> getUserAuth() async {
   FirebaseUser userAuth = await tryGetUserAuth();
