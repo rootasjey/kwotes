9c32a0cca90dafda55373c7ae523e92e03747a9f
rootasjey <jeremiecorpinot@outlook.com>
2020-07-03 15:02:01 +0200

    👆 Sticky header for small screens (del button)

--- lib/screens/quotidians.dart
+++ lib/screens/quotidians.dart
@@ -131,7 +131,7 @@ class QuotidiansState extends State<Quotidians> {
         appBar(),
 
         if (maxWidth < 600.0) customScrollViewChild(),
-        if (maxWidth >= 600.0) ...groupsGrid(),
+        if (maxWidth >= 600.0) ...groupedGrids(),
       ],
     );
   }
@@ -149,7 +149,7 @@ class QuotidiansState extends State<Quotidians> {
       return errorView();
     }
 
-    return groupsList();
+    return groupedLists();
   }
 
   Widget emptyView() {
@@ -190,64 +190,102 @@ class QuotidiansState extends State<Quotidians> {
     );
   }
 
-  List<Widget> groupsGrid() {
-    final Map<String, List<Quotidian>> groups = quotidians.groupBy(
-      (quotidian) => '${quotidian.date.year}-${quotidian.date.month}',
-    );
+  List<Widget> groupedGrid(String yearMonth, List<Quotidian> grouped) {
+    final headerKey = GlobalKey();
 
-    final List<Widget> groupedGrids = [];
-
-    groups.forEach((yearMonth, groupedQuotidians) {
-      final grid = groupGrid(yearMonth, groupedQuotidians);
-      groupedGrids.addAll(grid);
-    });
-
-    return groupedGrids;
-  }
-
-  Widget groupsList() {
-    final Map<String, List<Quotidian>> groups = quotidians.groupBy(
-      (quotidian) => '${quotidian.date.year}-${quotidian.date.month}',
-    );
-
-    final List<Widget> groupedList = [];
-
-    groups.forEach((yearMonth, groupedQuotidians) {
-      final singleGroup = monthGroup(yearMonth, groupedQuotidians);
-      groupedList.addAll(singleGroup);
-    });
-
-    return SliverList(
-      delegate: SliverChildListDelegate(groupedList),
-    );
-  }
-
-  Widget loadingView() {
-    return SliverList(
-      delegate: SliverChildListDelegate([
-          Padding(
-            padding: const EdgeInsets.only(top: 200.0),
-            child: LoadingAnimation(),
+    return [
+      SliverStickyHeader(
+        header: headerGroup(
+          headerKey: headerKey,
+          yearMonth: yearMonth,
+          group: grouped,
+        ),
+        sliver: SliverPadding(
+          key: headerKey,
+          padding: const EdgeInsets.all(10.0),
+          sliver: SliverGrid(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 300.0,
+              crossAxisSpacing: 10.0,
+              mainAxisSpacing: 10.0,
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                final quote = grouped.elementAt(index);
+                return itemGrid(quote);
+              },
+              childCount: grouped.length,
+            ),
           ),
-        ]
+        ),
       ),
-    );
+    ];
   }
 
-  List<Widget> monthGroup(String yearMonth, List<Quotidian> group) {
-    final splittedDate = yearMonth.split('-');
+  List<Widget> groupedGrids() {
+    final Map<String, List<Quotidian>> groups = quotidians.groupBy(
+      (quotidian) => '${quotidian.date.year}-${quotidian.date.month}',
+    );
 
-    final year = splittedDate[0];
-    final month = getMonthFromNumber(splittedDate[1].toInt());
+    final List<Widget> widgets = [];
 
+    groups.forEach((yearMonth, groupedQuotidians) {
+      final grid = groupedGrid(yearMonth, groupedQuotidians);
+      widgets.addAll(grid);
+    });
+
+    return widgets;
+  }
+
+  List<Widget> groupedList(String yearMonth, List<Quotidian> group) {
     final headerKey = GlobalKey();
 
     return [
       StickyHeader(
         key: headerKey,
-        header: Container(
-          color: stateColors.softBackground,
-          child: FlatButton(
+        header: headerGroup(
+          headerKey: headerKey,
+          yearMonth: yearMonth,
+          group: group,
+        ),
+        content: itemList(group),
+      ),
+    ];
+  }
+
+  Widget groupedLists() {
+    final Map<String, List<Quotidian>> groups = quotidians.groupBy(
+      (quotidian) => '${quotidian.date.year}-${quotidian.date.month}',
+    );
+
+    final List<Widget> widgets = [];
+
+    groups.forEach((yearMonth, groupedQuotidians) {
+      final singleGroup = groupedList(yearMonth, groupedQuotidians);
+      widgets.addAll(singleGroup);
+    });
+
+    return SliverList(
+      delegate: SliverChildListDelegate(widgets),
+    );
+  }
+
+  Widget headerGroup({
+    GlobalKey<State<StatefulWidget>> headerKey,
+    String yearMonth,
+    List<Quotidian> group,
+  }) {
+
+    final splittedDate = yearMonth.split('-');
+
+    final year = splittedDate[0];
+    final month = getMonthFromNumber(splittedDate[1].toInt());
+
+    return Container(
+      color: stateColors.softBackground,
+      child: Stack(
+        children: <Widget>[
+          FlatButton(
             onPressed: () {
               final renderObject = headerKey.currentContext.findRenderObject();
               renderObject.showOnScreen(duration: 1.seconds);
@@ -273,93 +311,191 @@ class QuotidiansState extends State<Quotidians> {
               ),
             ),
           ),
-        ),
-        content: singleGroupList(group),
+
+          Positioned(
+            right: 0.0,
+            child: FlatButton(
+              onPressed: () {
+                deleteMonthDialog(yearMonth, group,);
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 25.0,
+                ),
+                child: Icon(Icons.delete,),
+              ),
+            ),
+          ),
+        ],
       ),
-    ];
+    );
   }
 
-  List<Widget> groupGrid(String yearMonth, List<Quotidian> grouped) {
-    final splittedDate = yearMonth.split('-');
+  Widget itemGrid(Quotidian quotidian) {
+    final quote = quotidian.quote;
+    final topicColor = appTopicsColors.find(quote.topics.first);
 
-    final year = splittedDate[0];
-    final month = getMonthFromNumber(splittedDate[1].toInt());
+    return QuoteCard(
+      onTap: () {
+        FluroRouter.router.navigateTo(
+          context,
+          QuotePageRoute.replaceFirst(':id', quotidian.id)
+        );
+      },
+      title: quote.name,
+      popupMenuButton: PopupMenuButton<String>(
+        icon: Icon(
+          Icons.more_horiz,
+          color: Color(topicColor.decimal),
+        ),
+        onSelected: (value) {
+          if (value == 'delete') {
+            deleteAction(quotidian);
+            return;
+          }
+        },
+        itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+          PopupMenuItem(
+            value: 'delete',
+            child: ListTile(
+              leading: Icon(Icons.delete),
+              title: Text('Delete'),
+            )
+          ),
+        ],
+      ),
+      stackChildren: <Widget>[
+        Positioned(
+          bottom: 15.0,
+          right: 60.0,
+          child: Text(
+            quotidian.date.day.toString(),
+          ),
+        ),
+      ],
+    );
+  }
 
-    final headerKey = GlobalKey();
+  /// Contains multiple children in a Column.
+  Widget itemList(List<Quotidian> group) {
+    return Column(
+      children: group.map((quotidian) {
+        final topicColor = appTopicsColors
+          .find(quotidian.quote.topics.first);
 
-    return [
-      SliverStickyHeader(
-        header: Container(
-          color: stateColors.softBackground,
-          child: Stack(
+        return InkWell(
+          onTap: () {
+            FluroRouter.router.navigateTo(
+              context,
+              QuotePageRoute.replaceFirst(':id', quotidian.quote.id),
+            );
+          },
+          onLongPress: () => showQuoteSheet(quotidian: quotidian),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              FlatButton(
-                onPressed: () {
-                  final renderObject = headerKey.currentContext.findRenderObject();
-                  renderObject.showOnScreen(duration: 1.seconds);
-                },
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    top: 35.0,
-                    bottom: 10.0,
-                  ),
-                  child: Center(
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                          '$month $year',
-                        ),
+              Padding(padding: const EdgeInsets.only(top: 20.0),),
 
-                        SizedBox(
-                          width: 100.0,
-                          child: Divider(thickness: 2,),
-                        ),
-                      ],
-                    ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  quotidian.quote.name,
+                  style: TextStyle(
+                    fontSize: 20,
                   ),
                 ),
               ),
 
-              Positioned(
-                right: 20.0,
-                child: FlatButton(
-                  onPressed: () {
-                    showDeleteMonthDialog(yearMonth, grouped,);
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 25.0,
-                    ),
-                    child: Icon(Icons.delete,),
+              Center(
+                child: IconButton(
+                  onPressed: () => showQuoteSheet(quotidian: quotidian),
+                  icon: Icon(
+                    Icons.more_horiz,
+                    color: topicColor != null ?
+                    Color(topicColor.decimal) : stateColors.primary,
                   ),
                 ),
               ),
+
+              Center(
+                child: Opacity(
+                  opacity: .6,
+                  child: Text(
+                    quotidian.date.day.toString(),
+                  ),
+                ),
+              ),
+
+              Padding(padding: const EdgeInsets.only(top: 10.0),),
+              Divider(),
             ],
           ),
-        ),
-        sliver: SliverPadding(
-          key: headerKey,
-          padding: const EdgeInsets.all(10.0),
-          sliver: SliverGrid(
-            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 300.0,
-              crossAxisSpacing: 10.0,
-              mainAxisSpacing: 10.0,
-            ),
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                final quote = grouped.elementAt(index);
-                return itemGrid(quote);
-              },
-              childCount: grouped.length,
-            ),
-          ),
-        ),
-      ),
-    ];
+        );
+      }).toList()
+    );
   }
 
-  void showDeleteMonthDialog(String yearMonth, List<Quotidian> grouped) {
+  Widget loadingView() {
+    return SliverList(
+      delegate: SliverChildListDelegate([
+          Padding(
+            padding: const EdgeInsets.only(top: 200.0),
+            child: LoadingAnimation(),
+          ),
+        ]
+      ),
+    );
+  }
+
+  void deleteAction(Quotidian quotidian) async {
+    try {
+      await Firestore.instance
+        .collection('quotidians')
+        .document(quotidian.id)
+        .delete();
+
+      setState(() {
+        quotidians.removeWhere((element) => element.id == quotidian.id);
+      });
+
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.green,
+          content: Text(
+            'The quotidian has been successfully deleted.'
+          ),
+        )
+      );
+
+    } catch (error) {
+      debugPrint(error.toString());
+
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            'Sorry, an error occurred while deleting the quotidian.'
+          ),
+        )
+      );
+    }
+  }
+
+  void deleteMonth(List<Quotidian> group) async {
+    // NOTE: maybe do this job in a cloud function
+    group.forEach((quotidian) async {
+      await Firestore.instance
+        .collection('quotidians')
+        .document(quotidian.id)
+        .delete();
+
+      setState(() {
+        quotidians.removeWhere((element) => element.id == quotidian.id);
+      });
+    });
+
+  }
+
+  void deleteMonthDialog(String yearMonth, List<Quotidian> grouped) {
     showDialog(
       context: context,
       builder: (context) {
@@ -444,157 +580,6 @@ class QuotidiansState extends State<Quotidians> {
         );
       }
     );
-  }
-
-  Widget itemGrid(Quotidian quotidian) {
-    final quote = quotidian.quote;
-    final topicColor = appTopicsColors.find(quote.topics.first);
-
-    return QuoteCard(
-      onTap: () {
-        FluroRouter.router.navigateTo(
-          context,
-          QuotePageRoute.replaceFirst(':id', quotidian.id)
-        );
-      },
-      title: quote.name,
-      popupMenuButton: PopupMenuButton<String>(
-        icon: Icon(
-          Icons.more_horiz,
-          color: Color(topicColor.decimal),
-        ),
-        onSelected: (value) {
-          if (value == 'delete') {
-            deleteAction(quotidian);
-            return;
-          }
-        },
-        itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-          PopupMenuItem(
-            value: 'delete',
-            child: ListTile(
-              leading: Icon(Icons.delete),
-              title: Text('Delete'),
-            )
-          ),
-        ],
-      ),
-      stackChildren: <Widget>[
-        Positioned(
-          bottom: 15.0,
-          right: 60.0,
-          child: Text(
-            quotidian.date.day.toString(),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget singleGroupList(List<Quotidian> group) {
-    return Column(
-      children: group.map((quotidian) {
-        final topicColor = appTopicsColors
-          .find(quotidian.quote.topics.first);
-
-        return InkWell(
-          onTap: () {
-            FluroRouter.router.navigateTo(
-              context,
-              QuotePageRoute.replaceFirst(':id', quotidian.quote.id),
-            );
-          },
-          onLongPress: () => showQuoteSheet(quotidian: quotidian),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(padding: const EdgeInsets.only(top: 20.0),),
-
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(
-                  quotidian.quote.name,
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-
-              Center(
-                child: IconButton(
-                  onPressed: () => showQuoteSheet(quotidian: quotidian),
-                  icon: Icon(
-                    Icons.more_horiz,
-                    color: topicColor != null ?
-                    Color(topicColor.decimal) : stateColors.primary,
-                  ),
-                ),
-              ),
-
-              Center(
-                child: Opacity(
-                  opacity: .6,
-                  child: Text(
-                    quotidian.date.day.toString(),
-                  ),
-                ),
-              ),
-
-              Padding(padding: const EdgeInsets.only(top: 10.0),),
-              Divider(),
-            ],
-          ),
-        );
-      }).toList()
-    );
-  }
-
-  void deleteAction(Quotidian quotidian) async {
-    try {
-      await Firestore.instance
-        .collection('quotidians')
-        .document(quotidian.id)
-        .delete();
-
-      setState(() {
-        quotidians.removeWhere((element) => element.id == quotidian.id);
-      });
-
-      Scaffold.of(context).showSnackBar(
-        SnackBar(
-          backgroundColor: Colors.green,
-          content: Text(
-            'The quotidian has been successfully deleted.'
-          ),
-        )
-      );
-
-    } catch (error) {
-      debugPrint(error.toString());
-
-      Scaffold.of(context).showSnackBar(
-        SnackBar(
-          content: Text(
-            'Sorry, an error occurred while deleting the quotidian.'
-          ),
-        )
-      );
-    }
-  }
-
-  void deleteMonth(List<Quotidian> group) async {
-    // NOTE: maybe do this job in a cloud function
-    group.forEach((quotidian) async {
-      await Firestore.instance
-        .collection('quotidians')
-        .document(quotidian.id)
-        .delete();
-
-      setState(() {
-        quotidians.removeWhere((element) => element.id == quotidian.id);
-      });
-    });
-
   }
 
   Future fetch() async {
