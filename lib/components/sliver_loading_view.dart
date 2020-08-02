3e73ec0d4832fb77190e234a695af82f0382a1eb
rootasjey <jeremiecorpinot@outlook.com>
2020-08-02 22:23:42 +0200

    ➕ Add SliverLoadingView

---
+++ lib/components/sliver_loading_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:memorare/components/loading_animation.dart';
+
+class SliverLoadingView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildListDelegate([
+        Padding(
+          padding: const EdgeInsets.only(top: 200.0),
+          child: LoadingAnimation(),
+        ),
+      ]),
+    );
+  }
+}
