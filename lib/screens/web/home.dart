cc398abc140f11d5ffbdbf526c3d4a4ed53c28b4
rootasjey <jeremiecorpinot@outlook.com>
2020-08-05 15:49:54 +0200

    ➖ Delete FAB (add new quote) on Home

--- lib/screens/web/home.dart
+++ lib/screens/web/home.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:memorare/components/web/discover.dart';
 import 'package:memorare/components/web/footer.dart';
 import 'package:memorare/components/web/full_page_quotidian.dart';
 import 'package:memorare/components/web/home_app_bar.dart';
 import 'package:memorare/components/web/topics.dart';
-import 'package:memorare/data/add_quote_inputs.dart';
-import 'package:memorare/router/route_names.dart';
-import 'package:memorare/router/router.dart';
-import 'package:memorare/state/colors.dart';
-import 'package:memorare/state/user_state.dart';
 import 'package:supercharged/supercharged.dart';
 
 class Home extends StatefulWidget {
@@ -23,25 +17,6 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: Observer(
-        builder: (context) {
-          if (userState.isUserConnected) {
-            return FloatingActionButton.extended(
-              onPressed: () {
-                AddQuoteInputs.clearAll();
-                AddQuoteInputs.navigatedFromPath = 'dashboard';
-                FluroRouter.router.navigateTo(context, AddQuoteContentRoute);
-              },
-              backgroundColor: stateColors.primary,
-              foregroundColor: Colors.white,
-              icon: Icon(Icons.add),
-              label: Text('Propose new quote'),
-            );
-          }
-
-          return Padding(padding: EdgeInsets.zero,);
-        }
-      ),
       body: CustomScrollView(
         controller: scrollController,
         slivers: <Widget>[
