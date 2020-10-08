11b9dcfadb64e63377e5842ee7987aa18dd5331b
rootasjey <rootasjey@users.noreply.github.com>
2020-10-08 14:54:24 +0200

    ☝️ Update discover (mobile) page

--- lib/screens/discover.dart
+++ lib/screens/discover.dart
@@ -1,11 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:memorare/components/loading_animation.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:memorare/actions/share.dart';
+import 'package:memorare/components/error_container.dart';
+import 'package:memorare/components/reference_row.dart';
+import 'package:memorare/components/simple_appbar.dart';
+import 'package:memorare/components/sliver_loading_view.dart';
+import 'package:memorare/components/web/app_icon_header.dart';
+import 'package:memorare/components/web/empty_content.dart';
 import 'package:memorare/components/web/reference_card.dart';
 import 'package:memorare/components/web/fade_in_y.dart';
+import 'package:memorare/state/colors.dart';
+import 'package:memorare/types/enums.dart';
 import 'package:memorare/types/reference.dart';
-
-List<Reference> _references = [];
+import 'package:memorare/utils/app_localstorage.dart';
+import 'package:supercharged/supercharged.dart';
 
 class Discover extends StatefulWidget {
   @override
@@ -13,14 +22,29 @@ class Discover extends StatefulWidget {
 }
 
 class _DiscoverState extends State<Discover> {
-  String lang = 'en';
+  bool descending = true;
+  bool hasNext = true;
+  bool hasErrors = false;
   bool isLoading = false;
+  bool isLoadingMore = false;
+
+  DocumentSnapshot lastDoc;
+
+  final limit = 30;
+  final scrollController = ScrollController();
+  final pageRoute = 'DiscoverRoute';
+
+  ItemsLayout itemsStyle;
+  List<Reference> references = [];
+
+  String lang = 'en';
 
   @override
   void initState() {
     super.initState();
+    getSavedProps();
 
-    if (_references.length > 0) {
+    if (references.length > 0) {
       return;
     }
 
@@ -30,62 +54,320 @@ class _DiscoverState extends State<Discover> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Builder(
-        builder: (BuildContext context) {
-          if (isLoading) {
-            return LoadingAnimation(
-              textTitle: 'Loading Discover section...',
-            );
-          }
+      body: RefreshIndicator(
+          onRefresh: () async {
+            await fetch();
+            return null;
+          },
+          child: NotificationListener<ScrollNotification>(
+            onNotification: (ScrollNotification scrollNotif) {
+              if (scrollNotif.metrics.pixels <
+                  scrollNotif.metrics.maxScrollExtent) {
+                return false;
+              }
 
-          return RefreshIndicator(
-            onRefresh: () async {
-              await fetch();
-              return null;
+              if (hasNext && !isLoadingMore) {
+                fetchMore();
+              }
+
+              return false;
             },
-            child: ListView(
-              padding: EdgeInsets.only(
-                top: 50.0,
-                bottom: 200.0,
-              ),
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
-                  child: Text(
-                    'Discover',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Opacity(
-                    opacity: .6,
-                    child: Text(
-                      'Do you know these references?',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  )
-                ),
-
-                Divider(height: 60.0,),
-
-                Wrap(
-                  spacing: 10.0,
-                  runSpacing: 10.0,
-                  alignment: WrapAlignment.center,
-                  children: cardsList(),
-                ),
+            child: CustomScrollView(
+              controller: scrollController,
+              slivers: <Widget>[
+                appBar(),
+                body(),
               ],
             ),
+          )),
+    );
+  }
+
+  Widget appBar() {
+    return SimpleAppBar(
+      title: TextButton.icon(
+        onPressed: () {
+          scrollController.animateTo(
+            0,
+            duration: 250.milliseconds,
+            curve: Curves.easeIn,
           );
         },
+        icon: AppIconHeader(
+          padding: EdgeInsets.zero,
+          size: 30.0,
+        ),
+        label: Text(
+          'Discover',
+          style: TextStyle(
+            fontSize: 22.0,
+          ),
+        ),
+      ),
+      showNavBackIcon: false,
+      subHeader: Observer(
+        builder: (context) {
+          return Wrap(
+            spacing: 10.0,
+            children: <Widget>[
+              FadeInY(
+                beginY: 10.0,
+                delay: 2.0,
+                child: ChoiceChip(
+                  label: Text(
+                    'First added',
+                    style: TextStyle(
+                      color:
+                          !descending ? Colors.white : stateColors.foreground,
+                    ),
+                  ),
+                  tooltip: 'Order by first added',
+                  selected: !descending,
+                  selectedColor: stateColors.primary,
+                  onSelected: (selected) {
+                    if (!descending) {
+                      return;
+                    }
+
+                    descending = false;
+                    fetch();
+
+                    appLocalStorage.setPageOrder(
+                      descending: descending,
+                      pageRoute: pageRoute,
+                    );
+                  },
+                ),
+              ),
+              FadeInY(
+                beginY: 10.0,
+                delay: 2.5,
+                child: ChoiceChip(
+                  label: Text(
+                    'Last added',
+                    style: TextStyle(
+                      color: descending ? Colors.white : stateColors.foreground,
+                    ),
+                  ),
+                  tooltip: 'Order by most recently added',
+                  selected: descending,
+                  selectedColor: stateColors.primary,
+                  onSelected: (selected) {
+                    if (descending) {
+                      return;
+                    }
+
+                    descending = true;
+                    fetch();
+
+                    appLocalStorage.setPageOrder(
+                      descending: descending,
+                      pageRoute: pageRoute,
+                    );
+                  },
+                ),
+              ),
+              FadeInY(
+                beginY: 10.0,
+                delay: 3.0,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 10.0,
+                    left: 20.0,
+                    right: 20.0,
+                  ),
+                  child: Container(
+                    height: 25,
+                    width: 2.0,
+                    color: stateColors.foreground.withOpacity(0.5),
+                  ),
+                ),
+              ),
+              FadeInY(
+                beginY: 10.0,
+                delay: 3.5,
+                child: IconButton(
+                  onPressed: () {
+                    if (itemsStyle == ItemsLayout.list) {
+                      return;
+                    }
+
+                    setState(() {
+                      itemsStyle = ItemsLayout.list;
+                    });
+
+                    appLocalStorage.saveItemsStyle(
+                      pageRoute: pageRoute,
+                      style: ItemsLayout.list,
+                    );
+                  },
+                  icon: Icon(Icons.list),
+                  color: itemsStyle == ItemsLayout.list
+                      ? stateColors.primary
+                      : stateColors.foreground.withOpacity(0.5),
+                ),
+              ),
+              FadeInY(
+                beginY: 10.0,
+                delay: 3.5,
+                child: IconButton(
+                  onPressed: () {
+                    if (itemsStyle == ItemsLayout.grid) {
+                      return;
+                    }
+
+                    setState(() {
+                      itemsStyle = ItemsLayout.grid;
+                    });
+
+                    appLocalStorage.saveItemsStyle(
+                      pageRoute: pageRoute,
+                      style: ItemsLayout.grid,
+                    );
+                  },
+                  icon: Icon(Icons.grid_on),
+                  color: itemsStyle == ItemsLayout.grid
+                      ? stateColors.primary
+                      : stateColors.foreground.withOpacity(0.5),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  Widget body() {
+    if (isLoading) {
+      return SliverLoadingView();
+    }
+
+    if (!isLoading && hasErrors) {
+      return errorView();
+    }
+
+    if (references.length == 0) {
+      return emptyView();
+    }
+
+    if (itemsStyle == ItemsLayout.grid) {
+      return sliverGrid();
+    }
+
+    return sliverList();
+  }
+
+  Widget emptyView() {
+    return SliverList(
+      delegate: SliverChildListDelegate([
+        FadeInY(
+          delay: 2.0,
+          beginY: 50.0,
+          child: EmptyContent(
+            icon: Opacity(
+              opacity: .8,
+              child: Icon(
+                Icons.sentiment_neutral,
+                size: 120.0,
+                color: Color(0xFFFF005C),
+              ),
+            ),
+            title: "There's was an issue while loading discover page",
+            subtitle: 'Check your connection an try to refresh this page',
+            onRefresh: () => fetch(),
+          ),
+        ),
+      ]),
+    );
+  }
+
+  Widget errorView() {
+    return SliverList(
+      delegate: SliverChildListDelegate([
+        Padding(
+          padding: const EdgeInsets.only(top: 150.0),
+          child: ErrorContainer(
+            onRefresh: () => fetch(),
+          ),
+        ),
+      ]),
+    );
+  }
+
+  Widget sliverGrid() {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20.0,
+      ),
+      sliver: SliverGrid(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200.0,
+          childAspectRatio: 0.5,
+          mainAxisSpacing: 20.0,
+          crossAxisSpacing: 20.0,
+        ),
+        delegate: SliverChildBuilderDelegate(
+          (BuildContext context, int index) {
+            final reference = references[index];
+
+            return ReferenceCard(
+              height: 200.0,
+              width: 140.0,
+              id: reference.id,
+              imageUrl: reference.urls.image,
+              name: reference.name,
+              itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                PopupMenuItem(
+                    value: 'share',
+                    child: ListTile(
+                      leading: Icon(Icons.share),
+                      title: Text('Share'),
+                    )),
+              ],
+              onSelected: (value) {
+                if (value == 'share') {
+                  shareReference(context: context, reference: reference);
+                  return;
+                }
+              },
+            );
+          },
+          childCount: references.length,
+        ),
+      ),
+    );
+  }
+
+  Widget sliverList() {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          final reference = references.elementAt(index);
+
+          return ReferenceRow(
+            reference: reference,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 30.0,
+            ),
+            itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+              PopupMenuItem(
+                  value: 'share',
+                  child: ListTile(
+                    leading: Icon(Icons.share),
+                    title: Text('Share'),
+                  )),
+            ],
+            onSelected: (value) {
+              if (value == 'share') {
+                shareReference(context: context, reference: reference);
+                return;
+              }
+            },
+          );
+        },
+        childCount: references.length,
       ),
     );
   }
@@ -94,23 +376,21 @@ class _DiscoverState extends State<Discover> {
     List<Widget> cards = [];
     double index = 0;
 
-    for (var reference in _references) {
-      cards.add(
-        FadeInY(
-          delay: index,
-          beginY: 100.0,
-          child: ReferenceCard(
-            elevation     : 5.0,
-            height        : 240.0,
-            id            : reference.id,
-            imageUrl      : reference.urls.image,
-            name          : reference.name,
-            titleFontSize : 15.0,
-            type          : 'reference',
-            width         : 170.0,
-          ),
-        )
-      );
+    for (var reference in references) {
+      cards.add(FadeInY(
+        delay: index,
+        beginY: 100.0,
+        child: ReferenceCard(
+          elevation: 5.0,
+          height: 240.0,
+          id: reference.id,
+          imageUrl: reference.urls.image,
+          name: reference.name,
+          titleFontSize: 15.0,
+          type: 'reference',
+          width: 170.0,
+        ),
+      ));
 
       index += 1.0;
     }
@@ -119,26 +399,34 @@ class _DiscoverState extends State<Discover> {
   }
 
   Future fetch() async {
-    _references.clear();
-
     setState(() {
+      references.clear();
       isLoading = true;
     });
 
     try {
-      final refsSnapshot = await Firestore.instance
-        .collection('references')
-        .orderBy('updatedAt', descending: true)
-        .limit(4)
-        .getDocuments();
+      final snapshot = await Firestore.instance
+          .collection('references')
+          .orderBy('updatedAt', descending: descending)
+          .limit(limit)
+          .getDocuments();
 
-      if (refsSnapshot.documents.isNotEmpty) {
-        refsSnapshot.documents.forEach((doc) {
+      if (snapshot.documents.isEmpty) {
+        setState(() {
+          hasNext = false;
+          isLoading = false;
+        });
+
+        return;
+      }
+
+      if (snapshot.documents.isNotEmpty) {
+        snapshot.documents.forEach((doc) {
           final data = doc.data;
           data['id'] = doc.documentID;
 
           final ref = Reference.fromJSON(data);
-          _references.add(ref);
+          references.add(ref);
         });
       }
 
@@ -148,8 +436,9 @@ class _DiscoverState extends State<Discover> {
 
       setState(() {
         isLoading = false;
+        hasNext = snapshot.documents.isNotEmpty;
+        lastDoc = snapshot.documents.last;
       });
-
     } catch (error) {
       debugPrint(error.toString());
 
@@ -157,5 +446,51 @@ class _DiscoverState extends State<Discover> {
         isLoading = false;
       });
     }
+  }
+
+  Future fetchMore() async {
+    if (lastDoc == null) {
+      return;
+    }
+
+    isLoadingMore = true;
+
+    try {
+      final snapshot = await Firestore.instance
+          .collection('references')
+          .orderBy('updatedAt', descending: descending)
+          .limit(limit)
+          .getDocuments();
+
+      if (snapshot.documents.isEmpty) {
+        hasNext = false;
+        isLoadingMore = false;
+        return;
+      }
+
+      if (snapshot.documents.isNotEmpty) {
+        snapshot.documents.forEach((doc) {
+          final data = doc.data;
+          data['id'] = doc.documentID;
+
+          final ref = Reference.fromJSON(data);
+          references.add(ref);
+        });
+      }
+
+      setState(() {
+        isLoadingMore = false;
+        hasNext = snapshot.documents.isNotEmpty;
+        lastDoc = snapshot.documents.last;
+      });
+    } catch (error) {
+      debugPrint(error.toString());
+    }
+  }
+
+  void getSavedProps() {
+    lang = appLocalStorage.getPageLang(pageRoute: pageRoute);
+    descending = appLocalStorage.getPageOrder(pageRoute: pageRoute);
+    itemsStyle = appLocalStorage.getItemsStyle(pageRoute);
   }
 }
