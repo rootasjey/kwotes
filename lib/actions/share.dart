7b09befe40b5b8f559895448881d01cc93cc0c82
rootasjey <rootasjey@users.noreply.github.com>
2020-10-08 13:16:08 +0200

    ☝️ Rename function: shareQuoteWeb

--- lib/actions/share.dart
+++ lib/actions/share.dart
@@ -51,38 +51,14 @@ void shareAuthorWeb({@required Author author}) async {
 
 void shareQuote({@required BuildContext context, @required Quote quote}) {
   if (kIsWeb) {
-    shareQuoteTwitter(quote: quote);
+    shareQuoteWeb(quote: quote);
     return;
   }
 
-  shareQuoteFromMobile(quote: quote, context: context);
+  shareQuoteMobile(quote: quote, context: context);
 }
 
-/// Sahre the target quote to twitter.
-Future shareQuoteTwitter({@required Quote quote}) async {
-  final quoteName = quote.name;
-  final authorName = quote.author?.name ?? '';
-  final referenceName = quote.mainReference?.name ?? '';
-
-  String sharingText = quoteName;
-
-  if (authorName.isNotEmpty) {
-    sharingText += ' — $authorName';
-  }
-
-  if (referenceName != null && referenceName.length > 0) {
-    sharingText += ' — $referenceName';
-  }
-
-  final hashtags = '&hashtags=outofcontext';
-
-  final url =
-      'https://twitter.com/intent/tweet?via=outofcontextapp&text=$sharingText$hashtags';
-  await launch(url);
-}
-
-void shareQuoteFromMobile(
-    {@required BuildContext context, @required Quote quote}) {
+void shareQuoteMobile({@required BuildContext context, @required Quote quote}) {
   final RenderBox box = context.findRenderObject();
   final quoteName = quote.name;
   final authorName = quote.author?.name ?? '';
@@ -103,6 +79,29 @@ void shareQuoteFromMobile(
     subject: 'Out Of Context',
     sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size,
   );
+}
+
+/// Sahre the target quote to twitter.
+Future shareQuoteWeb({@required Quote quote}) async {
+  final quoteName = quote.name;
+  final authorName = quote.author?.name ?? '';
+  final referenceName = quote.mainReference?.name ?? '';
+
+  String sharingText = quoteName;
+
+  if (authorName.isNotEmpty) {
+    sharingText += ' — $authorName';
+  }
+
+  if (referenceName != null && referenceName.length > 0) {
+    sharingText += ' — $referenceName';
+  }
+
+  final hashtags = '&hashtags=outofcontext';
+
+  final url =
+      'https://twitter.com/intent/tweet?via=outofcontextapp&text=$sharingText$hashtags';
+  await launch(url);
 }
 
 void shareReference(
