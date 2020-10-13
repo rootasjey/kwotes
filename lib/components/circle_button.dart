63b3077600807672fb4ba8c562b71d257f2a9696
rootasjey <rootasjey@users.noreply.github.com>
2020-10-14 01:13:20 +0200

    ➕ Add CircleButton component

---
+++ lib/components/circle_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+/// An alternative to IconButton.
+class CircleButton extends StatelessWidget {
+  /// Tap callback.
+  final VoidCallback onTap;
+
+  /// Typically an Icon.
+  final Widget icon;
+
+  /// Size in radius of the widget.
+  final double radius;
+
+  /// Widget content backrgound color.
+  final Color backgroundColor;
+
+  final double elevation;
+
+  CircleButton({
+    this.onTap,
+    @required this.icon,
+    this.radius = 20.0,
+    this.elevation = 0.0,
+    this.backgroundColor = Colors.black12,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      shape: CircleBorder(),
+      clipBehavior: Clip.hardEdge,
+      color: Colors.transparent,
+      elevation: elevation,
+      child: Ink(
+        child: InkWell(
+          onTap: onTap,
+          child: CircleAvatar(
+            child: icon,
+            backgroundColor: Colors.black12,
+            radius: radius,
+          ),
+        ),
+      ),
+    );
+  }
+}
