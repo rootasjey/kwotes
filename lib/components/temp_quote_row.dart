83eb1838012d91ed5a89d155c818e745ac75fc76
rootasjey <rootasjey@users.noreply.github.com>
2020-10-19 09:53:22 +0200

    👌 Code format (re-order)

--- lib/components/temp_quote_row.dart
+++ lib/components/temp_quote_row.dart
@@ -70,15 +70,15 @@ class _TempQuoteRowState extends State<TempQuoteRow> {
     return cardLayout();
   }
 
-  Widget rowLayout() {
-    final quote = widget.tempQuote;
-    final author = quote.author;
+  Widget cardLayout() {
+    final tempQuote = widget.tempQuote;
 
     return Container(
-      padding: widget.padding,
+      width: widget.cardSize,
+      height: widget.cardSize,
       child: Card(
         elevation: elevation,
-        color: stateColors.appBackground,
+        margin: EdgeInsets.zero,
         child: InkWell(
           onTap: widget.onTap,
           onHover: (isHover) {
@@ -87,59 +87,51 @@ class _TempQuoteRowState extends State<TempQuoteRow> {
               iconColor = isHover ? iconHoverColor : null;
             });
           },
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                if (widget.isDraft) chipDraftInfo(),
-                Expanded(
-                  flex: 2,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        quote.name,
-                        style: TextStyle(
-                          fontSize: 18.0,
-                        ),
+          child: Stack(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(40.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      tempQuote.name.length > 60
+                          ? '${tempQuote.name.substring(0, 60)}...'
+                          : tempQuote.name,
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        // fontSize: FontSize.gridItem(widget.title),
                       ),
-                      Padding(padding: const EdgeInsets.only(top: 10.0)),
-                      Opacity(
-                        opacity: .5,
-                        child: Text(
-                          author == null || author.name.isEmpty
-                              ? ''
-                              : author.name,
-                        ),
-                      ),
-                    ],
+                    ),
+                  ],
+                ),
+              ),
+              if (widget.itemBuilder != null)
+                Positioned(
+                  right: 0,
+                  bottom: 0,
+                  child: PopupMenuButton<String>(
+                    icon: Opacity(
+                      opacity: .6,
+                      child: iconColor != null
+                          ? Icon(
+                              Icons.more_vert,
+                              color: iconColor,
+                            )
+                          : Icon(Icons.more_vert),
+                    ),
+                    onSelected: widget.onSelected,
+                    itemBuilder: widget.itemBuilder,
                   ),
                 ),
-                SizedBox(
-                  width: 50.0,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      PopupMenuButton<String>(
-                        icon: Opacity(
-                          opacity: .6,
-                          child: iconColor != null
-                              ? Icon(
-                                  Icons.more_vert,
-                                  color: iconColor,
-                                )
-                              : Icon(Icons.more_vert),
-                        ),
-                        onSelected: widget.onSelected,
-                        itemBuilder: widget.itemBuilder,
-                      ),
-                    ],
-                  ),
+              if (widget.stackChildren.length > 0) ...widget.stackChildren,
+              if (widget.isDraft)
+                Positioned(
+                  left: 20,
+                  top: 20.0,
+                  child: chipDraftInfo(),
                 ),
-              ],
-            ),
+            ],
           ),
         ),
       ),
@@ -212,15 +204,15 @@ class _TempQuoteRowState extends State<TempQuoteRow> {
     );
   }
 
-  Widget cardLayout() {
-    final tempQuote = widget.tempQuote;
+  Widget rowLayout() {
+    final quote = widget.tempQuote;
+    final author = quote.author;
 
     return Container(
-      width: widget.cardSize,
-      height: widget.cardSize,
+      padding: widget.padding,
       child: Card(
         elevation: elevation,
-        margin: EdgeInsets.zero,
+        color: stateColors.appBackground,
         child: InkWell(
           onTap: widget.onTap,
           onHover: (isHover) {
@@ -229,51 +221,59 @@ class _TempQuoteRowState extends State<TempQuoteRow> {
               iconColor = isHover ? iconHoverColor : null;
             });
           },
-          child: Stack(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(40.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      tempQuote.name.length > 60
-                          ? '${tempQuote.name.substring(0, 60)}...'
-                          : tempQuote.name,
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        // fontSize: FontSize.gridItem(widget.title),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                if (widget.isDraft) chipDraftInfo(),
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        quote.name,
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              if (widget.itemBuilder != null)
-                Positioned(
-                  right: 0,
-                  bottom: 0,
-                  child: PopupMenuButton<String>(
-                    icon: Opacity(
-                      opacity: .6,
-                      child: iconColor != null
-                          ? Icon(
-                              Icons.more_vert,
-                              color: iconColor,
-                            )
-                          : Icon(Icons.more_vert),
-                    ),
-                    onSelected: widget.onSelected,
-                    itemBuilder: widget.itemBuilder,
+                      Padding(padding: const EdgeInsets.only(top: 10.0)),
+                      Opacity(
+                        opacity: .5,
+                        child: Text(
+                          author == null || author.name.isEmpty
+                              ? ''
+                              : author.name,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              if (widget.stackChildren.length > 0) ...widget.stackChildren,
-              if (widget.isDraft)
-                Positioned(
-                  left: 20,
-                  top: 20.0,
-                  child: chipDraftInfo(),
+                SizedBox(
+                  width: 50.0,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      PopupMenuButton<String>(
+                        icon: Opacity(
+                          opacity: .6,
+                          child: iconColor != null
+                              ? Icon(
+                                  Icons.more_vert,
+                                  color: iconColor,
+                                )
+                              : Icon(Icons.more_vert),
+                        ),
+                        onSelected: widget.onSelected,
+                        itemBuilder: widget.itemBuilder,
+                      ),
+                    ],
+                  ),
                 ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
