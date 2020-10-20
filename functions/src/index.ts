610053c326e311a95f2c5a9d9fde6061ba8b636d
rootasjey <rootasjey@users.noreply.github.com>
2020-10-20 22:40:46 +0200

    ➕ (functions) Add search functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,9 @@
 import * as favouritesFunc from './favourites';
 import * as quotidiansFunc from './quotidians';
+import * as searchFunc from './search';
 import * as usersFunc from './users';
 
 export const favourites = favouritesFunc;
 export const quotidians = quotidiansFunc;
+export const search = searchFunc;
 export const users = usersFunc;
---
+++ functions/src/search.ts
@@ -0,0 +1,45 @@
+import * as functions from 'firebase-functions';
+import algolia from 'algoliasearch';
+
+const env = functions.config();
+
+const client = algolia(env.algolia.appid, env.algolia.apikey);
+const quotesIndex = client.initIndex('quotes_search');
+
+export const onIndexQuote = functions
+  .region('europe-west3')
+  .firestore
+  .document('quotes/{quoteId}')
+  .onCreate(async (snapshot) => {
+    const data = snapshot.data();
+    const objectID = snapshot.id;
+
+    return quotesIndex.saveObject({
+      objectID,
+      ...data,
+    })
+  });
+
+export const onReIndexQuote = functions
+  .region('europe-west3')
+  .firestore
+  .document('quotes/{quoteId}')
+  .onUpdate(async (snapshot) => {
+    const data = snapshot.after.data();
+    const objectID = snapshot.after.id;
+
+    return quotesIndex.saveObject({
+      objectID,
+      ...data,
+    })
+  });
+
+export const onUnIndexQuote = functions
+  .region('europe-west3')
+  .firestore
+  .document('quotes/{quoteId}')
+  .onDelete(async (snapshot) => {
+    const objectID = snapshot.id;
+    return quotesIndex.deleteObject(objectID);
+  });
+
