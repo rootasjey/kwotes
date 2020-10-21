efdc8daff78f0d0e22cfac368d19106fa393385e
rootasjey <rootasjey@users.noreply.github.com>
2020-10-21 17:46:03 +0200

    ➕ Add class: Release

--- lib/types/release.dart
+++ lib/types/release.dart
@@ -1,14 +1,17 @@
 class Release {
   /// Original release.
   DateTime original;
+  bool beforeJC;
 
   Release({
     this.original,
+    this.beforeJC,
   });
 
   factory Release.fromJSON(Map<String, dynamic> json) {
     return Release(
       original: json['original'],
+      beforeJC: json['beforeJC'],
     );
   }
 }
