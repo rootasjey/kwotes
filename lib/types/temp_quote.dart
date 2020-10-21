c8d36b29bcba08cbde16d756fc48bfa82a09f800
rootasjey <rootasjey@users.noreply.github.com>
2020-10-22 01:34:27 +0200

    👌 Code format

--- lib/types/temp_quote.dart
+++ lib/types/temp_quote.dart
@@ -6,19 +6,26 @@ import 'package:memorare/types/validation.dart';
 
 class TempQuote {
   final Author author;
-  final List<String> comments;
-  final DateTime createdAt;
-  final String id;
+
   // To distinguish offline draft.
   final bool isOffline;
-  final String lang;
-  final Reference mainReference;
-  final String name;
-  final List<Reference> references;
-  final String region;
-  final List<String> topics;
+
+  final DateTime createdAt;
   final DateTime updatedAt;
+
+  final List<String> comments;
+  final List<Reference> references;
+  final List<String> topics;
+
   final PartialUser user;
+
+  final Reference mainReference;
+
+  final String id;
+  final String lang;
+  final String name;
+  final String region;
+
   final Validation validation;
 
   TempQuote({
@@ -49,56 +56,58 @@ class TempQuote {
     }
 
     if (json['topics'] != null) {
-      final Map<String, dynamic> _topics = json['topics'];
-      _topics.forEach((key, value) {
+      final Map<String, dynamic> topics = json['topics'];
+      topics.forEach((key, value) {
         topicsList.add(key);
       });
     }
 
-    final _author = json['author'] != null ?
-      Author.fromJSON(json['author']) : null;
+    final author =
+        json['author'] != null ? Author.fromJSON(json['author']) : null;
 
-    final _mainReference = json['mainReference'] != null ?
-      Reference.fromJSON(json['mainReference']) : null;
+    final _mainReference = json['mainReference'] != null
+        ? Reference.fromJSON(json['mainReference'])
+        : null;
 
-    final _user = json['user'] != null ?
-      PartialUser.fromJSON(json['user']) : null;
+    final user =
+        json['user'] != null ? PartialUser.fromJSON(json['user']) : null;
 
-    final _validation = json['validation'] != null ?
-      Validation.fromJSON(json['validation']) : null;
+    final validation = json['validation'] != null
+        ? Validation.fromJSON(json['validation'])
+        : null;
 
-    final List<dynamic> _rawComments = json['comments'];
-    final _comments = <String>[];
+    final List<dynamic> rawComments = json['comments'];
+    final comments = <String>[];
 
-    if (_rawComments != null) {
-      _rawComments.forEach((rawComment) {
-        _comments.add(rawComment);
+    if (rawComments != null) {
+      rawComments.forEach((rawComment) {
+        comments.add(rawComment);
       });
     }
 
-    final createdAt = json['createdAt'].runtimeType == String ?
-      DateTime.parse(json['createdAt']) :
-      (json['createdAt'] as Timestamp).toDate();
+    final createdAt = json['createdAt'].runtimeType == String
+        ? DateTime.parse(json['createdAt'])
+        : (json['createdAt'] as Timestamp).toDate();
 
-    final updatedAt = json['updatedAt'].runtimeType == String ?
-      DateTime.parse(json['updatedAt']) :
-      (json['updatedAt'] as Timestamp).toDate();
+    final updatedAt = json['updatedAt'].runtimeType == String
+        ? DateTime.parse(json['updatedAt'])
+        : (json['updatedAt'] as Timestamp).toDate();
 
     return TempQuote(
-      author        : _author,
-      comments      : _comments,
-      createdAt     : createdAt,
-      id            : json['id'],
-      isOffline     : json['isOffline'] ?? false,
-      lang          : json['lang'],
-      mainReference : _mainReference,
-      name          : json['name'],
-      references    : referencesList,
-      region        : json['region'],
-      topics        : topicsList,
-      updatedAt     : updatedAt,
-      user          : _user,
-      validation    : _validation,
+      author: author,
+      comments: comments,
+      createdAt: createdAt,
+      id: json['id'],
+      isOffline: json['isOffline'] ?? false,
+      lang: json['lang'],
+      mainReference: _mainReference,
+      name: json['name'],
+      references: referencesList,
+      region: json['region'],
+      topics: topicsList,
+      updatedAt: updatedAt,
+      user: user,
+      validation: validation,
     );
   }
 }
