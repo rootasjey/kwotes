6b21c179c0bf5013c5a118303b5a2cc1004a1a12
rootasjey <rootasjey@users.noreply.github.com>
2020-10-22 03:28:34 +0200

    ☝️ (functions) Add authors index

--- functions/src/search.ts
+++ functions/src/search.ts
@@ -4,8 +4,50 @@ import algolia from 'algoliasearch';
 const env = functions.config();
 
 const client = algolia(env.algolia.appid, env.algolia.apikey);
+const authorsIndex = client.initIndex('authors');
 const quotesIndex = client.initIndex('quotes');
 
+// Author index
+// ------------
+export const onIndexAuthor = functions
+  .region('europe-west3')
+  .firestore
+  .document('authors/{authorId}')
+  .onCreate(async (snapshot) => {
+    const data = snapshot.data();
+    const objectID = snapshot.id;
+
+    return authorsIndex.saveObject({
+      objectID,
+      ...data,
+    })
+  });
+
+export const onReIndexAuthor = functions
+  .region('europe-west3')
+  .firestore
+  .document('authors/{authorId}')
+  .onUpdate(async (snapshot) => {
+    const data = snapshot.after.data();
+    const objectID = snapshot.after.id;
+
+    return authorsIndex.saveObject({
+      objectID,
+      ...data,
+    })
+  });
+
+export const onUnIndexAuthor = functions
+  .region('europe-west3')
+  .firestore
+  .document('authors/{authorId}')
+  .onDelete(async (snapshot) => {
+    const objectID = snapshot.id;
+    return authorsIndex.deleteObject(objectID);
+  });
+
+// Quotes index
+// ------------
 export const onIndexQuote = functions
   .region('europe-west3')
   .firestore
@@ -42,4 +84,3 @@ export const onUnIndexQuote = functions
     const objectID = snapshot.id;
     return quotesIndex.deleteObject(objectID);
   });
-
