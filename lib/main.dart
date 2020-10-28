58eaf20e2c451c29c82b6574b6b00d0afa3a1cf2
rootasjey <rootasjey@users.noreply.github.com>
2020-10-28 03:12:27 +0100

    🚑  Add Firebase init (new module rule)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -12,11 +13,14 @@ import 'package:figstyle/state/topics_colors.dart';
 import 'package:figstyle/state/user_state.dart';
 import 'package:figstyle/utils/app_localstorage.dart';
 
-void main() {
+void main() async {
   LicenseRegistry.addLicense(() async* {
     final license = await rootBundle.loadString('google_fonts/OFL.txt');
     yield LicenseEntryWithLineBreaks(['google_fonts'], license);
   });
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   return runApp(App());
 }
