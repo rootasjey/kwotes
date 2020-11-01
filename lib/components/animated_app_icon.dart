1e62ee5a381d274691cf0c7399ec008e678a7e5e
rootasjey <rootasjey@users.noreply.github.com>
2020-11-01 11:53:49 +0100

    ☝️ Update AnimatedAppIcon (layout)

--- lib/components/animated_app_icon.dart
+++ lib/components/animated_app_icon.dart
@@ -7,25 +7,10 @@ class AnimatedAppIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        width: size,
-        height: size,
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                bottom: 0.0,
-              ),
-              child: Image.asset(
-                'assets/images/app-icon-animation.gif',
-                height: 100.0,
-                width: 100.0,
-              ),
-            ),
-          ],
-        ),
-      ),
+    return Image.asset(
+      'assets/images/app-icon-animation.gif',
+      height: size,
+      width: size,
     );
   }
 }
