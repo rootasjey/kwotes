9704dbf2dfeff7937e894534f05974ade484b31d
rootasjey <rootasjey@users.noreply.github.com>
2020-11-03 14:19:28 +0100

    ➕ (func) Add user's stats: lists

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as favouritesFunc  from './favourites';
 import * as imagesFunc      from './images';
+import * as listsFunc       from './lists';
 import * as publishedFunc   from './published';
 import * as quotidiansFunc  from './quotidians';
 import * as searchFunc      from './search';
@@ -8,6 +9,7 @@ import * as usersFunc       from './users';
 
 export const favourites = favouritesFunc;
 export const images     = imagesFunc;
+export const lists      = listsFunc;
 export const published  = publishedFunc;
 export const quotidians = quotidiansFunc;
 export const search     = searchFunc;
---
+++ functions/src/lists.ts
@@ -0,0 +1,46 @@
+import * as functions from 'firebase-functions';
+import { adminApp } from './adminApp';
+
+const firestore = adminApp.firestore();
+
+export const onListAdded = functions
+  .region('europe-west3')
+  .firestore
+  .document('users/{userId}/lists/{listId}')
+  .onCreate(async (snapshot, context) => {
+    const user = await firestore
+      .collection('users')
+      .doc(context.params.userId)
+      .get();
+
+    if (!user.exists) { return; }
+    
+    const userData = user.data();
+    if (!userData) { return; }
+
+    let userLists: number = userData.stats.lists ?? 0;
+    
+    return await user.ref
+      .update('stats.lists', userLists + 1);
+  });
+
+export const onListDeleted = functions
+  .region('europe-west3')
+  .firestore
+  .document('users/{userId}/lists/{listId}')
+  .onDelete(async (snapshot, context) => {
+    const user = await firestore
+      .collection('users')
+      .doc(context.params.userId)
+      .get();
+
+    if (!user.exists) { return; }
+
+    const userData = user.data();
+    if (!userData) { return; }
+
+    let userLists: number = userData.stats.lists ?? 0;
+
+    return await user.ref
+      .update('stats.lists', Math.max(0, userLists - 1));
+  });
