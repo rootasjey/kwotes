7bd38e0b8801239ec1b0f31117f8c95ea5cf92a2
rootasjey <rootasjey@users.noreply.github.com>
2020-11-18 01:10:19 +0100

    ➕ Add dark & light colors

--- lib/state/colors.dart
+++ lib/state/colors.dart
@@ -28,6 +28,10 @@ abstract class StateColorsBase with Store {
   Color deletion = Color(0xfff55c5c);
   Color validation = Color(0xff38d589);
 
+  // Color dark = Color(0xFF000000);
+  Color dark = Color(0xFF303030);
+  Color light = Color(0xFFEEEEEE);
+
   @observable
   Color softBackground = Color(0xFFEEEEEE);
 
