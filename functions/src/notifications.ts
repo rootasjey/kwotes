82398048605eb9c766c41951e9243f6c809a48b2
rootasjey <rootasjey@users.noreply.github.com>
2020-11-26 00:47:42 +0100

    ☝️ (func) Rename func & add stats updates

--- functions/src/notifications.ts
+++ functions/src/notifications.ts
@@ -6,7 +6,7 @@ import { sendNotification } from './utils';
 const env = functions.config();
 const firestore = adminApp.firestore();
 
-export const notifyQuoteInValidation = functions
+export const incrementStatsAndSendPush = functions
   .region('europe-west3')
   .firestore
   .document('users/{userId}/notifications/{notificationId}')
@@ -27,12 +27,66 @@ export const notifyQuoteInValidation = functions
       return false;
     }
 
+    const notifications = userData.stats.notifications;
+    const total: number = notifications.total + 1;
+    const unread: number = notifications.unread + 1;
+
+    await firestore
+      .collection('users')
+      .doc(userId)
+      .update({
+        'stats.notifications': {
+          total: total,
+          unread: unread,
+        }
+      });
+
     const notifData = notifSnapshot.data();
     const subject: string = notifData.subject;
 
     if (subject == 'tempquotes') {
       return handleTempQuoteValidation({ userId, userData, notifSnapshot });
     }
+
+    return true;
+  });
+
+export const decrementStats = functions
+  .region('europe-west3')
+  .firestore
+  .document('users/{userId}/notifications/{notificationId}')
+  .onDelete(async (notifSnapshot, context) => {
+    const userId: string = context.params.userId;
+
+    const userSnapshot = await firestore
+      .collection('users')
+      .doc(userId)
+      .get();
+
+    if (!userSnapshot.exists) {
+      return false;
+    }
+
+    const userData = userSnapshot.data();
+    if (!userData) {
+      return false;
+    }
+
+    let total: number = userData.stats.notifications.total - 1;
+    let unread: number = userData.stats.notifications.unread - 1;
+
+    total = Math.max(total, 0);
+    unread = Math.max(unread, 0);
+
+    await firestore
+      .collection('users')
+      .doc(userId)
+      .update({
+        'stats.notifications': {
+          total: total,
+          unread: unread,
+        }
+      });
 
     return true;
   });
