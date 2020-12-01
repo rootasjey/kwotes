eb7ce15a502978235548c16f9a7ab088701564ec
rootasjey <rootasjey@users.noreply.github.com>
2020-12-01 01:24:58 +0100

    ➕ Add `CloudFuncError` class

---
+++ lib/types/cloud_func_error.dart
@@ -0,0 +1,19 @@
+class CloudFuncError {
+  final String message;
+  final String code;
+  final String details;
+
+  CloudFuncError({
+    this.message = '',
+    this.code = '',
+    this.details = '',
+  });
+
+  factory CloudFuncError.fromJSON(Map<String, dynamic> data) {
+    return CloudFuncError(
+      message: data['message'],
+      code: data['code'],
+      details: data['details'],
+    );
+  }
+}
