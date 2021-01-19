c1b27b5d43d760aaccd249b9deb2a094b652e97c
rootasjey <rootasjey@users.noreply.github.com>
2021-01-19 12:39:45 +0100

    ☝️ Update ColoredListTile with new props.

--- lib/components/colored_list_tile.dart
+++ lib/components/colored_list_tile.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:figstyle/state/colors.dart';
-import 'package:mobx/mobx.dart';
 
 class ColoredListTile extends StatefulWidget {
   final Color hoverColor;
@@ -11,63 +10,46 @@ class ColoredListTile extends StatefulWidget {
   final Widget title;
   final double width;
 
-  ColoredListTile({
-    this.selected = false,
-    this.hoverColor = Colors.blue,
+  const ColoredListTile({
+    Key key,
+    this.hoverColor,
     this.icon,
     this.onTap,
-    this.outlined = true,
+    this.outlined,
+    this.selected,
     this.title,
-    this.width = 200.0,
-  });
+    this.width,
+  }) : super(key: key);
 
   @override
   _ColoredListTileState createState() => _ColoredListTileState();
 }
 
 class _ColoredListTileState extends State<ColoredListTile> {
-  Color hoverColor = Colors.black45;
   Color baseColor = Colors.black45;
-
-  ReactionDisposer colorDisposer;
+  Color hoverColor = Colors.blue;
+  Color currentColor = Colors.black45;
 
   @override
   void initState() {
     super.initState();
 
-    colorDisposer = autorun((reaction) {
-      setState(() {
-        baseColor = widget.selected
-        ? widget.hoverColor
-        : stateColors.foreground.withOpacity(.5);
+    setState(() {
+      baseColor = widget.selected
+          ? widget.hoverColor
+          : stateColors.foreground.withOpacity(.5);
 
-        hoverColor = baseColor;
-      });
+      hoverColor = widget.hoverColor;
+      currentColor = baseColor;
     });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    if (colorDisposer != null) {
-      colorDisposer();
-    }
   }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onHover: (isHover) {
-        if (isHover) {
-          setState(() {
-            hoverColor = widget.hoverColor;
-          });
-
-          return;
-        }
-
         setState(() {
-          hoverColor = baseColor;
+          currentColor = isHover ? hoverColor : baseColor;
         });
       },
       onTap: widget.onTap,
@@ -75,21 +57,21 @@ class _ColoredListTileState extends State<ColoredListTile> {
         width: widget.width,
         child: DecoratedBox(
           decoration: widget.outlined
-            ? BoxDecoration(
-              borderRadius: BorderRadius.circular(2.0),
-              border: Border.all(
-                color: stateColors.foreground.withOpacity(0.2),
-                width: 1.0,
-              ),
-            )
-            : BoxDecoration(),
+              ? BoxDecoration(
+                  borderRadius: BorderRadius.circular(2.0),
+                  border: Border.all(
+                    color: stateColors.foreground.withOpacity(0.2),
+                    width: 1.0,
+                  ),
+                )
+              : BoxDecoration(),
           child: ListTile(
             leading: widget.icon != null
-              ? Icon(
-                  widget.icon,
-                  color: hoverColor,
-                )
-              : null,
+                ? Icon(
+                    widget.icon,
+                    color: currentColor,
+                  )
+                : null,
             title: widget.title,
           ),
         ),
