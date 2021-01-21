54c0da4acec69035a75f80af640a66a37510e006
rootasjey <rootasjey@users.noreply.github.com>
2021-01-21 06:47:21 +0100

    ☝️ Update quote page with better desktop layout

--- lib/screens/quote_page.dart
+++ lib/screens/quote_page.dart
@@ -1,9 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:figstyle/actions/quotes.dart';
+import 'package:figstyle/actions/quotidians.dart';
 import 'package:figstyle/actions/share.dart';
 import 'package:figstyle/components/fade_in_y.dart';
 import 'package:figstyle/components/user_lists.dart';
 import 'package:figstyle/router/app_router.gr.dart';
+import 'package:figstyle/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:figstyle/actions/favourites.dart';
 import 'package:figstyle/components/full_page_error.dart';
@@ -38,7 +41,11 @@ class QuotePage extends StatefulWidget {
 }
 
 class _QuotePageState extends State<QuotePage> {
+  /// True if data is loading.
   bool isLoading = false;
+
+  /// If there is a previous page, will be false.
+  bool showAnimations = false;
 
   Color accentColor = Colors.blue;
 
@@ -53,9 +60,11 @@ class _QuotePageState extends State<QuotePage> {
     super.initState();
 
     if (widget.quote == null) {
+      showAnimations = true;
       fetchQuote();
     } else {
       setState(() {
+        showAnimations = false;
         quote = widget.quote;
 
         final topicColor = appTopicsColors.find(quote.topics.first);
@@ -72,10 +81,7 @@ class _QuotePageState extends State<QuotePage> {
         physics: ClampingScrollPhysics(),
         controller: _pageScrollController,
         slivers: <Widget>[
-          DesktopAppBar(
-            showCloseButton: true,
-            showUserMenu: false,
-          ),
+          DesktopAppBar(),
           SliverList(
             delegate: SliverChildListDelegate.fixed([
               body(),
@@ -86,45 +92,14 @@ class _QuotePageState extends State<QuotePage> {
     );
   }
 
-  Widget body() {
-    if (isLoading) {
-      return FullPageLoading(
-        title: 'Loading quote...',
-      );
-    }
-
-    if (quote == null) {
-      return FullPageError(
-        message: 'Error while loading the quote.',
-      );
-    }
-
-    return OrientationBuilder(
-      builder: (context, orientation) {
-        return Column(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(
-                top: 50.0,
-                left: 15.0,
-                right: 15.0,
-                bottom: 40.0,
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  quoteName(),
-                  divider(),
-                  authorName(),
-                  referenceName(),
-                ],
-              ),
-            ),
-            userActions(),
-            topicsList(),
-          ],
-        );
-      },
+  Widget addToQuotidiansButton() {
+    return IconButton(
+      tooltip: "Add to quotidians",
+      onPressed: () => addToQuotidians(
+        quote: widget.quote,
+        lang: widget.quote.lang,
+      ),
+      icon: Icon(Icons.wb_sunny),
     );
   }
 
@@ -194,20 +169,49 @@ class _QuotePageState extends State<QuotePage> {
     );
   }
 
-  Widget authorName() {
-    final fontSize = MediaQuery.of(context).size.width < 400.0 ? 18.0 : 25.0;
+  Widget authorAndReference() {
+    bool showBar = false;
+    final author = quote?.author;
+    final reference = quote?.mainReference;
 
+    if (author != null &&
+        author.name.isNotEmpty &&
+        reference != null &&
+        reference.name.isNotEmpty) {
+      showBar = true;
+    }
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 4.0),
+      child: Row(
+        children: [
+          authorName(),
+          if (showBar)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text('―'),
+            ),
+          referenceName(),
+        ],
+      ),
+    );
+  }
+
+  Widget authorName() {
     return CustomAnimation(
-      delay: 1.seconds,
+      delay: 500.milliseconds,
       duration: 250.milliseconds,
       tween: Tween(begin: 0.0, end: 0.6),
-      child: InkWell(
-        onTap: onTapAuthor,
-        child: Text(
-          quote.author.name,
-          textAlign: TextAlign.right,
-          style: TextStyle(
-            fontSize: fontSize,
+      child: Tooltip(
+        message: 'Author',
+        child: InkWell(
+          onTap: onTapAuthor,
+          child: Text(
+            quote.author.name,
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.w700,
+            ),
           ),
         ),
       ),
@@ -220,12 +224,39 @@ class _QuotePageState extends State<QuotePage> {
     );
   }
 
+  Widget body() {
+    if (isLoading) {
+      return FullPageLoading(
+        title: 'Loading quote...',
+      );
+    }
+
+    if (quote == null) {
+      return FullPageError(
+        message: 'Error while loading the quote.',
+      );
+    }
+
+    return pageLayout();
+  }
+
+  Widget deletePubQuoteButton() {
+    return IconButton(
+      tooltip: "Delete published quote",
+      onPressed: () => confirmAndDeletePubQuote(),
+      icon: Icon(Icons.delete_outline),
+    );
+  }
+
   Widget divider() {
     return FadeInY(
       beginY: 10.0,
       delay: 300.milliseconds,
       child: Padding(
-        padding: const EdgeInsets.only(top: 60.0, bottom: 20.0),
+        padding: const EdgeInsets.only(
+          top: 30.0,
+          bottom: 10.0,
+        ),
         child: Container(
           width: 60.0,
           height: 5.0,
@@ -288,17 +319,102 @@ class _QuotePageState extends State<QuotePage> {
     );
   }
 
-  Widget quoteName() {
+  Widget desktopLayout() {
     final size = MediaQuery.of(context).size;
 
     return SafeArea(
-      child: createHeroQuoteAnimation(
-        quote: quote,
-        isMobile: size.width < 700.0,
-        screenWidth: size.width,
-        screenHeight: size.height,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Spacer(),
+          userActions(),
+          Expanded(
+            flex: 6,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                authorAndReference(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Card(
+                      elevation: 2.0,
+                      child: InkWell(
+                        onTap: () {},
+                        onLongPress: onLongPress,
+                        child: Padding(
+                          padding: const EdgeInsets.all(60.0),
+                          child: createHeroQuoteAnimation(
+                            quote: quote,
+                            isMobile: size.width < 700.0,
+                            screenWidth: size.width,
+                            screenHeight: size.height,
+                          ),
+                        ),
+                      ),
+                    ),
+                    topicsList(),
+                  ],
+                ),
+                // topicsList(),
+              ],
+            ),
+          ),
+          Spacer(),
+        ],
       ),
     );
+  }
+
+  Widget mobileLayout() {
+    final size = MediaQuery.of(context).size;
+
+    return SafeArea(
+      child: Column(
+        children: [
+          InkWell(
+            onTap: () {},
+            onLongPress: onLongPress,
+            child: Padding(
+              padding: const EdgeInsets.all(60.0),
+              child: createHeroQuoteAnimation(
+                quote: quote,
+                isMobile: size.width < 700.0,
+                screenWidth: size.width,
+                screenHeight: size.height,
+              ),
+            ),
+          ),
+          // authorAndReference(),
+          authorName(),
+          referenceName(),
+          topicsList(),
+          userActions(axis: Axis.horizontal),
+        ],
+      ),
+    );
+  }
+
+  Widget pageLayout() {
+    final size = MediaQuery.of(context).size;
+    bool isWide = size.width >= Constants.maxMobileWidth;
+
+    if (isWide) {
+      return Padding(
+        padding: const EdgeInsets.only(top: 60.0),
+        child: desktopLayout(),
+      );
+      // return OrientationBuilder(
+      //   builder: (context, orientation) {
+      //     return Padding(
+      //       padding: const EdgeInsets.only(top: 40.0),
+      //       child: quoteName(),
+      //     );
+      //   },
+      // );
+    }
+
+    return mobileLayout();
   }
 
   Widget referenceName() {
@@ -307,27 +423,26 @@ class _QuotePageState extends State<QuotePage> {
     }
 
     return CustomAnimation(
-      delay: 1.2.seconds,
+      delay: 700.milliseconds,
       duration: 250.milliseconds,
       tween: Tween(begin: 0.0, end: 0.5),
-      child: InkWell(
-        onTap: onReferenceTap,
-        child: Text(
-          quote.mainReference.name,
-          textAlign: TextAlign.right,
-          style: TextStyle(
-            fontSize: 18.0,
-            fontStyle: FontStyle.italic,
+      child: Tooltip(
+        message: 'Reference',
+        child: InkWell(
+          onTap: onReferenceTap,
+          child: Text(
+            quote.mainReference.name,
+            style: TextStyle(
+              fontSize: 18.0,
+              fontStyle: FontStyle.italic,
+            ),
           ),
         ),
       ),
       builder: (context, child, value) {
-        return Padding(
-          padding: const EdgeInsets.only(top: 8.0),
-          child: Opacity(
-            opacity: value,
-            child: child,
-          ),
+        return Opacity(
+          opacity: value,
+          child: child,
         );
       },
     );
@@ -335,8 +450,9 @@ class _QuotePageState extends State<QuotePage> {
 
   Widget shareButton() {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
       child: IconButton(
+        tooltip: 'Share this quote',
         onPressed: () async {
           shareQuote(context: context, quote: quote);
         },
@@ -347,55 +463,129 @@ class _QuotePageState extends State<QuotePage> {
 
   Widget topicsList() {
     if (quote.topics.length == 0) {
-      return Padding(padding: EdgeInsets.zero);
+      return Container();
     }
 
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      child: Column(
-        children: <Widget>[
-          Divider(
-            thickness: 1.0,
-          ),
-          SizedBox(
-            height: 300,
-            child: ListView(
-              shrinkWrap: true,
-              padding: EdgeInsets.only(
-                top: 80.0,
-              ),
-              scrollDirection: Axis.horizontal,
-              children: quote.topics.map((topic) {
-                final topicColor = appTopicsColors.find(topic);
+    final screenSize = MediaQuery.of(context).size;
+    bool isNarrow = screenSize.width < Constants.maxMobileWidth;
 
-                return TopicCardColor(
-                  elevation: 3.0,
-                  color: Color(topicColor.decimal),
-                  name: topicColor.name,
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w700,
-                  ),
-                );
-              }).toList(),
-            ),
-          ),
-        ],
+    double cardSize = 50.0;
+    EdgeInsets padding = EdgeInsets.zero;
+
+    if (isNarrow) {
+      cardSize = 70.0;
+      padding = const EdgeInsets.only(top: 32.0);
+    }
+
+    return Padding(
+      padding: padding,
+      child: Wrap(
+        alignment: WrapAlignment.end,
+        children: quote.topics.map((topic) {
+          final topicColor = appTopicsColors.find(topic);
+
+          return TopicCardColor(
+            elevation: 3.0,
+            size: cardSize,
+            color: Color(topicColor.decimal),
+            tooltip: topicColor.name,
+          );
+        }).toList(),
       ),
     );
   }
 
-  Widget userActions() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          favIconButton(),
-          shareButton(),
-          addToListButton(),
-        ],
+  Widget userActions({Axis axis = Axis.vertical}) {
+    var children = <Widget>[
+      favIconButton(),
+      shareButton(),
+      addToListButton(),
+    ];
+
+    if (stateUser.canManageQuote) {
+      final ldivider = axis == Axis.horizontal
+          ? VerticalDivider(thickness: 1.0)
+          : Divider(
+              thickness: 1.0,
+              height: 26.0,
+              indent: 12.0,
+              endIndent: 12.0,
+            );
+
+      children.addAll([
+        ldivider,
+        addToQuotidiansButton(),
+        deletePubQuoteButton(),
+      ]);
+    }
+
+    if (axis == Axis.horizontal) {
+      return Wrap(
+        children: children,
+      );
+    }
+
+    return Container(
+      width: 80.0,
+      padding: const EdgeInsets.only(top: 24.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: children,
       ),
+    );
+  }
+
+  void confirmAndDeletePubQuote() async {
+    showCustomModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Material(
+          child: SafeArea(
+            top: false,
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              ListTile(
+                title: Text(
+                  'Confirm',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                trailing: Icon(
+                  Icons.check,
+                  color: Colors.white,
+                ),
+                tileColor: Color(0xfff55c5c),
+                onTap: () async {
+                  context.router.pop();
+
+                  final success = await deleteQuote(quote: widget.quote);
+
+                  if (success) {
+                    context.router.pop();
+                  }
+                },
+              ),
+              ListTile(
+                title: Text('Cancel'),
+                trailing: Icon(Icons.close),
+                onTap: context.router.pop,
+              ),
+            ]),
+          ),
+        );
+      },
+      containerWidget: (context, animation, child) {
+        return SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Material(
+              clipBehavior: Clip.antiAlias,
+              borderRadius: BorderRadius.circular(12.0),
+              child: child,
+            ),
+          ),
+        );
+      },
     );
   }
 
@@ -477,11 +667,137 @@ class _QuotePageState extends State<QuotePage> {
     return success;
   }
 
+  void onLongPress() {
+    final children = [
+      ListTile(
+        title: Text('Share'),
+        trailing: Icon(
+          Icons.ios_share,
+        ),
+        onTap: () {
+          context.router.pop();
+          shareQuote(context: context, quote: widget.quote);
+        },
+      ),
+    ];
+
+    if (stateUser.isUserConnected) {
+      children.addAll([
+        ListTile(
+          title: Text('Add to...'),
+          trailing: Icon(
+            Icons.playlist_add,
+          ),
+          onTap: () {
+            context.router.pop();
+            showCupertinoModalBottomSheet(
+              context: context,
+              builder: (context) => UserLists(
+                scrollController: ModalScrollController.of(context),
+                quote: widget.quote,
+              ),
+            );
+          },
+        ),
+        ListTile(
+          title: quote.starred ? Text('Unlike') : Text('Like'),
+          trailing: quote.starred
+              ? Icon(Icons.favorite)
+              : Icon(Icons.favorite_border),
+          onTap: () async {
+            context.router.pop();
+            stateUser.mustUpdateFav = true;
+
+            final newValue = !quote.starred;
+
+            setState(() => quote.starred = newValue);
+
+            bool success = true;
+
+            if (quote.starred) {
+              success = await unlikeQuote();
+            } else {
+              success = await likeQuote();
+            }
+
+            if (!success) {
+              setState(() => quote.starred = !newValue);
+            }
+          },
+        ),
+      ]);
+    }
+
+    if (stateUser.canManageQuote) {
+      children.addAll([
+        ListTile(
+          title: Text('Delete'),
+          trailing: Icon(
+            Icons.delete_outline,
+          ),
+          onTap: () {
+            context.router.pop();
+            confirmAndDeletePubQuote();
+          },
+        ),
+        ListTile(
+          title: Text('Next quotidian'),
+          trailing: Icon(Icons.wb_sunny),
+          onTap: () {
+            context.router.pop();
+            addToQuotidians(
+              quote: widget.quote,
+              lang: widget.quote.lang,
+            );
+          },
+        ),
+      ]);
+    }
+
+    int flex =
+        MediaQuery.of(context).size.width < Constants.maxMobileWidth ? 5 : 1;
+
+    showCustomModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: children,
+        );
+      },
+      containerWidget: (context, animation, child) {
+        return SafeArea(
+          child: Row(
+            children: [
+              Spacer(),
+              Expanded(
+                flex: flex,
+                child: Container(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Material(
+                    clipBehavior: Clip.antiAlias,
+                    borderRadius: BorderRadius.circular(12.0),
+                    child: child,
+                  ),
+                ),
+              ),
+              Spacer(),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   void onTapAuthor() {
-    AuthorPageRoute(
-      authorId: quote.author.id,
-      authorName: quote.author.name,
-    ).show(context);
+    context.router.root.push(
+      AuthorsDeepRoute(children: [
+        AuthorPageRoute(
+          authorId: quote.author.id,
+          authorName: quote.author.name,
+        ),
+      ]),
+    );
   }
 
   void onReferenceTap() {
