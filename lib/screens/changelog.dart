3283ba6002f2ab70e7b842ce2779e7c46d4e68d4
rootasjey <rootasjey@users.noreply.github.com>
2021-01-22 23:28:30 +0100

    ☝️ Update changelog page layout

--- lib/screens/changelog.dart
+++ lib/screens/changelog.dart
@@ -1,7 +1,9 @@
 import 'package:figstyle/components/desktop_app_bar.dart';
+import 'package:figstyle/state/colors.dart';
 import 'package:figstyle/types/changelog_item.dart';
 import 'package:figstyle/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:unicons/unicons.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class Changelog extends StatefulWidget {
@@ -83,22 +85,16 @@ class _ChangelogState extends State<Changelog> {
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
 
-    bool showUserMenu = true;
     double horPadding = 80.0;
 
     if (width < Constants.maxMobileWidth) {
-      showUserMenu = false;
       horPadding = 20.0;
     }
 
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          DesktopAppBar(
-            title: "Changelog",
-            automaticallyImplyLeading: true,
-            showUserMenu: showUserMenu,
-          ),
+          DesktopAppBar(),
           SliverPadding(
             padding: EdgeInsets.symmetric(
               horizontal: horPadding,
@@ -106,68 +102,42 @@ class _ChangelogState extends State<Changelog> {
             ),
             sliver: SliverList(
               delegate: SliverChildListDelegate.fixed([
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 16.0),
-                  child: Opacity(
-                    opacity: 0.6,
-                    child: Text(
-                      "You can find the app version history below",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ),
-                ),
-                ExpansionPanelList(
-                  expansionCallback: (int index, bool isExpanded) {
-                    setState(() {
-                      changelogItemsList[index].isExpanded = !isExpanded;
-                    });
-                  },
-                  children: changelogItemsList.map((changelogItem) {
-                    final date = changelogItem.date;
-                    final day = date.day < 10 ? '0${date.day}' : date.day;
-                    final month =
-                        date.month < 10 ? '0${date.month}' : date.month;
-
-                    return ExpansionPanel(
-                      headerBuilder: (context, isExpanded) {
-                        return ListTile(
-                          title: changelogItem.title,
-                          subtitle: Text("$day/$month/${date.year}"),
-                          onTap: () {
-                            setState(
-                              () {
-                                changelogItem.isExpanded =
-                                    !changelogItem.isExpanded;
-                              },
-                            );
-                          },
-                        );
-                      },
-                      isExpanded: changelogItem.isExpanded,
-                      body: changelogItem.child,
-                    );
-                  }).toList(),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
-                  child: Opacity(
-                    opacity: 0.6,
-                    child: InkWell(
-                      onTap: () => launch(
-                          "https://github.com/rootasjey/fig.style/releases"),
-                      child: Text(
-                        "See releases online",
-                        style: TextStyle(decoration: TextDecoration.underline),
-                      ),
-                    ),
-                  ),
-                ),
+                titleContainer(),
+                subtitleContainer(),
+                expansionPanList(),
+                onlineReleasesButton(),
               ]),
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  ChangelogItem itemChangelogTemplate({
+    @required DateTime date,
+    @required String textTitle,
+    List<Widget> children = const <Widget>[],
+  }) {
+    return ChangelogItem(
+      title: Text(
+        textTitle,
+        style: TextStyle(
+          fontSize: 18.0,
+          color: stateColors.secondary,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+      date: date,
+      child: Align(
+        alignment: Alignment.topLeft,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: children,
+          ),
+        ),
       ),
     );
   }
@@ -188,26 +158,110 @@ class _ChangelogState extends State<Changelog> {
     );
   }
 
-  ChangelogItem itemChangelogTemplate({
-    @required DateTime date,
-    @required String textTitle,
-    List<Widget> children = const <Widget>[],
-  }) {
-    return ChangelogItem(
-      title: Text(
-        textTitle,
-        style: TextStyle(
-          fontWeight: FontWeight.w600,
+  Widget expansionPanList() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          width: 600.0,
+          child: ExpansionPanelList(
+            expansionCallback: (int index, bool isExpanded) {
+              setState(() {
+                changelogItemsList[index].isExpanded = !isExpanded;
+              });
+            },
+            children: changelogItemsList.map((changelogItem) {
+              final date = changelogItem.date;
+              final day = date.day < 10 ? '0${date.day}' : date.day;
+              final month = date.month < 10 ? '0${date.month}' : date.month;
+
+              return ExpansionPanel(
+                headerBuilder: (context, isExpanded) {
+                  return ListTile(
+                    title: changelogItem.title,
+                    subtitle: Opacity(
+                      opacity: 0.5,
+                      child: Text("$day/$month/${date.year}"),
+                    ),
+                    onTap: () {
+                      setState(
+                        () {
+                          changelogItem.isExpanded = !changelogItem.isExpanded;
+                        },
+                      );
+                    },
+                  );
+                },
+                isExpanded: changelogItem.isExpanded,
+                body: changelogItem.child,
+              );
+            }).toList(),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget onlineReleasesButton() {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 20.0),
+        child: Opacity(
+          opacity: 0.6,
+          child: InkWell(
+            onTap: () =>
+                launch("https://github.com/rootasjey/fig.style/releases"),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Wrap(
+                spacing: 20.0,
+                children: [
+                  Text(
+                    "See releases online",
+                    style: TextStyle(
+                      fontSize: 15,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                  Icon(
+                    UniconsLine.external_link_alt,
+                    size: 18.0,
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
-      date: date,
-      child: Align(
-        alignment: Alignment.topLeft,
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: children,
+    );
+  }
+
+  Widget subtitleContainer() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 24.0),
+      child: Opacity(
+        opacity: 0.4,
+        child: Text(
+          "You can find the app version history below",
+          style: TextStyle(
+            fontSize: 18.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget titleContainer() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Opacity(
+        opacity: 0.8,
+        child: Text(
+          "Changelog",
+          style: TextStyle(
+            fontSize: 60.0,
+            fontWeight: FontWeight.w600,
           ),
         ),
       ),
