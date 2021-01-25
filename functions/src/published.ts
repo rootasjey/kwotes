3a19139a3608f102167c4170d68670d68ec5b587
rootasjey <rootasjey@users.noreply.github.com>
2021-01-25 19:59:45 +0100

    ➕ (func) Add [onAuthorUpdated] (sync author.name)

--- functions/src/published.ts
+++ functions/src/published.ts
@@ -3,6 +3,40 @@ import { adminApp } from './adminApp';
 
 const firestore = adminApp.firestore();
 
+
+export const onAuthorUpdated = functions
+  .region('europe-west3')
+  .firestore
+  .document('authors/{authorId}')
+  .onUpdate(async (snapshot) => {
+    const beforeData = snapshot.before.data();
+    const afterData = snapshot.after.data();
+    const authorId = snapshot.after.id;
+
+    const beforeName: string = beforeData.name;
+    const afterName: string = afterData.name;
+
+    if (beforeName === afterName) {
+      return;
+    }
+
+    const allQuotesWithAuthorName = await firestore
+      .collection('quotes')
+      .where('author.id', '==', authorId)
+      .get();
+
+    if (allQuotesWithAuthorName.empty) {
+      return;
+    }
+
+    for await (const quoteDoc of allQuotesWithAuthorName.docs) {
+      await quoteDoc.ref
+        .update('author.name', afterName);
+    }
+
+    return true;
+  });
+
 export const onQuoteAdded = functions
   .region('europe-west3')
   .firestore
