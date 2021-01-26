fc5854abbf4711a9d2f985ec77a1c5317378b2fa
rootasjey <rootasjey@users.noreply.github.com>
2021-01-26 12:18:16 +0100

    👌 Code format (add comments)

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,7 +2,13 @@ class Constants {
   static const cardElevation = 2.0;
   static const maxMobileWidth = 700.0;
   static const appGithubUrl = 'https://github.com/rootasjey/fig.style';
+
+  /// Twitter web intent API -> [hashtags] section.
+  /// More info at https://twitter.com/intent/tweet.
   static const twitterShareHashtags = "&hashtags=quote,figstyle";
+
+  /// Twitter web intent API.
+  /// More info at https://twitter.com/intent/tweet.
   static const baseTwitterShareUrl = "https://twitter.com/intent/tweet"
       "?via=fig_style&text=";
   static const baseAuthorUrl = "https://fig.style/#/authors/";
