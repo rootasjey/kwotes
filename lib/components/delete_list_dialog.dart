42bc6d366900b764afa754eb5430a10aa36d5e87
rootasjey <rootasjey@users.noreply.github.com>
2021-02-15 14:24:31 +0100

    ➕ Add keyboard validation on delete list (enter/space key)

--- lib/components/delete_list_dialog.dart
+++ lib/components/delete_list_dialog.dart
@@ -2,6 +2,7 @@ import 'package:figstyle/components/sheet_header.dart';
 import 'package:figstyle/state/colors.dart';
 import 'package:flash/flash.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 Future showDeleteListDialog({
@@ -10,36 +11,49 @@ Future showDeleteListDialog({
   @required VoidCallback onCancel,
   @required String listName,
 }) async {
+  FocusNode focusNode = FocusNode();
+
   if (MediaQuery.of(context).size.width < 700.0) {
     return await showCustomModalBottomSheet(
       context: context,
       builder: (context) {
-        return Material(
-          child: SafeArea(
-            top: false,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ListTile(
-                  title: Text(
-                    'Confirm',
-                    style: TextStyle(
+        return RawKeyboardListener(
+          focusNode: focusNode,
+          autofocus: true,
+          onKey: (keyEvent) {
+            if (keyEvent.isKeyPressed(LogicalKeyboardKey.enter) ||
+                keyEvent.isKeyPressed(LogicalKeyboardKey.space)) {
+              onConfirm();
+              return;
+            }
+          },
+          child: Material(
+            child: SafeArea(
+              top: false,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                    title: Text(
+                      'Confirm',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    trailing: Icon(
+                      Icons.check,
                       color: Colors.white,
                     ),
+                    tileColor: stateColors.deletion,
+                    onTap: onConfirm,
                   ),
-                  trailing: Icon(
-                    Icons.check,
-                    color: Colors.white,
+                  ListTile(
+                    title: Text('Cancel'),
+                    trailing: Icon(Icons.close),
+                    onTap: onCancel,
                   ),
-                  tileColor: Color(0xfff55c5c),
-                  onTap: onConfirm,
-                ),
-                ListTile(
-                  title: Text('Cancel'),
-                  trailing: Icon(Icons.close),
-                  onTap: onCancel,
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         );
@@ -77,67 +91,78 @@ Future showDeleteListDialog({
             message: Container(
               height: MediaQuery.of(context).size.height - 100.0,
               padding: const EdgeInsets.all(60.0),
-              child: Material(
-                child: ListView(
-                  physics: ClampingScrollPhysics(),
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(40.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SheetHeader(
-                            title: "Delete list $listName",
-                            subTitle: "Are you sure?",
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(
-                              top: 20.0,
-                              left: 10.0,
+              child: RawKeyboardListener(
+                focusNode: focusNode,
+                autofocus: true,
+                onKey: (keyEvent) {
+                  if (keyEvent.isKeyPressed(LogicalKeyboardKey.enter) ||
+                      keyEvent.isKeyPressed(LogicalKeyboardKey.space)) {
+                    onConfirm();
+                    return;
+                  }
+                },
+                child: Material(
+                  child: ListView(
+                    physics: ClampingScrollPhysics(),
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(40.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SheetHeader(
+                              title: "Delete list $listName",
+                              subTitle: "Are you sure?",
                             ),
-                            child: Wrap(
-                              spacing: 10.0,
-                              runSpacing: 10.0,
-                              children: [
-                                OutlinedButton.icon(
-                                  onPressed: onCancel,
-                                  icon: Opacity(
-                                    opacity: 0.6,
-                                    child: Icon(Icons.clear),
-                                  ),
-                                  label: Opacity(
-                                    opacity: 0.6,
-                                    child: Text(
-                                      'Cancel',
+                            Padding(
+                              padding: EdgeInsets.only(
+                                top: 20.0,
+                                left: 10.0,
+                              ),
+                              child: Wrap(
+                                spacing: 10.0,
+                                runSpacing: 10.0,
+                                children: [
+                                  OutlinedButton.icon(
+                                    onPressed: onCancel,
+                                    icon: Opacity(
+                                      opacity: 0.6,
+                                      child: Icon(Icons.clear),
+                                    ),
+                                    label: Opacity(
+                                      opacity: 0.6,
+                                      child: Text(
+                                        'Cancel',
+                                      ),
+                                    ),
+                                    style: OutlinedButton.styleFrom(
+                                      primary: stateColors.foreground,
                                     ),
                                   ),
-                                  style: OutlinedButton.styleFrom(
-                                    primary: stateColors.foreground,
-                                  ),
-                                ),
-                                OutlinedButton.icon(
-                                  onPressed: onConfirm,
-                                  icon: Opacity(
-                                    opacity: 0.6,
-                                    child: Icon(Icons.check),
-                                  ),
-                                  label: Opacity(
-                                    opacity: 0.6,
-                                    child: Text(
-                                      'Delete',
+                                  OutlinedButton.icon(
+                                    onPressed: onConfirm,
+                                    icon: Opacity(
+                                      opacity: 0.6,
+                                      child: Icon(Icons.check),
+                                    ),
+                                    label: Opacity(
+                                      opacity: 0.6,
+                                      child: Text(
+                                        'Delete',
+                                      ),
+                                    ),
+                                    style: OutlinedButton.styleFrom(
+                                      primary: stateColors.validation,
                                     ),
                                   ),
-                                  style: OutlinedButton.styleFrom(
-                                    primary: stateColors.validation,
-                                  ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
