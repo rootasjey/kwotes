5f706193b34364e70e151bdb5861c9c6cdf5d1fc
rootasjey <rootasjey@users.noreply.github.com>
2021-02-17 21:14:04 +0100

    🚑  Add toJSON() for [FromReference]

--- lib/types/from_reference.dart
+++ lib/types/from_reference.dart
@@ -1,15 +1,28 @@
 /// From which reference this object is from.
 class FromReference {
-  /// Object id (Firestore)
-  final String id;
+  /// Reference's id to which the author belongs to.
+  String id;
+
+  /// Reference's name.
+  /// This property doesn't exist in Firestore,
+  /// and is used mainly <hen editing author
+  /// (better indication to which reference the author belongs to).
+  String name;
 
   FromReference({
     this.id = '',
+    this.name = '',
   });
 
   factory FromReference.fromJSON(Map<String, dynamic> json) {
     return FromReference(
       id: json['id'] ?? '',
     );
+  }
+
+  Map<String, dynamic> toJSON() {
+    final data = Map<String, dynamic>();
+    data['id'] = id;
+    return data;
   }
 }
