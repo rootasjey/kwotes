d011fd016e23c8c8231ec58df9c5586175d542b2
rootasjey <rootasjey@users.noreply.github.com>
2021-02-20 01:24:37 +0100

    ☝️ Scope Snack (snack bar) with methods

--- lib/utils/snack.dart
+++ lib/utils/snack.dart
@@ -3,6 +3,98 @@ import 'package:figstyle/utils/flash_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:unicons/unicons.dart';
 
+/// Snack bar class.
+/// Helper to quickly dispay different snacks types.
+class Snack {
+  /// Show a snack with an error message.
+  static Future e({
+    @required BuildContext context,
+    Duration duration = const Duration(seconds: 5),
+    Widget icon,
+    @required String message,
+    Widget primaryAction,
+    String title = '',
+  }) {
+    Widget _icon;
+
+    if (icon != null) {
+      _icon = icon;
+    } else {
+      _icon = Icon(
+        UniconsLine.times,
+        color: Colors.pink,
+      );
+    }
+
+    return FlashHelper.groundedBottom(
+      context,
+      title: title,
+      message: message,
+      icon: _icon,
+      primaryAction: primaryAction,
+    );
+  }
+
+  /// Show a snack with an informative message.
+  static Future i({
+    @required BuildContext context,
+    Duration duration = const Duration(seconds: 5),
+    Widget icon,
+    @required String message,
+    Widget primaryAction,
+    String title = '',
+  }) {
+    Widget _icon;
+
+    if (icon != null) {
+      _icon = icon;
+    } else {
+      _icon = Icon(
+        UniconsLine.info,
+        color: Colors.blue,
+      );
+    }
+
+    return FlashHelper.groundedBottom(
+      context,
+      title: title,
+      message: message,
+      icon: _icon,
+      primaryAction: primaryAction,
+    );
+  }
+
+  /// Show a snack with a success message.
+  static Future s({
+    @required BuildContext context,
+    Duration duration = const Duration(seconds: 5),
+    Widget icon,
+    @required String message,
+    Widget primaryAction,
+    String title = '',
+  }) {
+    Widget _icon;
+
+    if (icon != null) {
+      _icon = icon;
+    } else {
+      _icon = Icon(
+        UniconsLine.check,
+        color: Colors.green,
+      );
+    }
+
+    return FlashHelper.groundedBottom(
+      context,
+      title: title,
+      message: message,
+      icon: _icon,
+      duration: duration,
+      primaryAction: primaryAction,
+    );
+  }
+}
+
 Future showSnack({
   @required BuildContext context,
   String title = '',
@@ -16,7 +108,12 @@ Future showSnack({
       context,
       title: title,
       message: message,
-      icon: icon != null ? icon : Icon(UniconsLine.times, color: Colors.pink),
+      icon: icon != null
+          ? icon
+          : Icon(
+              UniconsLine.times,
+              color: Colors.pink,
+            ),
       primaryAction: primaryAction,
     );
   } else if (type == SnackType.success) {
@@ -24,7 +121,12 @@ Future showSnack({
       context,
       title: title,
       message: message,
-      icon: icon != null ? icon : Icon(UniconsLine.check, color: Colors.green),
+      icon: icon != null
+          ? icon
+          : Icon(
+              UniconsLine.check,
+              color: Colors.green,
+            ),
       primaryAction: primaryAction,
     );
   }
