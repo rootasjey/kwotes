1ebf016a680f77df218fc83ea89e5fba1e1b2657
rootasjey <rootasjey@users.noreply.github.com>
2021-03-08 20:18:14 +0100

    🚀  Update app version to [2.68.2]

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static const appVersion = '2.68.1';
+  static const appVersion = '2.68.2';
   static const appBuildNumber = 13;
   static const cardElevation = 2.0;
   static const maxMobileWidth = 700.0;
