fc6d194e4306c61a2bbed321ac3bdf0f5b6ecf41
rootasjey <rootasjey@users.noreply.github.com>
2021-03-14 21:08:48 +0100

    ➕ Add [elevation] property to LangPopupMenuButton

--- lib/components/lang_popup_menu_button.dart
+++ lib/components/lang_popup_menu_button.dart
@@ -7,11 +7,13 @@ class LangPopupMenuButton extends StatelessWidget {
   final Function(String) onLangChanged;
   final double opacity;
   final EdgeInsets padding;
+  final double elevation;
 
   const LangPopupMenuButton({
     Key key,
     @required this.lang,
     @required this.onLangChanged,
+    this.elevation = 0.0,
     this.opacity = 1.0,
     this.padding = const EdgeInsets.only(top: 2.0),
   }) : super(key: key);
@@ -20,29 +22,32 @@ class LangPopupMenuButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: padding,
-      child: Opacity(
-        opacity: opacity,
-        child: PopupMenuButton<String>(
-          tooltip: "Change language",
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              lang.toUpperCase(),
-              style: TextStyle(
-                color: stateColors.foreground,
-                fontSize: 16.0,
+      child: Material(
+        elevation: elevation,
+        child: Opacity(
+          opacity: opacity,
+          child: PopupMenuButton<String>(
+            tooltip: "Change language",
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                lang.toUpperCase(),
+                style: TextStyle(
+                  color: stateColors.foreground,
+                  fontSize: 16.0,
+                ),
               ),
             ),
+            onSelected: onLangChanged,
+            itemBuilder: (context) => Language.available()
+                .map(
+                  (value) => PopupMenuItem(
+                    value: value,
+                    child: Text(value.toUpperCase()),
+                  ),
+                )
+                .toList(),
           ),
-          onSelected: onLangChanged,
-          itemBuilder: (context) => Language.available()
-              .map(
-                (value) => PopupMenuItem(
-                  value: value,
-                  child: Text(value.toUpperCase()),
-                ),
-              )
-              .toList(),
         ),
       ),
     );
