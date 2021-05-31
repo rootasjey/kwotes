0b86bbb56ebae559e333220f112b5dbfffb7b7e5
rootasjey <rootasjey@users.noreply.github.com>
2021-05-31 21:42:32 +0200

    build: :wrench: update (auto) config → new Flutter version

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/rootasjey/dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/rootasjey/GitHub/fig.style"
+export "FLUTTER_APPLICATION_PATH=/Users/rootasjey/GitHub/fig_style"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
