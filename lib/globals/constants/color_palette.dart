d2b527ff8afd2e1ff61d7a0e8b7cca110db6cf4f
rootasjey <5097282+rootasjey@users.noreply.github.com>
2023-11-24 03:58:49 +0100

    feat: :lipstick: update primary & secondary colors
    Add `createMaterialColorFrom(Color)` method.

--- lib/globals/constants/color_palette.dart
+++ lib/globals/constants/color_palette.dart
@@ -73,6 +73,27 @@ class ColorPalette {
     return foundColor;
   }
 
+  /// Create a [MaterialColor] from a [Color].
+  MaterialColor createMaterialColorFrom(Color color) {
+    List strengths = <double>[.05];
+    final swatch = <int, Color>{};
+    final int r = color.red, g = color.green, b = color.blue;
+
+    for (int i = 1; i < 10; i++) {
+      strengths.add(0.1 * i);
+    }
+    for (var strength in strengths) {
+      final double ds = 0.5 - strength;
+      swatch[(strength * 1000).round()] = Color.fromRGBO(
+        r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+        g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+        b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+        1,
+      );
+    }
+    return MaterialColor(color.value, swatch);
+  }
+
   List<Topic> topics = [];
 
   Topic getRandomTopic() {
@@ -104,7 +125,7 @@ class ColorPalette {
 
   final Color dark = const Color(0xFF161616);
 
-  final Color drafts = Colors.amber.shade600;
+  final Color drafts = Colors.orange;
 
   final Color delete = Colors.pink;
 
@@ -124,7 +145,12 @@ class ColorPalette {
   final Color password = Colors.orange.shade300;
 
   /// Primary application's color.
-  final Color primary = Colors.blue.shade700;
+  final Color primary = Colors.deepOrange;
+  // final Color primary = Colors.blue.shade700;
+
+  /// Secondary application's color.
+  Color secondary = Colors.blue;
+  // Color secondary = Colors.pink;
 
   /// Color for profile.
   final Color profile = Colors.indigo;
@@ -142,12 +168,9 @@ class ColorPalette {
   final Color sections = Colors.green.shade700;
 
   /// Color for settings.
-  final Color settings = Colors.teal;
+  final Color settings = Colors.amber;
 
   final Color bio = Colors.deepPurple.shade300;
-
-  /// Secondary application's color.
-  Color secondary = Colors.pink;
 
   final Color validation = Colors.green;
 }
