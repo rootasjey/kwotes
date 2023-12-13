37f5b67f6dc5db6fbc7463b6e139787a1ea8997a
rootasjey <5097282+rootasjey@users.noreply.github.com>
2023-12-14 00:45:45 +0100

    feat: :sparkles: add `EnumFrameBorderStyle` enum
    Allow us to choose between different border styles.

---
+++ lib/types/enums/enum_frame_border_style.dart
@@ -0,0 +1,11 @@
+/// Frame border style.
+enum EnumFrameBorderStyle {
+  /// App window will have a colored frame border.
+  colored,
+
+  /// App window will have a discrete frame border.
+  discrete,
+
+  /// App window will have either a black or white frame border.
+  highContrast,
+}
