ffbdc41d506f6f0792ec1b4aaba3f5dbf191343a
rootasjey <5097282+rootasjey@users.noreply.github.com>
2024-01-11 13:09:37 +0100

    feat: :sparkles: udpate context menu on topic page

--- lib/screens/topic_page/topic_page.dart
+++ lib/screens/topic_page/topic_page.dart
@@ -3,6 +3,8 @@ import "package:cloud_firestore/cloud_firestore.dart";
 import "package:easy_localization/easy_localization.dart";
 import "package:flutter/material.dart";
 import "package:flutter_improved_scrolling/flutter_improved_scrolling.dart";
+import "package:flutter_solidart/flutter_solidart.dart";
+import "package:flutter_tabler_icons/flutter_tabler_icons.dart";
 import "package:kwotes/actions/quote_actions.dart";
 import "package:kwotes/components/context_menu_components.dart";
 import "package:kwotes/components/custom_scroll_behaviour.dart";
@@ -14,15 +16,21 @@ import "package:kwotes/router/navigation_state_helper.dart";
 import "package:kwotes/screens/search/search_quote_text.dart";
 import "package:kwotes/screens/topic_page/topic_page_header.dart";
 import "package:kwotes/types/alias/json_alias.dart";
+import "package:kwotes/types/enums/enum_draft_quote_operation.dart";
 import "package:kwotes/types/enums/enum_language_selection.dart";
 import "package:kwotes/types/enums/enum_page_state.dart";
+import "package:kwotes/types/enums/enum_signal_id.dart";
 import "package:kwotes/types/firestore/document_snapshot_map.dart";
 import "package:kwotes/types/firestore/query_doc_snap_map.dart";
 import "package:kwotes/types/firestore/query_map.dart";
 import "package:kwotes/types/firestore/query_snap_map.dart";
 import "package:kwotes/types/quote.dart";
+import "package:kwotes/types/user/user_firestore.dart";
+import "package:kwotes/types/user/user_rights.dart";
 import "package:loggy/loggy.dart";
+import "package:screenshot/screenshot.dart";
 import "package:super_context_menu/super_context_menu.dart";
+import "package:text_wrap_auto_size/solution.dart";
 
 class TopicPage extends StatefulWidget {
   const TopicPage({
@@ -57,8 +65,22 @@ class _TopicPageState extends State<TopicPage> with UiLoggy {
   /// List of quotes.
   final List<Quote> _quotes = [];
 
+  /// Screenshot controller.
+  final ScreenshotController _screenshotController = ScreenshotController();
+
   /// Page scroll controller (used to fetch more data).
   final ScrollController _pageScrollController = ScrollController();
+
+  /// Text wrap solution to calculate font size according to window size.
+  Solution _textWrapSolution = Solution(
+    const Text(""),
+    const TextStyle(),
+    const Size(0, 0),
+    const Size(0, 0),
+  );
+
+  /// Collection name.
+  final String _collectionName = "quotes";
 
   @override
   void initState() {
@@ -86,15 +108,8 @@ class _TopicPageState extends State<TopicPage> with UiLoggy {
     }
 
     final bool isMobileSize = Utils.measurements.isMobileSize(context);
-    final EdgeInsets bodyPadding = isMobileSize
-        ? const EdgeInsets.only(
-            left: 16.0,
-            right: 16.0,
-          )
-        : const EdgeInsets.only(
-            left: 48.0,
-            right: 48.0,
-          );
+    final Signal<UserFirestore> signalUserFirestore =
+        context.get<Signal<UserFirestore>>(EnumSignalId.userFirestore);
 
     return Focus(
       autofocus: true,
@@ -108,31 +123,52 @@ class _TopicPageState extends State<TopicPage> with UiLoggy {
               controller: _pageScrollController,
               slivers: [
                 TopicPageHeader(
+                  isMobileSize: isMobileSize,
                   topic: getTopic(),
                   onTapName: onTapTopicName,
                 ),
-                SliverPadding(
-                  padding: bodyPadding,
-                  sliver: SliverList.builder(
-                    itemBuilder: (BuildContext context, int index) {
-                      final Quote quote = _quotes[index];
-                      return SearchQuoteText(
-                        quote: quote,
-                        onTapQuote: onTapQuote,
-                        // onDoubleTapQuote: onDoubleTapQuote,
-                        tiny: isMobileSize,
-                        margin: const EdgeInsets.symmetric(
-                          vertical: 12.0,
-                        ),
-                        quoteMenuProvider: (MenuRequest menuRequest) =>
-                            ContextMenuComponents.quoteMenuProvider(
-                          context,
-                          quote: quote,
-                        ),
-                      );
-                    },
-                    itemCount: _quotes.length,
-                  ),
+                SignalBuilder(
+                  signal: signalUserFirestore,
+                  builder: (context, userFirestore, child) {
+                    final UserRights userRights = userFirestore.rights;
+                    final bool isAdmin = userRights.canManageQuotes;
+                    final onChangeLanguage =
+                        isAdmin ? onChangeQuoteLanguage : null;
+
+                    return SliverPadding(
+                      padding: getBodyPadding(isMobileSize),
+                      sliver: SliverList.builder(
+                        itemBuilder: (BuildContext context, int index) {
+                          final Quote quote = _quotes[index];
+                          return SearchQuoteText(
+                            quote: quote,
+                            onTapQuote: onTapQuote,
+                            // onDoubleTapQuote: onDoubleTapQuote,
+                            tiny: isMobileSize,
+                            margin: const EdgeInsets.symmetric(
+                              vertical: 12.0,
+                            ),
+                            quoteMenuProvider: (MenuRequest menuRequest) {
+                              return ContextMenuComponents.quoteMenuProvider(
+                                context,
+                                quote: quote,
+                                onCopyQuote: onCopyQuote,
+                                onCopyQuoteUrl: onCopyQuoteUrl,
+                                onChangeLanguage: onChangeLanguage,
+                                onDelete: isAdmin ? onDeleteQuote : null,
+                                onEdit: isAdmin ? onEditQuote : null,
+                                onShareImage: onShareImage,
+                                onShareLink: onShareLink,
+                                onShareText: onShareText,
+                                userId: userFirestore.id,
+                              );
+                            },
+                          );
+                        },
+                        itemCount: _quotes.length,
+                      ),
+                    );
+                  },
                 ),
               ],
             ),
@@ -142,6 +178,22 @@ class _TopicPageState extends State<TopicPage> with UiLoggy {
     );
   }
 
+  /// Return body padding.
+  EdgeInsets getBodyPadding(bool isMobileSize) {
+    if (isMobileSize) {
+      return const EdgeInsets.only(
+        left: 16.0,
+        right: 16.0,
+      );
+    }
+
+    return const EdgeInsets.only(
+      left: 48.0,
+      right: 48.0,
+    );
+  }
+
+  /// Get language from vault.
   Future<String> getLanguage() async {
     final EnumLanguageSelection savedLanguage = await Utils.vault.getLanguage();
     if (Utils.linguistic.available().contains(savedLanguage)) {
@@ -158,7 +210,7 @@ class _TopicPageState extends State<TopicPage> with UiLoggy {
     final String topic = getTopic();
 
     final QueryMap query = FirebaseFirestore.instance
-        .collection("quotes")
+        .collection(_collectionName)
         .where("topics.$topic", isEqualTo: true)
         .where("language", isEqualTo: language)
         .orderBy("created_at", descending: _descending)
@@ -171,6 +223,7 @@ class _TopicPageState extends State<TopicPage> with UiLoggy {
     return query.startAfterDocument(lastDocument);
   }
 
+  /// Retrieve topic name from navigation state.
   String getTopic() {
     return widget.topic != HomeContentLocation.topicRoute.split("/").last
         ? widget.topic
@@ -219,6 +272,165 @@ class _TopicPageState extends State<TopicPage> with UiLoggy {
     }
   }
 
+  /// Callback to update a quote's language.
+  void onChangeQuoteLanguage(Quote quote, String language) async {
+    final Signal<UserFirestore> currentUser =
+        context.get<Signal<UserFirestore>>(EnumSignalId.userFirestore);
+
+    final UserRights userRights = currentUser.value.rights;
+    final bool canManageQuotes = userRights.canManageQuotes;
+
+    if (!canManageQuotes) {
+      return;
+    }
+
+    final int index = _quotes.indexOf(quote);
+    if (index == -1) {
+      return;
+    }
+
+    if (language == quote.language) {
+      return;
+    }
+
+    final String selectedLanguage = await getLanguage();
+    if (selectedLanguage != language) {
+      setState(() => _quotes.removeAt(index));
+    }
+
+    try {
+      await FirebaseFirestore.instance
+          .collection(_collectionName)
+          .doc(quote.id)
+          .update({"language": language});
+    } catch (error) {
+      loggy.error(error);
+      if (!mounted) return;
+      Utils.graphic.showSnackbar(
+        context,
+        message: "quote.update.failed".tr(),
+      );
+    }
+  }
+
+  /// Callback to copy.
+  void onCopyQuote(Quote quote) {
+    QuoteActions.copyQuote(quote);
+    final bool isMobileSize = Utils.measurements.isMobileSize(context);
+    Utils.graphic.showCopyQuoteSnackbar(context, isMobileSize: isMobileSize);
+  }
+
+  /// Callback to copy quote url.
+  void onCopyQuoteUrl(Quote quote) {
+    QuoteActions.copyQuoteUrl(quote);
+    final bool isMobileSize = Utils.measurements.isMobileSize(context);
+    Utils.graphic.showCopyQuoteLinkSnackbar(
+      context,
+      isMobileSize: isMobileSize,
+    );
+  }
+
+  /// Callback to delete a published quote.
+  void onDeleteQuote(Quote quote) async {
+    final Signal<UserFirestore> currentUser =
+        context.get<Signal<UserFirestore>>(EnumSignalId.userFirestore);
+
+    final UserRights userRights = currentUser.value.rights;
+    final bool canManageQuotes = userRights.canManageQuotes;
+
+    if (!canManageQuotes) return;
+
+    final int index = _quotes.indexOf(quote);
+    if (index == -1) return;
+
+    setState(() => _quotes.removeAt(index));
+
+    try {
+      await FirebaseFirestore.instance
+          .collection(_collectionName)
+          .doc(quote.id)
+          .delete();
+      loggy.info("will delete quote: ${quote.id}");
+
+      if (!mounted) return;
+      Utils.graphic.showSnackbarWithCustomText(
+        context,
+        duration: const Duration(seconds: 10),
+        text: Row(children: [
+          Expanded(
+            flex: 0,
+            child: Text(
+              "quote.delete.success".tr(),
+              style: Utils.calligraphy.body(
+                textStyle: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w500,
+                  color: Theme.of(context)
+                      .textTheme
+                      .bodyMedium
+                      ?.color
+                      ?.withOpacity(0.6),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: TextButton(
+              onPressed: () {
+                setState(() => _quotes.insert(index, quote));
+                FirebaseFirestore.instance
+                    .collection(_collectionName)
+                    .doc(quote.id)
+                    .set(quote.toMap(
+                      operation: EnumQuoteOperation.restore,
+                    ));
+
+                ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                loggy.info("reverse add quote: ${quote.id}");
+              },
+              style: TextButton.styleFrom(
+                  textStyle: const TextStyle(
+                fontSize: 16.0,
+              )),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "rollback".tr(),
+                    style: Utils.calligraphy.body(
+                      textStyle: const TextStyle(
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 8.0),
+                    child: Icon(TablerIcons.rotate_2),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ]),
+      );
+    } catch (error) {
+      loggy.error(error);
+      setState(() => _quotes.insert(index, quote));
+    }
+  }
+
+  /// Callback to edit a published quote.
+  void onEditQuote(Quote quote) {
+    NavigationStateHelper.quote = quote;
+    context.beamToNamed(
+      HomeContentLocation.editQuoteRoute.replaceFirst(":quoteId", quote.id),
+      data: {
+        "quoteId": quote.id,
+      },
+    );
+  }
+
   /// On double tap quote.
   void onDoubleTapQuote(Quote quote) {
     QuoteActions.copyQuote(quote);
@@ -229,6 +441,7 @@ class _TopicPageState extends State<TopicPage> with UiLoggy {
     );
   }
 
+  /// Navigate to quote page.
   void onTapQuote(Quote quote) {
     NavigationStateHelper.lastTopicName = widget.topic;
     Beamer.of(context).beamToNamed(
@@ -238,6 +451,7 @@ class _TopicPageState extends State<TopicPage> with UiLoggy {
     );
   }
 
+  /// Watch scrolling to fetch more data.
   void onScroll(double offset) {
     if (!_hasMoreResults) {
       return;
@@ -259,6 +473,44 @@ class _TopicPageState extends State<TopicPage> with UiLoggy {
     }
   }
 
+  /// Open share image bottom sheet.
+  void onShareImage(Quote quote) {
+    final Size windowSize = MediaQuery.of(context).size;
+    _textWrapSolution = Utils.graphic.getTextSolution(
+      quote: quote,
+      windowSize: windowSize,
+    );
+
+    Utils.graphic.onOpenShareImage(
+      context,
+      mounted: mounted,
+      quote: quote,
+      screenshotController: _screenshotController,
+      textWrapSolution: _textWrapSolution,
+    );
+  }
+
+  /// Callback fired to share quote as link.
+  void onShareLink(Quote quote) {
+    Utils.graphic.onShareLink(context, quote: quote);
+  }
+
+  /// Callback fired to share quote as text.
+  void onShareText(Quote quote) {
+    Utils.graphic.onShareText(
+      context,
+      quote: quote,
+      onCopyQuote: (Quote quote) {
+        onCopyQuote(quote);
+        Utils.graphic.showSnackbar(
+          context,
+          message: "quote.copy.success.name".tr(),
+        );
+      },
+    );
+  }
+
+  /// Callback on tap topic name -> scroll to top.
   void onTapTopicName() {
     _pageScrollController.animateTo(
       0.0,
