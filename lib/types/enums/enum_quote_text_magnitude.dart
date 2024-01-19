380bba3c38e198e552d8266c9e9cc28278313365
rootasjey <5097282+rootasjey@users.noreply.github.com>
2024-01-19 13:23:59 +0100

    feat: :sparkles: add `EnumQuoteTextMagnitude` enum

---
+++ lib/types/enums/enum_quote_text_magnitude.dart
@@ -0,0 +1,11 @@
+/// Predefined quote text size enumeration.
+enum EnumQuoteTextMagnitude {
+  /// Big text size.
+  big,
+
+  /// Normal text size.
+  medium,
+
+  /// Small text size.
+  small,
+}
