965604cb7901a78a9be3b663d51e5f73bced4e88
rootasjey <5097282+rootasjey@users.noreply.github.com>
2024-04-30 13:42:28 +0200

    feat: :sparkles: update `ShowcaseText` to accept hero tag, subtitle value
    Update `ShowcaseText` avatar position & form.

--- lib/components/texts/showcase_text.dart
+++ lib/components/texts/showcase_text.dart
@@ -9,6 +9,7 @@ class ShowcaseText extends StatefulWidget {
     required this.textValue,
     this.isDark = false,
     this.isMobileSize = false,
+    this.useSquareAvatar = false,
     this.margin = EdgeInsets.zero,
     this.padding = const EdgeInsets.all(2.0),
     this.onTap,
@@ -16,6 +17,8 @@ class ShowcaseText extends StatefulWidget {
     this.index = 0,
     this.initialForegroundColor,
     this.imageProvider,
+    this.subtitleValue = "",
+    this.titleHeroTag,
   });
 
   /// Whether to adapt UI to dark theme.
@@ -23,6 +26,9 @@ class ShowcaseText extends StatefulWidget {
 
   /// Adapt UI to mobile size.
   final bool isMobileSize;
+
+  /// Use square avatar if true.
+  final bool useSquareAvatar;
 
   /// Initial foreground color.
   final Color? initialForegroundColor;
@@ -45,14 +51,22 @@ class ShowcaseText extends StatefulWidget {
   /// Document ID for hero animation transition.
   final String docId;
 
-  /// Text value.
+  /// Title text.
   final String textValue;
+
+  /// Subtitle text.
+  final String subtitleValue;
+
+  /// Title hero tag.
+  final Object? titleHeroTag;
 
   @override
   State<ShowcaseText> createState() => _ShowcaseTextState();
 }
 
 class _ShowcaseTextState extends State<ShowcaseText> {
+  bool _colorFilterActive = true;
+
   /// Color on hover.
   Color? _accentColor;
 
@@ -88,16 +102,61 @@ class _ShowcaseTextState extends State<ShowcaseText> {
     }
 
     final ImageProvider? imageProvider = widget.imageProvider;
+    Widget avatar = const SizedBox.shrink();
+
+    if (imageProvider != null && !widget.useSquareAvatar) {
+      avatar = BetterAvatar(
+        imageProvider: imageProvider,
+        radius: 16.0,
+        onTap: widget.onTap,
+        heroTag: widget.docId,
+        colorFilter: _colorFilterActive
+            ? const ColorFilter.mode(
+                Colors.grey,
+                BlendMode.saturation,
+              )
+            : null,
+        margin: const EdgeInsets.only(right: 12.0),
+      );
+    } else if (imageProvider != null && widget.useSquareAvatar) {
+      avatar = Hero(
+        tag: widget.docId,
+        child: Card(
+          margin: const EdgeInsets.only(right: 12.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4.0),
+          ),
+          child: Container(
+            width: 50.0,
+            height: 50.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4.0),
+            ),
+            clipBehavior: Clip.antiAlias,
+            child: Image(
+              image: imageProvider,
+              fit: BoxFit.cover,
+              color: _colorFilterActive ? Colors.grey : null,
+              colorBlendMode: BlendMode.saturation,
+            ),
+          ),
+        ),
+      );
+    }
 
     return Padding(
       padding: widget.margin,
       child: InkWell(
         onTap: widget.onTap,
+        onTapDown: onTapDown,
+        onTapCancel: onTapCancel,
+        onTapUp: onTapUp,
         splashColor: _foregroundColor?.withOpacity(0.4),
         hoverColor: Colors.transparent,
         borderRadius: BorderRadius.circular(4.0),
         onHover: (bool isHover) {
           setState(() {
+            _colorFilterActive = false;
             _foregroundColor = isHover ? _accentColor : _initialForegroundColor;
           });
         },
@@ -105,35 +164,61 @@ class _ShowcaseTextState extends State<ShowcaseText> {
           padding: widget.padding,
           child: Row(
             children: [
+              if (imageProvider != null) avatar,
               Expanded(
-                child: Hero(
-                  tag: widget.docId,
-                  child: Text(
-                    widget.textValue,
-                    style: Utils.calligraphy.body(
-                      textStyle: TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.w200,
-                        color: _foregroundColor,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Hero(
+                      tag: widget.titleHeroTag ?? ValueKey(widget.index),
+                      child: Text(
+                        widget.textValue,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: Utils.calligraphy.body(
+                          textStyle: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w400,
+                            color: _foregroundColor,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                    if (widget.subtitleValue.isNotEmpty)
+                      Text(
+                        widget.subtitleValue,
+                        style: Utils.calligraphy.body(
+                            textStyle: TextStyle(
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.w400,
+                          color: _foregroundColor?.withOpacity(0.3),
+                        )),
+                      ),
+                  ],
                 ),
               ),
-              if (imageProvider != null)
-                BetterAvatar(
-                  imageProvider: imageProvider,
-                  radius: 16.0,
-                  colorFilter: const ColorFilter.mode(
-                    Colors.grey,
-                    BlendMode.saturation,
-                  ),
-                  margin: const EdgeInsets.only(right: 8.0),
-                ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  void onTapDown(TapDownDetails details) {
+    setState(() {
+      _colorFilterActive = false;
+    });
+  }
+
+  void onTapCancel() {
+    setState(() {
+      _colorFilterActive = true;
+    });
+  }
+
+  void onTapUp(TapUpDetails details) {
+    setState(() {
+      _colorFilterActive = true;
+    });
   }
 }
