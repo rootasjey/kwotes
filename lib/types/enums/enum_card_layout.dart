2a09cd1f05cccb9b7ea1968b5ac51bc2d5da45a2
rootasjey <5097282+rootasjey@users.noreply.github.com>
2024-06-14 02:05:55 +0200

    feat: :label: add `EnumCardLayout` enum

---
+++ lib/types/enums/enum_card_layout.dart
@@ -0,0 +1,15 @@
+/// Predefined card layout enumeration.
+enum EnumCardLayout {
+  /// Compact card layout.
+  compact,
+
+  /// Large text card layout.
+  largeText,
+
+  /// Square card layout.
+  /// (e.g. 300px width and 300px height).
+  normal,
+
+  /// Wide card layout (e.g. 300px width and 400px height).
+  wide,
+}
