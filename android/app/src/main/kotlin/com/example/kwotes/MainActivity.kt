63512bded92d087515f11c81b3addd0941ec3648
rootasjey <5097282+rootasjey@users.noreply.github.com>
2024-07-17 21:46:18 +0200

    fix: :bug: fix package name in `MainActivity.kt` (android)

--- android/app/src/main/kotlin/com/example/kwotes/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/kwotes/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.kwotes
+package fr.codingbox.kwotes
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-//class MainActivity: FlutterActivity() {
-class MainActivity: FlutterFragmentActivity() {
+class MainActivity: FlutterActivity() {
+//class MainActivity: FlutterFragmentActivity() {
 }
